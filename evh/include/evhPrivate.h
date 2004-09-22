243a4c78b43b8f812dbf58e2f6245f81aeef8ae3
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-09-22 08:54:16 +0000

    Fix directory structure and add additional files

---
+++ evh/include/evhPrivate.h
@@ -0,0 +1,43 @@
+#ifndef evhPrivate_H
+#define evhPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: evhPrivate.h,v 1.1 2004-09-22 08:54:16 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    22-Sep-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "evh"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!evhPrivate_H*/
+
+/*___oOo___*/

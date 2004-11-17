aaab36e31ef4667b318a5150566cc065d2b98c7a
gzins <gzins@jmmc.fr>
2004-11-17 10:54:32 +0000

    *** empty log message ***

---
+++ evh/include/evh.h
@@ -0,0 +1,32 @@
+#ifndef evh_H
+#define evh_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: evh.h,v 1.1 2004-11-17 10:54:32 gzins Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* gzins     09-Nov-2004  Created
+*
+*******************************************************************************/
+/**
+ * \file
+ * This header contains all the 'evh' library functions declarations.
+ */
+#include "evhCALLBACK.h"
+#include "evhCALLBACK_LIST.h"
+#include "evhCMD_CALLBACK.h"
+#include "evhCMD_KEY.h"
+#include "evhHANDLER.h"
+#include "evhIOSTREAM_CALLBACK.h"
+#include "evhIOSTREAM_KEY.h"
+#include "evhKEY.h"
+#include "evhTASK.h"
+#include "evhSERVER.h"
+#include "evhErrors.h"
+
+#endif /*!evh_H*/
+
+/*___oOo___*/
+

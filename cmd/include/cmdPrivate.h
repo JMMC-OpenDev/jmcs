7acae85c8400f8b1018f2bc6fc95780cdebef061
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-11-19 15:57:12 +0000

    Fix directory structure and add additional files

---
+++ cmd/include/cmdPrivate.h
@@ -0,0 +1,43 @@
+#ifndef cmdPrivate_H
+#define cmdPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: cmdPrivate.h,v 1.1 2004-11-19 15:57:12 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    19-Nov-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "cmd"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!cmdPrivate_H*/
+
+/*___oOo___*/

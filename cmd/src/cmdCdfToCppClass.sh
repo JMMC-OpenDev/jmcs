96c8da80ff983af981b44725d7c61c40ff3f3528
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2004-11-21 19:59:02 +0000

    Update documentation

--- cmd/src/cmdCdfToCppClass.sh
+++ cmd/src/cmdCdfToCppClass.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
-
+#*******************************************************************************
+# JMMC project
 #
+# "@(#) $Id: cmdCdfToCppClass.sh,v 1.2 2004-11-21 19:59:02 mella Exp $"
+#
+# who       when         what
+# --------  -----------  -------------------------------------------------------
+# mella     21-Nov-2004  Created
+#
+#
+#*******************************************************************************
+
+#/**
+# \file
+# Generates a dedicated command class.
+#
+# \synopsis
+# cmdCdfToCppClass commandDefinitionFile moduleName 
+#
+# \param commandDefinitionFile : your command definition file.
+# \param moduleName : the name of the module to prefix for code generation.
+#
+# \n
+# \details
 # This script generates a Cpp Class associated to the given command 
 # description file.
+# 
+# \n
+# \warning This script requires xsltproc
 #
+# \n
+# \ex
+# \n Generate h and C file for a command class (dummyVALID_CMD.C and
+# dummyVALID_CMD.h). 
+# \code
+# cmdCdfToCppClass ../config/VALID.cdf dummy
+# \endcode
+# 
 #
+# \sa cmdIsCdfValid
+# 
+# \n 
+# 
+# */
 
 if [ $# != 2 ]
 then
@@ -17,3 +55,5 @@ XSLFILE=`miscLocateFile cmdCdfToCppClass.xsl`
 echo "Transforming $1 with $XSLFILE"
 
 xsltproc --stringparam "moduleName" $MODULENAME $XSLFILE $1
+
+#___oOo___

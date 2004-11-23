69c2378266895567d218f6079784b08ace21dc7b
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2004-11-23 08:40:27 +0000

    First revision

---
+++ cmd/include/cmd.h
@@ -0,0 +1,35 @@
+#ifndef cmd_H
+#define cmd_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: cmd.h,v 1.1 2004-11-23 08:40:27 mella Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* mella     23-Nov-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * cmd class declaration.
+ */
+
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+
+/**
+ * cmd Classes declaration
+ */
+#include "cmdCMD.h"
+#include "cmdPARAM.h"
+
+
+#endif /*!cmd_H*/
+
+/*___oOo___*/

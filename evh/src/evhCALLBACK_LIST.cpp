4e4b301e9dca6a6f340a7e9863977000f2ac6a53
gzins <gzins@jmmc.fr>
2004-12-08 13:32:15 +0000

    Handled evhCB_DELETE callback return value Added some method documentation

--- evh/src/evhCALLBACK_LIST.cpp
+++ evh/src/evhCALLBACK_LIST.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
 * JMMC project
 *
-* "@(#) $Id: evhCALLBACK_LIST.cpp,v 1.1 2004-12-05 19:00:25 gzins Exp $"
+* "@(#) $Id: evhCALLBACK_LIST.cpp,v 1.2 2004-12-08 13:32:15 gzins Exp $"
 *
 * who       when         what
 * --------  -----------  -------------------------------------------------------
@@ -15,7 +15,7 @@
  * Definition of the evhCALLBACK_LIST class.
  */
 
-static char *rcsId="@(#) $Id: evhCALLBACK_LIST.cpp,v 1.1 2004-12-05 19:00:25 gzins Exp $"; 
+static char *rcsId="@(#) $Id: evhCALLBACK_LIST.cpp,v 1.2 2004-12-08 13:32:15 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -158,38 +158,82 @@ mcsUINT32 evhCALLBACK_LIST::Size(void)
 }
 
 
+/**
+ * Executes all the callbacks of the list.
+ *
+ * The received messsage is passed as argument to the callback, with the user
+ * data pointer which has been given when callback has been added.
+ *
+ * If a callback returns with the evhCB_FAILURE bit set, the method returns
+ * immediately, i.e. the remaining callbacks in the list are not executed.
+ *
+ * If a callback returns with the evhCB_DELETE bit set, the method just
+ * detaches the callback (i.e. it is not removed from the list). The method
+ * Clean() to remove the detached event from the list.
+ *           
+ * \return SUCCESS or FAILURE (see above).
+ */
 mcsCOMPL_STAT evhCALLBACK_LIST::Run(const msgMESSAGE &msg)
 {
     logExtDbg("evhCALLBACK_LIST::Run()");
 
-    // Search callback in the list
+    // For each callback in the list
     std::list<evhCALLBACK *>::iterator iter;
     for (iter=_callbackList.begin(); iter != _callbackList.end(); iter++)
     {
-        logExtDbg("((*iter).Run(msg)()"); 
-        if ((((evhCMD_CALLBACK *)(*iter))->Run(msg) & evhCB_FAILURE) != 0)
+        // Run callback
+        evhCB_COMPL_STAT status;
+        status = ((evhCMD_CALLBACK *)(*iter))->Run(msg);
+        if ((status & evhCB_FAILURE) != 0)
         {
             return FAILURE;
         }
+        if ((status & evhCB_DELETE) != 0)
+        {
+            return ((evhCMD_CALLBACK *)(*iter))->Detach();
+        }
     }
+    // End for
 
     return SUCCESS;
 }
 
+/**
+ * Executes all the callbacks of the list.
+ *
+ * The received I/O descriptor is passed as argument to the callback, with the
+ * user data pointer which has been given when callback has been added.
+ *
+ * If a callback returns with the evhCB_FAILURE bit set, the method returns
+ * immediately, i.e. the remaining callbacks in the list are not executed.
+ *
+ * If a callback returns with the evhCB_DELETE bit set, the method just
+ * detaches the callback (i.e. it is not removed from the list). The method
+ * Clean() to remove the detached event from the list.
+ *           
+ * \return SUCCESS or FAILURE (see above).
+ */
 mcsCOMPL_STAT evhCALLBACK_LIST::Run(const int fd)
 {
     logExtDbg("evhCALLBACK_LIST::Run()");
 
-    // Search callback in the list
+    // For each callback in the list
     std::list<evhCALLBACK *>::iterator iter;
     for (iter=_callbackList.begin(); iter != _callbackList.end(); iter++)
     {
-        logExtDbg("((*iter).Run(msg)()"); 
-        if ((((evhIOSTREAM_CALLBACK *)(*iter))->Run(fd) & evhCB_FAILURE) != 0)
+        // Run callback
+        evhCB_COMPL_STAT status;
+        status = ((evhIOSTREAM_CALLBACK *)(*iter))->Run(fd);
+        if ((status & evhCB_FAILURE) != 0)
         {
             return FAILURE;
         }
+        if ((status & evhCB_DELETE) != 0)
+        {
+            return ((evhIOSTREAM_CALLBACK *)(*iter))->Detach();
+        }
     }
+    // End for
 
     return SUCCESS;
 }

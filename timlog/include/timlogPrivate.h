876fda2265c83a2b183e36b1176add81d1b29c88
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-12-17 08:53:05 +0000

    Fix directory structure and add additional files

---
+++ timlog/include/timlogPrivate.h
@@ -0,0 +1,43 @@
+#ifndef timlogPrivate_H
+#define timlogPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: timlogPrivate.h,v 1.1 2004-12-17 08:53:05 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    17-Dec-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "timlog"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!timlogPrivate_H*/
+
+/*___oOo___*/

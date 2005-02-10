2a0ba924a558c1f7458c6493f4309222111eb16c
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-02-10 13:34:06 +0000

    Fix directory structure and add additional files

---
+++ misco/include/misco.h
@@ -0,0 +1,40 @@
+#ifndef misco_H
+#define misco_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: misco.h,v 1.1 2005-02-10 13:34:06 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * misco general header file.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!misco_H*/
+
+/*___oOo___*/

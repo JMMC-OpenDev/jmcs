acc0aada73e44c72735e9e870c74c91fdfcccc51
gzins <gzins@jmmc.fr>
2005-02-11 09:37:23 +0000

    Added miscoDYN_BUF.h

--- misco/include/misco.h
+++ misco/include/misco.h
@@ -3,16 +3,19 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: misco.h,v 1.1 2005-02-10 13:34:06 sccmgr Exp $"
+ * "@(#) $Id: misco.h,v 1.2 2005-02-11 09:37:23 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/02/10 13:34:06  sccmgr
+ * Fix directory structure and add additional files
+ *
  ******************************************************************************/
 
 /**
  * \file
- * misco general header file.
+ * Main header file, grouping all the public headers of this module library.
  */
 
 /* The following piece of code alternates the linkage type to C for all 
@@ -27,7 +30,7 @@ extern "C" {
 /*
  * Local headers
  */
-
+#include "miscoDYN_BUF.h"
  
 
 #ifdef __cplusplus

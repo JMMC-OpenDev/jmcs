b3a050cf004d1a01f031d941102c60b0629710f4
gzins <gzins@jmmc.fr>
2005-02-15 10:45:22 +0000

    Added CVS log as file modification history

--- fnd/src/fndOBJECT.cpp
+++ fnd/src/fndOBJECT.cpp
@@ -1,20 +1,20 @@
 /*******************************************************************************
-* JMMC project
-*
-* "@(#) $Id: fndOBJECT.cpp,v 1.1 2004-12-05 18:47:14 gzins Exp $"
-*
-* who       when         what
-* --------  -----------  -------------------------------------------------------
-* gzins     22-Sep-2004  Created from VLT SW
-*
-*******************************************************************************/
+ * JMMC project
+ *
+ * "@(#) $Id: fndOBJECT.cpp,v 1.2 2005-02-15 10:45:22 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $* gzins     22-Sep-2004  Created from VLT SW
+ *
+ ******************************************************************************/
 
 /**
  * \file
  * Definition of fndOBJECT class.
  */
 
-static char *rcsId="@(#) $Id: fndOBJECT.cpp,v 1.1 2004-12-05 18:47:14 gzins Exp $"; 
+static char *rcsId="@(#) $Id: fndOBJECT.cpp,v 1.2 2005-02-15 10:45:22 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 

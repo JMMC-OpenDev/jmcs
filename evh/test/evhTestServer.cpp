72994ea3e6b91a3df6e3fdecefc44496aba99e00
gzins <gzins@jmmc.fr>
2005-05-19 15:22:14 +0000

    Updated to test message queue features

--- evh/test/evhTestServer.cpp
+++ evh/test/evhTestServer.cpp
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: evhTestServer.cpp,v 1.8 2005-02-03 12:44:39 gzins Exp $"
+ * "@(#) $Id: evhTestServer.cpp,v 1.9 2005-05-19 15:22:14 gzins Exp $"
  *
  * History:
  * --------
  * $Log: not supported by cvs2svn $
+ * Revision 1.8  2005/02/03 12:44:39  gzins
+ * Changed SUCCESS/FAILURE to mcsSUCCESS/mcsFAILURE
+ *
  * Revision 1.7  2005/01/29 20:18:20  gzins
  * Added errCloseStack when error occurs
  *
@@ -22,7 +25,7 @@
  * Test program for evhSERVER class.
  */
 
-static char *rcsId="@(#) $Id: evhTestServer.cpp,v 1.8 2005-02-03 12:44:39 gzins Exp $"; 
+static char *rcsId="@(#) $Id: evhTestServer.cpp,v 1.9 2005-05-19 15:22:14 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -62,16 +65,19 @@ public:
     virtual mcsCOMPL_STAT AppInit();
     virtual evhCB_COMPL_STAT SetupCB (msgMESSAGE &msg,void*);
     virtual evhCB_COMPL_STAT SetupReplyCB (msgMESSAGE &msg,void*);
+    virtual evhCB_COMPL_STAT WaitCB (msgMESSAGE &msg,void*);
     virtual evhCB_COMPL_STAT StdinCB (const int,void*);
 protected:
 
 private:
     msgMESSAGE _msg;
     evhINTERFACE _msgManagerIf;
+    evhINTERFACE _thisProcessIf;
 };
 
 // Constructor & destructor
-evhTEST::evhTEST():_msgManagerIf("Message Manager", "mgManager") 
+evhTEST::evhTEST():_msgManagerIf("Message Manager", "msgManager"),
+    _thisProcessIf("This process", "evhTestServer")
 {
 };
 evhTEST::~evhTEST() 
@@ -87,12 +93,17 @@ mcsCOMPL_STAT evhTEST::AppInit()
     evhCMD_CALLBACK cb1(this, (evhCMD_CB_METHOD)&evhTEST::SetupCB);
     AddCallback(key1, cb1);
 
-    // Attach callback to stdin 
-    int fd;
-    evhIOSTREAM_CALLBACK cb2(this, (evhIOSTREAM_CB_METHOD)&evhTEST::StdinCB);
-    fd = 0; 
-    evhIOSTREAM_KEY key2(fd);
+     // Attach callback to the WAIT command
+    evhCMD_KEY key2("WAIT");
+    evhCMD_CALLBACK cb2(this, (evhCMD_CB_METHOD)&evhTEST::WaitCB);
     AddCallback(key2, cb2);
+
+   // Attach callback to stdin 
+    int fd;
+    evhIOSTREAM_CALLBACK cb3(this, (evhIOSTREAM_CB_METHOD)&evhTEST::StdinCB);
+    fd = 0; 
+    evhIOSTREAM_KEY key3(fd);
+    AddCallback(key3, cb3);
     return mcsSUCCESS;
 }
 
@@ -100,19 +111,38 @@ mcsCOMPL_STAT evhTEST::AppInit()
 evhCB_COMPL_STAT evhTEST::SetupCB (msgMESSAGE &msg,void*)
 {
     logExtDbg("evhTEST::SetupCB ()"); 
-    evhCMD_CALLBACK cb1(this, (evhCMD_CB_METHOD)&evhTEST::SetupReplyCB);
-    _msg = msg;
+   
+    msgMESSAGE *newMsg = new msgMESSAGE(msg);
+    evhCMD_CALLBACK cb1(this, (evhCMD_CB_METHOD)&evhTEST::SetupReplyCB, newMsg);
     _msgManagerIf.Forward("PING", "", cb1);
 
     return evhCB_SUCCESS;
 };
 
 // Callback for SETUP command reply
-evhCB_COMPL_STAT evhTEST::SetupReplyCB (msgMESSAGE &msg,void*)
+evhCB_COMPL_STAT evhTEST::SetupReplyCB (msgMESSAGE &, void*msg)
 {
     logExtDbg("evhTEST::SetupReplyCB()"); 
-    SendReply(_msg);
+    ((msgMESSAGE *)msg)->SetBody("SETUP done.");
+    SendReply(*((msgMESSAGE *)msg));
+    delete((msgMESSAGE *)msg);
     return evhCB_DELETE;
+};
+
+// Callback for WAIT command
+evhCB_COMPL_STAT evhTEST::WaitCB (msgMESSAGE &msg,void*)
+{
+    logExtDbg("evhTEST::WaitCB ()"); 
+    
+    // Wait 5 seconds for the expected reply (it will never arrived because this
+    // command is sent to the process itself).
+    if (_thisProcessIf.Send("SETUP", "", 5000) == mcsFAILURE)
+    {
+        printf("Timeout expired ...\n");
+    }
+    SendReply(msg);
+
+    return evhCB_SUCCESS;
 };
 
 // Callback for stdin

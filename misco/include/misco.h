0b85a4b2273d98616d9e179469e938dc24d6b6c6
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-05-26 13:48:46 +0000

    This commit was manufactured by cvs2svn to create tag 'SEP2005/misco'.

---
+++ misco/include/misco.h
@@ -0,0 +1,35 @@
+#ifndef misco_H
+#define misco_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: misco.h,v 1.3 2005-04-08 12:41:19 gluck Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/02/11 09:37:23  gzins
+ * Added miscoDYN_BUF.h
+ *
+ * Revision 1.1  2005/02/10 13:34:06  sccmgr
+ * Fix directory structure and add additional files
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Main header file, grouping all the public headers of this module library.
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+/*
+ * Local headers
+ */
+#include "miscoDYN_BUF.h"
+ 
+#endif /*!misco_H*/
+
+/*___oOo___*/

c47a1190633a9f5e3474987d283e9f201d6c5495
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-06-02 13:09:28 +0000

    Fix directory structure and add additional files

---
+++ sdb/include/sdb.h
@@ -0,0 +1,27 @@
+#ifndef sdb_H
+#define sdb_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sdb.h,v 1.1 2005-06-02 13:09:28 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * sdb general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!sdb_H*/
+
+/*___oOo___*/
---
+++ sdb/include/sdbPrivate.h
@@ -0,0 +1,30 @@
+#ifndef sdbPrivate_H
+#define sdbPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sdbPrivate.h,v 1.1 2005-06-02 13:09:28 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * sdb private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "sdb"
+
+ 
+
+#endif /*!sdbPrivate_H*/
+
+/*___oOo___*/

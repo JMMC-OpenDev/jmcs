f07ed6960a1c111cd85fa733f31125b6667f45e7
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-06-17 08:37:21 +0000

    This commit was manufactured by cvs2svn to create tag 'FEB2006/fnd'.

---
+++ fnd/src/fndOBJECT.cpp
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: fndOBJECT.cpp,v 1.2 2005-02-15 10:45:22 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $* gzins     22-Sep-2004  Created from VLT SW
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Definition of fndOBJECT class.
+ */
+
+static char *rcsId="@(#) $Id: fndOBJECT.cpp,v 1.2 2005-02-15 10:45:22 gzins Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+
+/* 
+ * System Headers 
+ */
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "fndOBJECT.h"
+#include "fndPrivate.h"
+
+
+/*
+ * Class constructor
+ */
+fndOBJECT::fndOBJECT()
+{
+}
+
+/*
+ * Class destructor
+ */
+fndOBJECT::~fndOBJECT()
+{
+}
+
+/*___oOo___*/

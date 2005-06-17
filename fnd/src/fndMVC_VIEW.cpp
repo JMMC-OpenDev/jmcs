eae14e5f3dbecf77dafc1b447aeb31594f671f78
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-06-17 08:37:20 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2006/fnd'.

---
+++ fnd/src/fndMVC_VIEW.cpp
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: fndMVC_VIEW.cpp,v 1.2 2005-06-17 08:28:54 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/06/13 10:24:06  scetre
+ * Implementation of MVC base class
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ *  Definition of fndMVC_VIEW class.
+ */
+
+static char *rcsId="@(#) $Id: fndMVC_VIEW.cpp,v 1.2 2005-06-17 08:28:54 gzins Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+/*
+ * Local Headers 
+ */
+#include "fndMVC_VIEW.h"
+#include "fndPrivate.h"
+
+/**
+ * Class constructor
+ */
+fndMVC_VIEW::fndMVC_VIEW()
+{
+}
+
+/**
+ * Class destructor
+ */
+fndMVC_VIEW::~fndMVC_VIEW()
+{
+}
+
+/*___oOo___*/

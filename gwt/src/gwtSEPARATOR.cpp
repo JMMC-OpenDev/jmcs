f4e03b425ed33f78f3d37c33e7ac715c740c6d0c
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2005-08-30 06:55:45 +0000

    Append attributes used for debug

--- gwt/src/gwtSEPARATOR.cpp
+++ gwt/src/gwtSEPARATOR.cpp
@@ -1,11 +1,15 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: gwtSEPARATOR.cpp,v 1.1 2005-01-27 18:09:35 gzins Exp $"
+ * "@(#) $Id: gwtSEPARATOR.cpp,v 1.2 2005-08-30 06:55:45 mella Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/01/27 18:09:35  gzins
+ * Renamed .C to .cpp
+ * Added CVS loh as modification history.
+ *
  * mella     16-Sep-2004  Created
  *
  ******************************************************************************/
@@ -15,7 +19,7 @@
  * Definition of gwtSEPARATOR class.
  */
 
-static char *rcsId="@(#) $Id: gwtSEPARATOR.cpp,v 1.1 2005-01-27 18:09:35 gzins Exp $"; 
+static char *rcsId="@(#) $Id: gwtSEPARATOR.cpp,v 1.2 2005-08-30 06:55:45 mella Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -68,7 +72,9 @@ gwtSEPARATOR::~gwtSEPARATOR()
 string gwtSEPARATOR::GetXmlBlock()
 {
     logExtDbg("gwtSEPARATOR::GetXmlBlock()");
-    string s("<SEPARATOR />");
+    string s("<SEPARATOR ");
+    AppendXmlAttributes(s);
+    s.append("/>");
     return s;
 }
 

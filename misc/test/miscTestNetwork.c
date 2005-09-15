00ab1979e8282f2b3042475f58561032131aeaac
scetre <scetre@jmmc.fr>
2005-09-15 14:26:20 +0000

    Added miscGetHostByName in the miscNetwork file and test

--- misc/test/miscTestNetwork.c
+++ misc/test/miscTestNetwork.c
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: miscTestNetwork.c,v 1.5 2005-09-15 14:19:27 scetre Exp $"
+ * "@(#) $Id: miscTestNetwork.c,v 1.6 2005-09-15 14:26:20 scetre Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.5  2005/09/15 14:19:27  scetre
+ * Added miscGetHostByName in the miscNetwork file
+ *
  * Revision 1.4  2005/02/15 09:44:37  gzins
  * Added CVS log as file modification history
  *
@@ -13,7 +16,7 @@
  *
  ******************************************************************************/
 
-static char *rcsId="@(#) $Id: miscTestNetwork.c,v 1.5 2005-09-15 14:19:27 scetre Exp $"; 
+static char *rcsId="@(#) $Id: miscTestNetwork.c,v 1.6 2005-09-15 14:26:20 scetre Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 /* 
@@ -88,7 +91,8 @@ int main (int argc, char *argv[])
     mcsSTRING32 host;
     mcsSTRING32 hostIp; 
     strcpy(host, "vizier.u-strasbg.fr");
-    if (miscGetIpByName(hostIp, host) == mcsFAILURE)
+    //strcpy(host, "130.79.128.13");
+    if (miscGetHostByName(hostIp, host) == mcsFAILURE)
     {
         printf("mcsFAILURE.\n");
         errCloseStack();

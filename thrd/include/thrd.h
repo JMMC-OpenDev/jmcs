7eb42a8e8b0e511657a20e41de08fb4c2dc9ab94
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-10-20 15:01:20 +0000

    Fix directory structure and add additional files

---
+++ thrd/include/thrd.h
@@ -0,0 +1,27 @@
+#ifndef thrd_H
+#define thrd_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: thrd.h,v 1.1 2005-10-20 15:01:20 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * thrd general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!thrd_H*/
+
+/*___oOo___*/

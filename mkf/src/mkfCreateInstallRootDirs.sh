bc1a8606fa3c8be415781ab940c5c1103ecd1722
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-12-05 14:32:46 +0000

    This commit was manufactured by cvs2svn to create tag 'FEB2005/mkf'.

---
+++ mkf/src/mkfCreateInstallRootDirs.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: mkfCreateInstallRootDirs.sh,v 1.5 2005-12-05 14:32:45 gzins Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# Revision 1.4  2005/12/02 08:28:06  gzins
+# Updated to create directories according to the new MCS directory structure
+#
+# Revision 1.3  2005/02/15 08:40:15  gzins
+# Added CVS log as file modification history
+#
+# gzins     10-09-2004  Created
+# gzins     10-11-2004  Added creation of MCSDATA in MCSROOT
+#
+#*******************************************************************************
+# NAME
+#   mkfCreateInstallRootDirs- create/check standard installation directory
+#   structure
+#
+# SYNOPSIS
+#   mkfCreateInstallRootDirs [<root name>] 
+#
+# DESCRIPTION
+#   Utility used ot create new or missing part of installation directory
+#   structure for INTROOT (Integration area) or MCSROOT (MCS Root directory
+#   structure)
+#
+#       <root name> the name of the directory from which the directory structure
+#              starts. If not existing already, directory/ies are created.
+#   
+#   If <root name> is not given, this script simply print out the list of
+#   directories to be created.
+#
+# FILES
+#
+# ENVIRONMENT
+#
+# RETURN VALUES
+#
+# CAUTIONS
+#
+# SEE ALSO 
+#   MCS - Programming Standard
+#
+# BUGS    
+#
+#----------------------------------------------------------------------
+#
+
+# signal trap (if any)
+
+
+# Input parameters given should be 2 or 3 and in the correct format:
+if [ $# != 0 -a $# != 1 ]
+then 
+    echo -e "\n\tUsage: mkfCreateInstallRootDirs [<root name>]\n"
+    exit 1
+fi 
+
+# Define the content of each area 
+
+# Directories that shall be present in any area 
+BASE_DIR_LIST="config      \
+               doc         \
+               bin         \
+               include     \
+               lib         \
+               errors      \
+               man         \
+               man/man1    \
+               man/man2    \
+               man/man3    \
+               man/man4    \
+               man/man5    \
+               man/man6    \
+               man/man7    \
+               man/man8    \
+               templates   \
+               "
+
+# Additional directories for MCSDATA
+MCSDATA_DIR_LIST="log    \
+                  tmp    \
+                 "
+
+# If no root directory is given
+if [ $# = 0 ]
+then
+    # Print out directory list
+    for dir in $BASE_DIR_LIST
+    do
+        echo "$dir"
+    done
+    exit 0
+fi
+
+# Get the input parameters
+ROOT_NAME=$1
+
+# Check validity for parameter ROOT_NAME
+# Test existence of a ROOT_NAME file
+if [ -f $ROOT_NAME ]
+then
+    # a file called ROOT_NAME already exists
+    echo -e "\n ERROR: I cannot create the starting directory because a file"
+    echo "             called >>$ROOT_NAME<< already exists."
+    echo "             Use another name or remove the existing file"
+    echo ""
+    exit 1
+fi
+
+# Test existence of a ROOT_NAME directory
+if [ ! -d $ROOT_NAME ]
+then
+    # there is no ROOT_NAME directory
+    # Create ROOT_NAME directory
+    if mkdir $ROOT_NAME
+    then
+        # Creation succeeds
+        echo "   CREATED >>> |---$ROOT_NAME "
+    else
+        # Creation failed 
+        echo -e "\n ERROR: I cannot create the starting directory"
+        echo -e "          >>$ROOT_NAME<<"
+        echo "             Please fix the problem and try again."
+        echo ""
+        exit 1
+    fi
+fi
+
+
+# Check directory structure
+
+# If not already there, create all the needed subdirectories
+for dir in $BASE_DIR_LIST
+do
+    if [ ! -d $ROOT_NAME/$dir ]
+    then
+        mkdir $ROOT_NAME/$dir
+        echo "   CREATED >>>     |---$dir "
+    fi
+done
+
+
+# INTROOT case
+if [ "$ROOT_NAME" = "$INTROOT" ]
+then
+    for dir in $BASE_DIR_LIST
+    do
+        # directories must be writable by other developers
+        chmod 777 $ROOT_NAME/$dir
+    done
+# MCSROOT case
+else
+    # Test existence of a MCSDATA directory
+    if [ ! -d $MCSDATA ]
+    then
+        echo -e "\nCheck $MCSDATA directory structure"
+        # there is no MCSDATA directory
+        # Create MCSDATA directory
+        if mkdir $MCSDATA
+        then
+            # Creation succeeds
+            echo -e "   CREATED >>> |---$MCSDATA "
+        else
+            # Creation failed 
+            echo -e "\n ERROR: I cannot create the MCSDATA directory"
+            echo -e "          >>$MCSDATA<<"
+            echo "             Please fix the problem and try again."
+            echo ""
+            exit 1
+        fi
+    fi
+    # Create MCSDATA additional directories
+    for dir in $MCSDATA_DIR_LIST
+    do
+        if [ ! -d $MCSDATA/$dir ]
+        then
+            mkdir $MCSDATA/$dir
+            echo "   CREATED >>>     |---$dir "
+        fi
+        # directories must be writable by all
+        chmod 777 $MCSDATA/$dir
+    done
+fi
+
+#
+#___oOo___
---
+++ mkf/test/mkfTest.c
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * JMMC project
+ * 
+ * "@(#) $Id: mkfTest.c,v 1.2 2005-02-15 09:12:26 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * gzins     09-Sep-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the class file, which ends at this dot.
+ * 
+ * OPTIONAL detailed description of the class file follows here.
+ *
+ * \usedfiles
+ * OPTIONAL. If files are used, for each one, name, and usage description.
+ * \filename fileName1 :  usage description of fileName1
+ * \filename fileName2 :  usage description of fileName2
+ *
+ * \n
+ * \env
+ * OPTIONAL. If needed, environmental variables accessed by the program. For
+ * each variable, name, and usage description, as below.
+ * \envvar envVar1 :  usage description of envVar1
+ * \envvar envVar2 :  usage description of envVar2
+ * 
+ * \n
+ * \warning OPTIONAL. Warning if any (software requirements, ...)
+ *
+ * \n
+ * \ex
+ * OPTIONAL. Code example if needed
+ * \n Brief example description.
+ * \code
+ * Insert your code example here
+ * \endcode
+ *
+ * \sa OPTIONAL. See also section, in which you can refer other documented
+ * entities. Doxygen will create the link automatically.
+ * \sa <entity to refer>
+ * 
+ * \bug OPTIONAL. Bugs list if it exists.
+ * \bug For example, description of the first bug
+ * \bug For example, description of the second bug
+ * 
+ * \todo OPTIONAL. Things to forsee list, if needed. 
+ * \todo Action1 : description of action 1.
+ * 
+ */
+
+static char *rcsId="@(#) $Id: mkfTest.c,v 1.2 2005-02-15 09:12:26 gzins Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+
+/* 
+ * System Headers
+ */
+#include <stdio.h>
+
+
+void mkfTestFunction(void )
+{
+    printf("mkfTestFunction()\n");
+}
+
+
+
+/*___oOo___*/

b3f714335c809390b162863f34aa7782962ce0c0
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2005-12-06 09:08:47 +0000

    This commit was manufactured by cvs2svn to create tag 'FEB2005/mcscfg'.

---
+++ mcscfg/src/mcscfgShow.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: mcscfgShow.sh,v 1.1 2005-12-06 07:38:59 gzins Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+#
+#*******************************************************************************
+
+#/**
+# \file
+# This script show the current MCS configuration. 
+#
+# \synopsis
+# mcscfgShow
+#
+# */
+
+echo "MCSTOP     = $MCSTOP";
+echo "MCSDATA    = $MCSDATA";
+echo "MCSRELEASE = $MCSRELEASE";
+echo "MCSROOT    = $MCSROOT";
+echo "MCSENV     = $MCSENV";
+echo "INTROOT    = $INTROOT"
+
+#___oOo___

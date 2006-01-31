b213120d310399bb726b7ce1dd8c88f01b5cdaaa
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-01-31 01:13:28 +0000

    Fix directory structure and add additional files

---
+++ mth/include/mth.h
@@ -0,0 +1,27 @@
+#ifndef mth_H
+#define mth_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: mth.h,v 1.1 2006-01-31 01:13:28 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * mth general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!mth_H*/
+
+/*___oOo___*/

3b2037eb2ce8f99581a707afe017b01da7f2774f
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-03-07 14:22:23 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2006/ctoo'.

---
+++ ctoo/src/ctooGetCode.sh
@@ -0,0 +1,140 @@
+#! /bin/bash
+
+#******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: ctooGetCode.sh,v 1.6 2006-03-07 14:22:22 mella Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# Revision 1.5  2005/04/11 07:52:50  gluck
+# Fix problem report 3: directories creation with correct header file for C++ and template names update
+#
+# Revision 1.4  2005/01/24 15:47:51  gluck
+# Bug correction for log message automatic insertion ($Log: not supported by cvs2svn $
+# Bug correction for log message automatic insertion (Revision 1.5  2005/04/11 07:52:50  gluck
+# Bug correction for log message automatic insertion (Fix problem report 3: directories creation with correct header file for C++ and template names update
+# Bug correction for log message automatic insertion ()
+#
+# lgluck    14/05/04    Created
+#
+#******************************************************************************
+
+#/**
+# \file
+# Interactive script to get standard templates for coding.
+#
+# \synopsis
+# ctooGetCode 
+#
+# \details
+# Interactive utility to get template files for code. Following templates are
+# available :
+#   -# c-main
+#   -# c-procedure
+#   -# h-file
+#   -# c++-small-main
+#   -# c++-class-file
+#   -# c++-h-file
+#   -# script
+#   -# Makefile
+#   -# java-class
+# 
+# \sa ctooGetTemplateForCoding.sh, ctooGetTemplateFile.sh, ctooGetTemplate.sh
+#  
+# \n 
+# 
+# */
+
+
+# signal trap (if any)
+
+
+# Print out the menu
+cat <<xyz
+--------------------------------------------------------------------------------
+This menu allows you to copy one of the available templates with a filename of
+your choice in the current directory (the output file must not already exist).
+
+Templates are available for:
+
+        1- c-main                         7- script                
+        2- c-procedure                    
+        3- c-h-file                       8- Makefile
+                                          
+        4- c++-small-main                 9- java-main
+        5- c++-class-definition-file     10- java-class
+        6- c++-class-interface-file      11- java-interface
+xyz
+
+# Propose the user to enter his choice
+echo -e "\n-> Enter the number corresponding to the template type you need or"
+echo -e "   press <Enter> to exit: \c"
+
+# Read the choice
+read choice
+
+
+# Treat the choice
+
+# Test if the choice is not empty
+if test -n "$choice"
+then
+    # Examine the choice
+    case $choice in
+        1)  # c-main choice
+            ctooGetTemplateForCoding c-main
+            ;;
+        
+        2)  # c-procedure choice
+            ctooGetTemplateForCoding c-procedure
+            ;;
+        
+        3)  # h-file choice
+            ctooGetTemplateForCoding c-h-file
+            ;;
+        
+        4)  # c++-small-main choice
+            ctooGetTemplateForCoding c++-small-main 
+            ;;
+        
+        5)  # c++-class-file choice
+            ctooGetTemplateForCoding c++-class-definition-file 
+            ;;
+        
+        6)  # c++-h-file choice
+            ctooGetTemplateForCoding c++-class-interface-file
+            ;;
+        
+        7)  # script file choice
+            ctooGetTemplateForCoding script
+            ;;
+        
+        8)  # Makefile choice
+            ctooGetTemplateForCoding Makefile
+            ;;
+ 
+        9)  # java-main choice
+            ctooGetTemplateForCoding java-main
+            ;;
+        
+        10)  # java-class choice
+            ctooGetTemplateForCoding java-class
+            ;;
+        
+        11)  # java-interface choice
+            ctooGetTemplateForCoding java-interface
+            ;;
+        
+        *) echo "ERROR : invalid choice";;
+        
+    esac
+
+else
+    # <Enter> was pressed
+    exit
+fi
+
+#
+# ___oOo___
---
+++ ctoo/src/ctooGetModuleName.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: ctooGetModuleName.sh,v 1.8 2005-01-24 15:47:51 gluck Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# gluck     14-Sep-2004  Created
+# gzins     09-Dec-2004  Updated to print module name on stdout instead of
+#                        returning value in shell variable.
+#
+#*******************************************************************************
+
+#/**
+# \file
+# Get module name.
+#
+# \synopsis
+# ctooGetModuleName 
+#
+# \details
+# Get module name, extracting it from a file, normaly existing in each module
+# directory  structure.
+# 
+# \usedfiles
+# \filename moduleDescription.xml : file used to extract module name, located
+# in the doc subdirectory of a module directory structure
+#
+# \n
+# \warning This script should be executed from one of the first level
+# subdirectory of a module structure.
+#
+# \n
+# \sa ctooGetPrivateHeaderFile.sh, ctooGetTemplateForCoding.sh
+# 
+# \n
+#
+# */
+
+
+# signal trap (if any)
+
+
+# test doc directory existence
+dir=../doc
+if [ ! -d $dir ]
+then
+    echo "ERROR - ctooGetmoduleName: $dir directory does not exist." >&2
+    echo "        please check your module directory structure" >&2
+    exit 1
+fi
+
+# test moduledescription.xml file existence
+file=../doc/moduleDescription.xml
+if [ ! -f $file ]
+then
+    echo "ERROR - ctooGetmoduleName: $file file does not exist." >&2
+    echo "        please check your module directory structure"  >&2
+    exit 1
+fi
+    
+# get line containing the name of the module in moduledescription.xml file
+# => <module name="modulename">
+lineContainingModuleName=`grep "^<module name=\".*\">$" ../doc/moduleDescription.xml`
+
+# trim left the above extracted line => modulename">
+rightSideOfModuleName=${lineContainingModuleName##<module name=\"}
+
+# trim right the above extracted string to get module name => modulename
+ROOT_NAME=${rightSideOfModuleName%%\">}
+
+# If no module name can be found => error
+if [ "$ROOT_NAME" = "" ]
+then
+    echo "ERROR - ctooGetModuleName: Could not get module name." >&2
+    exit 1
+else
+    # Print out module name
+    echo $ROOT_NAME
+    exit 0
+fi
+
+#___ooo___

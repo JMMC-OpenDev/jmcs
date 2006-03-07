cf5634d9fb7062b4dbabacaaaf85aeea3c2b8f09
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-03-07 09:55:50 +0000

    Fix directory structure and add additional files

---
+++ modjava/include/modjavaPrivate.h
@@ -0,0 +1,30 @@
+#ifndef modjavaPrivate_H
+#define modjavaPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: modjavaPrivate.h,v 1.1 2006-03-07 09:55:50 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * modjava private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "modjava"
+
+ 
+
+#endif /*!modjavaPrivate_H*/
+
+/*___oOo___*/

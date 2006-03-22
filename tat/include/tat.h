5209ad0e6f71b26541af6790b6f31a1eb94ca77b
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-03-22 06:58:28 +0000

    Fix directory structure and add additional files

---
+++ tat/include/tat.h
@@ -0,0 +1,27 @@
+#ifndef tat_H
+#define tat_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: tat.h,v 1.1 2006-03-22 06:58:28 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * tat general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!tat_H*/
+
+/*___oOo___*/

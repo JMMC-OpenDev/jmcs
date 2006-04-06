a9dcf7f6d72657d6f634ec6b8d2967ed2eaee375
sly74fr <slymacdev@free.fr>
2006-04-06 14:35:39 +0000

    Streamlined class documentation header

--- src/fr/jmmc/mcs/util/ColorEncoder.java
+++ src/fr/jmmc/mcs/util/ColorEncoder.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: ColorEncoder.java,v 1.2 2006-04-06 14:23:08 mella Exp $"
+ * "@(#) $Id: ColorEncoder.java,v 1.3 2006-04-06 14:35:39 lafrasse Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2006/04/06 14:23:08  mella
+ * *** empty log message ***
+ *
  * Revision 1.1  2006/03/31 14:29:53  mella
  * First Revision
  *
@@ -18,9 +21,6 @@ import java.awt.Color;
 
 /**
  * DOCUMENT ME!
- *
- * @author $author$
- * @version $Revision: 1.2 $
  */
 public class ColorEncoder
 {

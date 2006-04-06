a01e9b1ec8ea9f4bd56c521c81a2ea7dbf78d5a1
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-04-06 08:20:07 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2006/msg'.

---
+++ msg/src/msgManager.cpp
@@ -0,0 +1,143 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: msgManager.cpp,v 1.5 2005-02-04 15:57:06 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.4  2005/01/24 15:02:47  gzins
+ * Added CVS logs as modification history
+ *
+ * gzins     07-Jan-2005  Changed SUCCESS/FAILURE to mcsSUCCESS/mcsFAILURE 
+ * lafrasse  15-Dec-2004  Re-added Doxygen documentation from the npw removed
+ *                        msgManager.c
+ * gzins     06-Dec-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * \e \<msgManager\> - MCS inter-process communication server.
+ *
+ * \b Synopsis:\n
+ * \e \<msgManager\>
+ *
+ * \b Details:\n
+ * \e \<msgManager\> is the communication server doing message forwarding
+ * between processes. Each process connected to this server can send messages to
+ * (and receive messages from) the other connected processes.
+ */
+
+static char *rcsId="@(#) $Id: msgManager.cpp,v 1.5 2005-02-04 15:57:06 lafrasse Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+
+/* 
+ * System Headers 
+ */
+#include <stdlib.h>
+#include <iostream>
+#include <signal.h>
+
+/**
+ * \namespace std
+ * Export standard iostream objects (cin, cout,...).
+ */
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "msgMANAGER.h"
+#include "msgPrivate.h"
+
+/*
+ * Local Variables
+ */
+static msgMANAGER *msgManager=NULL;
+
+/* 
+ * Signal catching functions  
+ */
+
+/**
+ * Trap certain system signals.
+ *
+ * Particulary manages 'broken pipe' and 'dead process' signals, plus the end
+ * signal (i.e. CTRL-C).
+ *
+ * \param signalNumber the system signal to be trapped
+ */
+
+void msgSignalHandler (int signalNumber)
+{
+    logInfo("Received %d system signal...", signalNumber);
+    if (signalNumber == SIGPIPE)
+    {
+        return;
+    }
+    logInfo("%s program aborted.", mcsGetProcName());
+    delete (msgManager);
+    exit (EXIT_SUCCESS);
+}
+
+/* 
+ * Main
+ */
+
+int main(int argc, char *argv[])
+{
+    // Message manager instance
+    msgManager = new msgMANAGER;
+    
+    /* Init system signal trapping */
+    if (signal(SIGINT, msgSignalHandler) == SIG_ERR)
+    {
+        logError("signal(SIGINT, ...) function error");
+        exit(EXIT_FAILURE);
+    }
+    if (signal (SIGTERM, msgSignalHandler) == SIG_ERR)
+    {
+        logError("signal(SIGTERM, ...) function error");
+        exit(EXIT_FAILURE);
+    }
+    if (signal (SIGPIPE, msgSignalHandler) == SIG_ERR)
+    {
+        logError("signal(SIGPIPE, ...) function error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialization
+    if (msgManager->Init(argc, argv) == mcsFAILURE)
+    {
+        // Close error stack
+        errCloseStack();
+        exit (EXIT_FAILURE);
+    }
+
+    // Enter in main loop
+    if (msgManager->MainLoop() == mcsFAILURE)
+    {
+        // Error handling if necessary
+        
+        // Exit from the application with mcsFAILURE
+        exit (EXIT_FAILURE);
+    }
+
+    // Exit from the application with mcsSUCCESS
+    delete (msgManager);
+    exit (EXIT_SUCCESS);
+}
+
+
+/*___oOo___*/
---
+++ msg/test/msgTestSocketClientTimeOut.cpp
@@ -0,0 +1,131 @@
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: msgTestSocketClientTimeOut.cpp,v 1.2 2005-09-12 13:08:05 scetre Exp $"
+*
+* History
+* -------
+* $Log: not supported by cvs2svn $
+* Revision 1.1  2005/09/12 13:06:49  scetre
+* Added test on Socket client with time out when receiving CDS data
+*
+* Revision 1.5  2005/04/22 09:25:09  mella
+* Replace FAILURE by mcsFAILURE
+*
+* Revision 1.4  2005/02/09 16:41:02  lafrasse
+* Changed the way mesage are displayed (now use msgMESSAGE 'operator<<' instead of 'Display')
+*
+* Revision 1.3  2005/02/04 15:57:06  lafrasse
+* Massive documentation review an refinment (also added automatic CVS log inclusion in every files)
+*
+* lafrasse  14-Dec-2004  Added error code management
+* lafrasse  24-Nov-2004  Created
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Small test client based on msgSOCKET_CLIENT.
+ *
+ * \synopsis
+ * \<msgTestSocketClient\>
+ */
+
+static char *rcsId="@(#) $Id: msgTestSocketClientTimeOut.cpp,v 1.2 2005-09-12 13:08:05 scetre Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+
+/* 
+ * System Headers 
+ */
+#include <stdlib.h>
+#include <iostream>
+
+/**
+ * \namespace std
+ * Export standard iostream objects (cin, cout,...).
+ */
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "msg.h"
+#include "msgPrivate.h"
+
+
+/* 
+ * Main
+ */
+int main(int argc, char *argv[])
+{
+    msgMESSAGE msg;
+    static char * buffer = "GET http://vizier.u-strasbg.fr/viz-bin/asu-xml?-source=II/225/catalog\n";
+    string buffer2;
+
+    // Initialize MCS services
+    if (mcsInit(argv[0]) == mcsFAILURE)
+    {
+        // Exit from the application with FAILURE
+        exit (EXIT_FAILURE);
+    }
+
+    int portNumber = 80;
+    // vizier CDS IP
+    const std::string host = "130.79.128.13";
+    cout << "Client creating a new msgSOCKET_CLIENT object ... ";
+    msgSOCKET_CLIENT clientSocket;
+    cout << "OK" << endl;
+    cout << "Client connecting to server on , vizier.u-strasbg.fr port '" << portNumber
+        << "'... ";
+    if (clientSocket.Open(host, portNumber) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl << endl;
+
+    cout << "Client sending   '" << buffer << "' to server ... ";
+    if (clientSocket.Send(buffer) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl;
+    cout << "Client receiving '";
+    if (clientSocket.Receive(buffer2, 1000) == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    cout << buffer2  ;
+    
+    // Close MCS services
+    mcsExit();
+    
+    // Exit from the application with SUCCESS
+    exit (EXIT_SUCCESS);
+
+// If an error occured, show the error stack and exit
+errCond:
+    if (errStackIsEmpty() == mcsFALSE)
+    {
+        errDisplayStack();
+        errCloseStack();
+    }
+
+    mcsExit();
+    exit(EXIT_FAILURE);
+}
+
+
+/*___oOo___*/
+

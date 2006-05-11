b1038d6d347f0fa85f8e3fcde2fa7a4eee9b1059
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-05-11 13:05:04 +0000

    This commit was manufactured by cvs2svn to create tag 'FEB2011/err'.

---
+++ err/src/errLocalStackIsEmpty_L.c
@@ -0,0 +1,71 @@
+/*******************************************************************************
+* JMMC project
+*
+* History
+* -------
+* $Log: not supported by cvs2svn $
+* Revision 1.5  2005/06/01 13:23:49  gzins
+* Changed logExtDbg to logTrace
+*
+* Revision 1.4  2005/02/15 08:09:35  gzins
+* Added file description
+*
+* Revision 1.3  2005/01/27 14:10:50  gzins
+* Changed errERROR to errERROR_STACK
+*
+* Revision 1.2  2005/01/24 14:49:18  gzins
+* Used CVS log as modification history
+*
+* gzins     17-Jun-2004  completed implementation
+* berezne   02-Jun-2004  created
+*
+*-----------------------------------------------------------------------------*/
+
+/**
+ * \file
+ * Definition of errLocalStackIsEmpty function.
+ */
+
+static char *rcsId __attribute__ ((unused)) = "@(#) $Id: errLocalStackIsEmpty_L.c,v 1.6 2006-01-10 14:40:39 mella Exp $"; 
+
+
+/* 
+ * System Headers
+ */
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Common Software Headers
+ */
+#include "mcs.h"
+#include "log.h"
+
+/*
+ * Local Headers
+ */
+#include "err.h"
+#include "errPrivate.h"
+
+/**
+ * Checks is the error stack is empty 
+ * 
+ * \param error Error structure.
+ *
+ * \return mcsTRUE if the error stack is empty, otherwise mcsFALSE.
+ */
+mcsLOGICAL errLocalStackIsEmpty (errERROR_STACK          *error)
+{
+    logTrace("errLocalStackIsEmpty()");
+
+    /* If error stack is not initialised, do it */
+    if (error->thisPtr != error)
+    {
+        errResetLocalStack(error);
+    } 
+
+    /* Returns empty flag */
+    return (error->stackEmpty);
+}
+/*___oOo___*/
+

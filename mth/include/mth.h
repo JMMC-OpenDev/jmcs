94b39460cd45da7619965c3de0a5fe161d8f8f10
gzins <gzins@jmmc.fr>
2006-06-09 12:16:10 +0000

    Moved mthNORM to mthGAUSSIAN_RANDOM

--- mth/include/mth.h
+++ mth/include/mth.h
@@ -3,11 +3,15 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: mth.h,v 1.3 2006-06-02 13:33:03 lsauge Exp $"
+ * "@(#) $Id: mth.h,v 1.4 2006-06-09 12:16:10 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2006/06/02 13:33:03  lsauge
+ * Integrate mthGAUSSIAN_RANDOM.
+ * mth.h became mthBesselK.h, mthGAUSSIAN_RANDOM.h created and mth.h creted, integrating the 2 previous header files
+ *
  * Revision 1.2  2006/01/31 01:21:22  gzins
  * Added Bessel function declarations
  *
@@ -22,7 +26,7 @@
  */
 
 #include "mthBesselK.h"
-#include "mthNORM.h"
+#include "mthGAUSSIAN_RANDOM.h"
 
 #endif /*!mth_H*/
 

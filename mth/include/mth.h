38dc7d802b268527057e3c859a6e33ccc3bdb0a3
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-06-09 12:22:20 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2007/mth'.

---
+++ mth/include/mth.h
@@ -0,0 +1,33 @@
+#ifndef mth_H
+#define mth_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: mth.h,v 1.4 2006-06-09 12:16:10 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.3  2006/06/02 13:33:03  lsauge
+ * Integrate mthGAUSSIAN_RANDOM.
+ * mth.h became mthBesselK.h, mthGAUSSIAN_RANDOM.h created and mth.h creted, integrating the 2 previous header files
+ *
+ * Revision 1.2  2006/01/31 01:21:22  gzins
+ * Added Bessel function declarations
+ *
+ * Revision 1.1  2006/01/31 01:13:28  sccmgr
+ * Fix directory structure and add additional files
+ *
+ ******************************************************************************/
+
+/**
+ * @file
+ * Mathematical header file.
+ */
+
+#include "mthBesselK.h"
+#include "mthGAUSSIAN_RANDOM.h"
+
+#endif /*!mth_H*/
+
+/*___oOo___*/

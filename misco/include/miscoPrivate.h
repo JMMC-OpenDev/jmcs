219e3432090d8312e099e91ee1307ea9a01e3fe7
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2007-02-22 12:23:09 +0000

    This commit was manufactured by cvs2svn to create tag 'MAY2009/misco'.

---
+++ misco/include/miscoPrivate.h
@@ -0,0 +1,41 @@
+#ifndef miscoPrivate_H
+#define miscoPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: miscoPrivate.h,v 1.1 2005-02-10 13:34:06 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * misco private header file.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "misco"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!miscoPrivate_H*/
+
+/*___oOo___*/

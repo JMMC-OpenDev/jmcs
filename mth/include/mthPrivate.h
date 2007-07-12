92936665d5dd8da490030159ee3e33e1216e1f59
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2007-07-12 15:16:39 +0000

    This commit was manufactured by cvs2svn to create tag 'AUG2009/mth'.

---
+++ mth/include/mthPrivate.h
@@ -0,0 +1,30 @@
+#ifndef mthPrivate_H
+#define mthPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: mthPrivate.h,v 1.1 2006-01-31 01:13:28 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * mth private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "mth"
+
+ 
+
+#endif /*!mthPrivate_H*/
+
+/*___oOo___*/

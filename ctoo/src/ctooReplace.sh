0e45a9fce74babc09303cb15094a0b97f3e83b44
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2007-11-06 05:29:16 +0000

    Use store in place option for sed Remove unused PATH redefinition

--- ctoo/src/ctooReplace.sh
+++ ctoo/src/ctooReplace.sh
@@ -3,13 +3,19 @@
 #*******************************************************************************
 # JMMC project
 #
-# "@(#) $Id: ctooReplace.sh,v 1.5 2006-01-09 13:00:46 mella Exp $"
+# "@(#) $Id: ctooReplace.sh,v 1.6 2007-11-06 05:29:16 mella Exp $"
 #
 # History
 # -------
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2006/01/09 13:00:46  mella
+# Updated documetation
+#
 # Revision 1.4  2005/01/24 15:47:51  gluck
-# Bug correction for log message automatic insertion ($Log: not supported by cvs2svn $)
+# Bug correction for log message automatic insertion ($Log: not supported by cvs2svn $
+# Bug correction for log message automatic insertion (Revision 1.5  2006/01/09 13:00:46  mella
+# Bug correction for log message automatic insertion (Updated documetation
+# Bug correction for log message automatic insertion ()
 #
 # lafrasse  29/07/04    Forked form VLT Software
 # lafrasse  11/12/04    Changed documentation from VLT Software style to Doxygen
@@ -18,58 +24,56 @@
 
 #/**
 # \file
-#	ctooReplace - replace strings in files
+#     ctooReplace - replace strings in files
 #
 # \synopsis
-#	ctooReplace \<old-string\> \<new-string\> \<file1\> [\<file2\> ...]
+#     ctooReplace \<old-string\> \<new-string\> \<file1\> [\<file2\> ...]
 #
 # \details
-#	ctooReplace replaces \<old-string\> with \<new-string\> in given files.
+#       ctooReplace replaces \<old-string\> with \<new-string\> in given files.
 #
 #   This script returns 0 is everything was OK, otherwise 1 if any error
 #   occrured.
 #
 # \usedfiles
-#	All files in the argument list will be read and modified.
-#	All originals will be moved to backup files ('~' appended).
+#       All files in the argument list will be read and modified.
+#       All originals will be moved to backup files ('~' appended).
 #
 # \warning
-#	For security make a backup of all given files before!
-#	Do not give regular expressions for \<old-prefix\> or \<new-prefix\>.
+#       For security make a backup of all given files before!
+#   Do not give regular expressions for \<old-string> or \<new-string\>. 
+#   
 #   If your expression does not make any changes, you may have to unescape some
-#   special characters. 
+#   special characters.
 #   We succeeded to replaced some strings that contains * using \*.
 #
 # \n
 # \ex
-#	Replace `foo' occurences by `bar' in all files of the `mod' module:
+#       Replace `foo' occurences by `bar' in all files of the `mod' module:
 #
-#		cd mod
-#		ctooReplace foo bar `find . -type f`
+#               cd mod
+#               ctooReplace foo bar `find . -type f`
 #
 # \sa
-#	sed(1), find(1)
+#   sed(1), find(1)
 #
 # */
-
-localpath=`dirname $0`
-PATH=/bin:/usr/bin:$localpath
 
 case $# in
     0|1|2) echo $0' (Usage): ctooReplace str1 str2 files' 1>&2; exit 1
 esac
 
-# Old and new module prefixes:
+# store old and new string prefixes:
 left="$1"; right="$2"; shift; shift
 
-for i 
-do 
-    if test -f $i 
+# loop over rest of command line arguments (it should be filenames)
+# and store modifications in place with a backup file suffixed by ~~
+for i
+do
+    if test -f $i
     then
         echo "ctooReplace: $i"
-        sed "s@$left@$right@g" $i > $i~~
-        mv $i $i~
-        mv $i~~ $i
+        sed -i~~ "s@$left@$right@g" $i 
     fi
 done
 

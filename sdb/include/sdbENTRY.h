de4d20bf9b459ea405ee659309f0f091562136cb
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2007-11-07 13:49:11 +0000

    This commit was manufactured by cvs2svn to create tag 'AUG2009/sdb'.

---
+++ sdb/include/sdbENTRY.h
@@ -0,0 +1,76 @@
+#ifndef sdbENTRY_H
+#define sdbENTRY_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sdbENTRY.h,v 1.1 2007-10-26 13:25:26 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * Declaration of sdbENTRY class.
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+
+/*
+ * MCS header
+ */
+#include "mcs.h"
+#include "thrd.h"
+
+
+/*
+ * Class declaration
+ */
+
+/**
+ * Status database class.
+ *
+ * This class provides protected method to read and write a database entry; i.e.
+ * includes mutual-exclusion mechanism for readind and writing operations.
+ */
+class sdbENTRY
+{
+public:
+    // Class constructor
+    sdbENTRY();
+
+    // Class destructor
+    virtual ~sdbENTRY();
+
+    mcsCOMPL_STAT  Init    (void);
+    mcsCOMPL_STAT  Destroy (void);
+
+    mcsCOMPL_STAT  Write   (const char*       message);
+    mcsCOMPL_STAT  Read    (      char*       message,
+                                  mcsLOGICAL  waitNewMessage = mcsFALSE,
+                                  mcsINT32    timeoutInMs = -1);
+
+    mcsLOGICAL     IsInit  (void);
+
+protected:
+    
+private:
+    // Declaration of copy constructor and assignment operator as private
+    // methods, in order to hide them from the users.
+    sdbENTRY(const sdbENTRY&);
+    sdbENTRY& operator=(const sdbENTRY&);
+
+    thrdMUTEX      _mutex;
+    mcsLOGICAL     _initSucceed;
+
+    mcsSTRING256   _buffer;
+    mcsLOGICAL     _isNewMessage;
+};
+
+#endif /*!sdbENTRY_H*/
+
+/*___oOo___*/
---
+++ sdb/include/sdbPrivate.h
@@ -0,0 +1,33 @@
+#ifndef sdbPrivate_H
+#define sdbPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sdbPrivate.h,v 1.2 2005-12-20 13:52:34 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/06/02 13:09:28  sccmgr
+ * Fix directory structure and add additional files
+ *
+ ******************************************************************************/
+
+/**
+ * @file
+ * sdb private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "sdb"
+
+ 
+
+#endif /*!sdbPrivate_H*/
+
+/*___oOo___*/

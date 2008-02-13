cd84b2f830ebdebfc07cae38ce67e13fabb81f23
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2008-02-13 08:00:04 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2007/env'.

---
+++ env/include/env.h
@@ -0,0 +1,25 @@
+#ifndef env_H
+#define env_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: env.h,v 1.2 2005-02-13 16:53:13 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * lafrasse  07-Dec-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * This header contains all the 'env' library objects includes.
+ *
+ */
+
+#include "envLIST.h"
+
+#endif /*!env_H*/
+
+/*___oOo___*/

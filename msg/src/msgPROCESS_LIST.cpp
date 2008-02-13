5a85b5c074b17de1cc9dde50777f11292af97a4d
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2008-02-13 08:07:45 +0000

    This commit was manufactured by cvs2svn to create tag 'APR2007/msg'.

---
+++ msg/src/msgPROCESS_LIST.cpp
@@ -0,0 +1,265 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: msgPROCESS_LIST.cpp,v 1.9 2006-05-11 13:04:56 mella Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.8  2005/02/09 16:38:29  lafrasse
+ * Changed method prototypes to use as much 'const' parameters as possible
+ *
+ * Revision 1.7  2005/02/04 15:57:06  lafrasse
+ * Massive documentation review an refinment (also added automatic CVS log inclusion in every files)
+ *
+ * Revision 1.6  2005/01/29 20:01:00  gzins
+ * Minor change in code example
+ *
+ * Revision 1.5  2005/01/24 15:02:47  gzins
+ * Added CVS logs as modification history
+ *
+ * gzins     07-Jan-2005  Changed SUCCESS/FAILURE to mcsSUCCESS/mcsFAILURE 
+ * gzins     14-Dec-2004  Minor documentation changes 
+ * gzins     08-Dec-2004  Added descriptor argument to GetProcess()
+ * gzins     06-Dec-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Storage class used to hold all the msgPROCESS objects of the \<msgManager\>.
+ *
+ * \sa msgPROCESS_LIST
+ */
+
+static char *rcsId __attribute__ ((unused)) ="@(#) $Id: msgPROCESS_LIST.cpp,v 1.9 2006-05-11 13:04:56 mella Exp $";
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+/*
+ * Local Headers 
+ */
+#include "msgPROCESS_LIST.h"
+#include "msgPrivate.h"
+
+
+/**
+ * Class constructor
+ */
+msgPROCESS_LIST::msgPROCESS_LIST()
+{
+}
+
+/**
+ * Class destructor
+ */
+msgPROCESS_LIST::~msgPROCESS_LIST()
+{
+}
+
+
+/*
+ * Public methods
+ */
+
+/**
+ * Return whether the list is empty or not.  
+ *
+ * \return mcsTRUE if the list is empty, mcsFALSE otherwise
+ */
+mcsLOGICAL msgPROCESS_LIST::IsEmpty(void)
+{
+    logExtDbg("msgPROCESS_LIST::IsEmpty()");
+    if (_processList.empty() == true)
+    {
+        return mcsTRUE;
+    }
+    else
+    {
+        return mcsFALSE;
+    }
+}
+
+/**
+ * Return the number of processes currently stored in the list.
+ *
+ * \return the number of processes in the list
+ */
+mcsUINT32 msgPROCESS_LIST::Size(void) 
+{
+    return _processList.size();
+}
+
+/**
+ * Add a process at the end of the list.
+ *
+ * \param process the msgPROCESS object to be added to the list
+ *
+ * \return always mcsSUCCESS
+ */
+mcsCOMPL_STAT msgPROCESS_LIST::AddAtTail(msgPROCESS *process)
+{
+    logExtDbg("msgPROCESS_LIST::AddAtTail()");
+
+    // Put the process at the end of the list
+    _processList.push_back(process);
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Returns the next process in the list.
+ *
+ * This method returns a pointer on the next element of the list. If \em init
+ * equals mcsTRUE, returns a pointer on the first element of the list.
+ * 
+ * This method can be used to move forward in the list, as shown below:
+ * \code
+ *     for (unsigned int el = 0; el < processList.Size(); el++)
+ *     {
+ *         processList.GetNextProcess((mcsLOGICAL)(el==0))->Close();
+ *     }
+ * \endcode
+ *
+ * \param init if equals to mcsTRUE, return the first element of the list,
+ * otherwise return the one next to the current internal position
+ *
+ * \return a pointer on the next element of the list, or NULL if the end of the
+ * list is reached
+ */
+msgPROCESS *msgPROCESS_LIST::GetNextProcess(const mcsLOGICAL init) 
+{
+    logExtDbg("msgPROCESS_LIST::GetNextProcess()");
+    
+    // If init is TRUE
+    if (init == mcsTRUE)
+    {
+        // Get First element of the list
+        _processIterator = _processList.begin();
+    }
+    else
+    {
+        // Get next element
+        _processIterator++;
+
+        // If end of list is reached
+        if (_processIterator == _processList.end())
+        {
+            // Return NULL
+            return NULL;
+        }
+    }
+
+    // Return current element
+    return (*_processIterator);
+}
+
+/**
+ * Return the process of the list which has been registered with the given name.
+ *
+ * This method looks in the list for the process which has been registered with
+ * the given name, and which is connected to the \em sd socket. If \em sd is not
+ * specified, the first process with the given name is returned.
+ *  
+ * This method can be used to figure out whether a process is in the list or
+ * not, as shown below:
+ * \code
+ *     if (processList.GetProcess(process.GetName()) == NULL)
+ *     {
+ *         printf ("Process not found in list !!");
+ *     }
+ * \endcode
+ * 
+ * \param name the searched process name
+ * \param sd optionnal socket descriptor to which the searched process should be
+ * connected
+ * 
+ * \return a pointer on the searched process, or NULL if the process was not
+ * found in list.
+ */
+msgPROCESS *msgPROCESS_LIST::GetProcess(const char *name, const mcsINT32 sd)
+{
+    logExtDbg("msgPROCESS_LIST::GetProcess()");
+
+    // Search process in the list
+    std::list<msgPROCESS *>::iterator iter;
+    for (iter=_processList.begin(); iter != _processList.end(); iter++)
+    {
+        // If found
+        if ((strcmp((*iter)->GetName(), name) == 0) &&
+            ((sd == -1) || ((*iter)->GetDescriptor() == sd)))
+        {
+            // Return pointer to the element 
+            return (*iter);
+        }
+    }
+
+    // If not found return NULL pointer
+    return NULL;
+}
+
+/**
+ * Remove a process from the list
+ *
+ * This method looks for the process corresponding to the specified socket
+ * descriptor \em sd in the list. If found, removes it, otherwise does nothing.
+ *
+ * \warning if the list contains more than one instance corresponding to \em sd,
+ * only the first occurence is removed. 
+ *
+ * \param sd socket descriptor of the process to be removed from the list
+ *
+ * \return always mcsSUCCESS
+ */
+mcsCOMPL_STAT msgPROCESS_LIST::Remove(mcsINT32 sd)
+{
+    logExtDbg("msgPROCESS_LIST::Remove()");
+
+    // Search process in the list
+    std::list<msgPROCESS *>::iterator iter;
+    for (iter=_processList.begin(); iter != _processList.end(); iter++)
+    {
+        // If found
+        if ((*iter)->GetDescriptor() == sd)
+        {
+            // Delete element
+            delete (*iter);
+            // Clear element from list
+            _processList.erase(iter);
+            return mcsSUCCESS;
+        }
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Erase all elements of the list.
+ *
+ * \return always mcsSUCCESS
+ */
+mcsCOMPL_STAT msgPROCESS_LIST::Clear(void)
+{
+    // Delete all objects in list 
+    std::list<msgPROCESS *>::iterator iter;
+    for (iter=_processList.begin(); iter != _processList.end(); iter++)
+    {
+        delete (*iter);
+    }
+
+    // Clear list
+    _processList.clear();
+    return mcsSUCCESS;
+}
+
+/*___oOo___*/
---
+++ msg/test/msgTestSocketClient.cpp
@@ -0,0 +1,187 @@
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: msgTestSocketClient.cpp,v 1.7 2006-05-11 13:04:56 mella Exp $"
+*
+* History
+* -------
+* $Log: not supported by cvs2svn $
+* Revision 1.6  2005/09/12 15:42:48  scetre
+* Removed Receive method with only string parameter
+* Defined default timeout in Receive function definition
+*
+* Revision 1.5  2005/04/22 09:25:09  mella
+* Replace FAILURE by mcsFAILURE
+*
+* Revision 1.4  2005/02/09 16:41:02  lafrasse
+* Changed the way mesage are displayed (now use msgMESSAGE 'operator<<' instead of 'Display')
+*
+* Revision 1.3  2005/02/04 15:57:06  lafrasse
+* Massive documentation review an refinment (also added automatic CVS log inclusion in every files)
+*
+* lafrasse  14-Dec-2004  Added error code management
+* lafrasse  24-Nov-2004  Created
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Small test client based on msgSOCKET_CLIENT.
+ *
+ * \synopsis
+ * \<msgTestSocketClient\>
+ */
+
+static char *rcsId __attribute__ ((unused)) ="@(#) $Id: msgTestSocketClient.cpp,v 1.7 2006-05-11 13:04:56 mella Exp $";
+
+/* 
+ * System Headers 
+ */
+#include <stdlib.h>
+#include <iostream>
+
+/**
+ * \namespace std
+ * Export standard iostream objects (cin, cout,...).
+ */
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "msg.h"
+#include "msgPrivate.h"
+
+
+/* 
+ * Main
+ */
+int main(int argc, char *argv[])
+{
+    msgMESSAGE msg;
+    string buffer = "GET /\n";
+
+    // Initialize MCS services
+    if (mcsInit(argv[0]) == mcsFAILURE)
+    {
+        // Exit from the application with FAILURE
+        exit (EXIT_FAILURE);
+    }
+
+    int portNumber = 80;
+
+    cout << "Client creating a new msgSOCKET_CLIENT object ... ";
+    msgSOCKET_CLIENT clientSocket;
+    cout << "OK" << endl;
+    cout << "Client connecting to server on localhost, port '" << portNumber
+         << "'... ";
+    if (clientSocket.Open("localhost", portNumber) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl << endl;
+
+    cout << "Client sending   '" << buffer << "' to server ... ";
+    if (clientSocket.Send(buffer) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl;
+    cout << "Client receiving '";
+    if (clientSocket.Receive(buffer, 5000) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    
+    cout << buffer << "' from server ... OK" << endl;
+    cout << "Client receiving '";
+    if (clientSocket.Receive(buffer) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << buffer << "' from server ... OK" << endl;
+    buffer = "Bye server said client !";
+    cout << "Client sending   '" << buffer << "' to server ... ";
+    if (clientSocket.Send(buffer) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl << endl;
+    
+    if (msg.SetSender("client") == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    if (msg.SetRecipient("server") == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    if (msg.SetType(msgTYPE_COMMAND) == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    if (msg.SetCommand("COMMAND") == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    if (msg.SetBody("Hello server, client speaking !",
+                     sizeof("Hello server, client speaking !")) == mcsFAILURE)
+    {
+        goto errCond;
+    }
+    cout << "Client sending '" << msg << "' to server ... ";
+    if (clientSocket.Send(msg) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl << endl;
+    cout << "Client receiving '";
+    if (clientSocket.Receive(msg, 1000) == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << msg << "' from server ... OK" << endl << endl;
+
+    cout << "Client deconnecting from server ... ";
+    if (clientSocket.Close() == mcsFAILURE)
+    {
+        cout << "KO" << endl;
+        goto errCond;
+    }
+    cout << "OK" << endl;
+
+    // Close MCS services
+    mcsExit();
+    
+    // Exit from the application with SUCCESS
+    exit (EXIT_SUCCESS);
+
+// If an error occured, show the error stack and exit
+errCond:
+    if (errStackIsEmpty() == mcsFALSE)
+    {
+        errCloseStack();
+    }
+
+    mcsExit();
+    exit(EXIT_FAILURE);
+}
+
+
+/*___oOo___*/

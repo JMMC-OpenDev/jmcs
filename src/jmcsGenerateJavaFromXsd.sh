20b568dc0898acc8f2b2482b22859063a16579a8
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2008-04-22 12:57:35 +0000

    Add castor handling

--- src/jmcsGenerateJavaFromXsd.sh
+++ src/jmcsGenerateJavaFromXsd.sh
@@ -2,11 +2,14 @@
 #*******************************************************************************
 # JMMC project
 #
-# "@(#) $Id: jmcsGenerateJavaFromXsd.sh,v 1.1 2008-04-16 14:15:27 fgalland Exp $"
+# "@(#) $Id: jmcsGenerateJavaFromXsd.sh,v 1.2 2008-04-22 12:57:35 mella Exp $"
 #
 # History
 # -------
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2008/04/16 14:15:27  fgalland
+# Creation.
+#
 #
 #*******************************************************************************
 
@@ -19,7 +22,12 @@
 
 #MODEL_SCHEMA=$(miscLocateFile mfmdl.xsd)
 MODEL_SCHEMA=fr/jmmc/mcs/gui/ApplicationDataSchema.xsd
-# generate model java source from xml schema
-echo "Generating classes for $MODEL_SCHEMA"
-echo " Using classpath : $(mkfMakeJavaClasspath)"
-java -classpath $(mkfMakeJavaClasspath) org.exolab.castor.builder.SourceGenerator -i ${MODEL_SCHEMA} -f -package fr.jmmc.mcs.gui.castor $*
+if [ "$MODEL_SCHEMA" -nt "fr/jmmc/mcs/gui/castor" ]
+then
+    # generate model java source from xml schema
+    echo "Generating classes for $MODEL_SCHEMA"
+    echo " Using classpath : $(mkfMakeJavaClasspath)"
+    java -classpath $(mkfMakeJavaClasspath) org.exolab.castor.builder.SourceGenerator -i ${MODEL_SCHEMA} -f -package fr.jmmc.mcs.gui.castor $*
+else
+    echo "Generated classes for $MODEL_SCHEMA up-to-date"
+fi

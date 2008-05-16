9590e873e144762d9dcbe9e670a07c0a858b4432
bcolucci <bcolucci@jmmc.fr>
2008-05-16 13:01:34 +0000

    Removed unecessary try/catch, and added argument checks. Threaded it.

--- jmcs/src/fr/jmmc/mcs/gui/FeedbackReportModel.java
+++ jmcs/src/fr/jmmc/mcs/gui/FeedbackReportModel.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: FeedbackReportModel.java,v 1.2 2008-04-24 15:55:57 mella Exp $"
+ * "@(#) $Id: FeedbackReportModel.java,v 1.3 2008-05-16 13:01:34 bcolucci Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2008/04/24 15:55:57  mella
+ * Added applicationDataModel to constructor.
+ *
  * Revision 1.1  2008/04/22 09:15:56  bcolucci
  * Created FeedbackReport.
  *
@@ -15,6 +18,8 @@ package fr.jmmc.mcs.gui;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.*;
 
+import java.lang.Thread;
+
 import java.util.*;
 import java.util.logging.*;
 
@@ -23,13 +28,13 @@ import javax.swing.JOptionPane;
 
 
 /** Model of FeedbackView class */
-public class FeedbackReportModel
+public class FeedbackReportModel extends Thread
 {
     /** Logger */
     private static final Logger _logger = Logger.getLogger(FeedbackReportModel.class.getName());
 
     /** URL of the PHP script that handles form parameters */
-    private static final String _phpScriptURL = "http://jmmc.fr/feedback/feedback.php";
+    private static final String _phpScriptURL = "http://jmmc.fr/~bcolucci/feedback/feedback.php";
 
     /** ApplicationData model */
     public static ApplicationDataModel _applicationDataModel;
@@ -64,20 +69,47 @@ public class FeedbackReportModel
     /** Application-specific information */
     private String _applicationSpecificInformation = "None";
 
+    /**
+     * DOCUMENT ME!
+     */
+    FeedbackReport _feedbackReport = null;
+
+    /**
+     * DOCUMENT ME!
+     */
+    private boolean readyToSend = false;
+
     /** Creates a new FeedbackReportModel object */
-    public FeedbackReportModel(ApplicationDataModel applicationDataModel)
+    public FeedbackReportModel(ApplicationDataModel applicationDataModel,
+        FeedbackReport feedbackReport)
     {
-        _typeDataModel = new DefaultComboBoxModel(_types);
-        _logger.fine("TypeDataModel constructed");
+        _feedbackReport = feedbackReport;
 
-        _applicationDataModel     = applicationDataModel;
-        // Get informations to send with the report
-        _programVersion           = _applicationDataModel.getProgramVersion();
-        _programName              = _applicationDataModel.getProgramName();
+        if (applicationDataModel != null)
+        {
+            _typeDataModel = new DefaultComboBoxModel(_types);
+            _logger.fine("TypeDataModel constructed");
 
-        _systemConfig             = getSystemConfig();
-        _applicationLog           = App.getLogOutput();
-        _logger.fine("All generated report informations have been collected");
+            _applicationDataModel     = applicationDataModel;
+            // Get informations to send with the report
+            _programVersion           = _applicationDataModel.getProgramVersion();
+            _programName              = _applicationDataModel.getProgramName();
+
+            _systemConfig             = getSystemConfig();
+            _applicationLog           = App.getLogOutput();
+            _logger.fine(
+                "All generated report informations have been collected");
+        }
+    }
+
+    /**
+     * Creates a new setReadyToSend object.
+     *
+     * @param ready DOCUMENT ME!
+     */
+    public void setReadyToSend(boolean ready)
+    {
+        readyToSend = ready;
     }
 
     /**
@@ -130,57 +162,87 @@ public class FeedbackReportModel
      */
     public void setTypeDataModel(DefaultComboBoxModel typeDataModel)
     {
-        _typeDataModel = typeDataModel;
-        _logger.fine("Type data model value has been set");
+        if (typeDataModel != null)
+        {
+            _typeDataModel = typeDataModel;
+            _logger.fine("Type data model value has been set");
+        }
     }
 
     /** Send the report peer mail */
-    public void sendReport()
+    public void run()
     {
-        try
+        while (! isInterrupted())
         {
-            // Create an HTTP client to send report information to our PHP script
-            HttpClient client = new HttpClient();
-            PostMethod method = new PostMethod(_phpScriptURL);
-            _logger.fine("Http client and post method have been created");
-
-            // Compose HTML form parameters
-            method.addParameter("programName", _programName);
-            method.addParameter("programVersion", _programVersion);
-            method.addParameter("systemConfig", _systemConfig);
-            method.addParameter("applicationLog", _applicationLog);
-            method.addParameter("userEmail", _mail);
-
-            String type = (String) _typeDataModel.getSelectedItem();
-            method.addParameter("reportClass", type);
-            method.addParameter("description", _description);
-            method.addParameter("applicationSpecificInformation",
-                _applicationSpecificInformation);
-            _logger.fine("All post parameters have been set");
-
-            // Send feedback report to PHP script
-            client.executeMethod(method);
-            _logger.fine("The report mail has been send");
-
-            // Get PHP script result (either SUCCESS or FAILURE)
-            String response = method.getResponseBodyAsString();
-
-            if (response.contains("FAILED"))
+            try
             {
-                _logger.fine("The PHP response is FAILURE");
-
-                String errorMessage = "Feedback Report message has not been sent.\nPlease check your internet connection.";
-                JOptionPane.showMessageDialog(null, errorMessage,
-                    "Feedback Report Failed", JOptionPane.ERROR_MESSAGE);
+                sleep(10);
             }
-            else
+            catch (Exception ex)
             {
-                _logger.fine("The PHP response is SUCCESS");
             }
-        }
-        catch (Exception ex)
-        {
-            _logger.log(Level.SEVERE, "Cannot send feedback report", ex);
+
+            if (readyToSend)
+            {
+                setMail(FeedbackReport.getMail());
+                setTypeDataModel(FeedbackReport.getDefaultComboBoxModel());
+                setDescription(FeedbackReport.getDescription());
+
+                try
+                {
+                    // Create an HTTP client to send report information to our PHP script
+                    HttpClient client = new HttpClient();
+                    PostMethod method = new PostMethod(_phpScriptURL);
+
+                    _logger.fine(
+                        "Http client and post method have been created");
+
+                    // Compose HTML form parameters
+                    method.addParameter("programName", _programName);
+                    method.addParameter("programVersion", _programVersion);
+                    method.addParameter("systemConfig", _systemConfig);
+                    method.addParameter("applicationLog", _applicationLog);
+                    method.addParameter("userEmail", _mail);
+
+                    String type = (String) _typeDataModel.getSelectedItem();
+                    method.addParameter("reportClass", type);
+                    method.addParameter("description", _description);
+                    method.addParameter("applicationSpecificInformation",
+                        _applicationSpecificInformation);
+                    _logger.fine("All post parameters have been set");
+
+                    // Send feedback report to PHP script
+                    client.executeMethod(method);
+                    _logger.fine("The report mail has been send");
+
+                    // Get PHP script result (either SUCCESS or FAILURE)
+                    String response = method.getResponseBodyAsString();
+
+                    System.out.println(response);
+
+                    if (response.contains(
+                                "The requested URL was not found on this server."))
+                    {
+                        _logger.fine("The PHP response is FAILURE");
+                        _feedbackReport.setReportSend(false);
+
+                        String errorMessage = "Feedback Report message has not been sent.\nPlease check your internet connection.";
+                        JOptionPane.showMessageDialog(null, errorMessage,
+                            "Feedback Report Failed", JOptionPane.ERROR_MESSAGE);
+                    }
+                    else
+                    {
+                        _feedbackReport.setReportSend(true);
+                        _logger.fine("The PHP response is SUCCESS");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.log(Level.SEVERE, "Cannot send feedback report", ex);
+                }
+
+                readyToSend = false;
+            }
         }
     }
 
@@ -196,21 +258,13 @@ public class FeedbackReportModel
         Enumeration hostPropertiesEnumeration = hostProperties.propertyNames();
         String      allHostProperties         = "";
 
-        try
+        // For each system property, we make a string like "{name} : {value}"
+        while (hostPropertiesEnumeration.hasMoreElements())
         {
-            // For each system property, we make a string like "{name} : {value}"
-            while (hostPropertiesEnumeration.hasMoreElements())
-            {
-                String propertyName  = String.valueOf(hostPropertiesEnumeration.nextElement());
-                String propertyValue = System.getProperty(propertyName);
+            String propertyName  = String.valueOf(hostPropertiesEnumeration.nextElement());
+            String propertyValue = System.getProperty(propertyName);
 
-                allHostProperties += (propertyName + " : " + propertyValue +
-                "\n");
-            }
-        }
-        catch (Exception ex)
-        {
-            _logger.log(Level.WARNING, "Cannot get system configuration", ex);
+            allHostProperties += (propertyName + " : " + propertyValue + "\n");
         }
 
         return allHostProperties;

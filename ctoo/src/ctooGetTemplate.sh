e748dd49aca570a51be3bca550bc47eca682d655
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2008-09-03 15:33:02 +0000

    This commit was manufactured by cvs2svn to create tag 'MAR2011/ctoo'.

---
+++ ctoo/src/ctooGetTemplate.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: ctooGetTemplate.sh,v 1.6 2006-03-07 14:22:22 mella Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# Revision 1.5  2005/01/24 15:47:51  gluck
+# Bug correction for log message automatic insertion ($Log: not supported by cvs2svn $)
+#
+# lgluck    23-Apr-2004 Created
+# gzins     04-Dec-2004 Removed test related to directory structure 
+#
+#*******************************************************************************
+
+#/**
+# \file
+# Create module directory structure or template files for code.
+#
+# \synopsis
+# ctooGetTemplate
+#
+# \details
+# Allow to create 2 kinds of working structure for development.
+# -# module directory structure : a way to organize properly module files
+# -# templates for code : following standard templates are available
+#                         - shell scripts
+#                         - C
+#                         - java
+#                         - C++
+#                         - Makefile
+# 
+# */
+
+
+# signal trap (if any)
+
+
+# Print out the menu
+cat <<xyz
+--------------------------------------------------------------------------------
+Templates are available for :
+    1- module directory structure
+    2- code
+xyz
+
+# Propose the user to enter his choice
+echo -e "\n-> Enter the number corresponding to the working structure you need "
+echo -e "   or press <Enter> to exit : \c"
+
+# Read the choice
+read choice
+
+# Treat the choice
+
+# Test if the choice is not empty
+if test -n "$choice"
+then
+    # Examine the choice
+    case $choice in
+        1) ctooGetDirectoryStructure;;
+        2) ctooGetCode;;
+        *) echo "ERROR : invalid choice";;
+    esac
+else
+    # <Enter> was pressed
+    exit
+fi
+
+#
+# ___oOo___

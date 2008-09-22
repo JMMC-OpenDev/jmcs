d83aaaa1209d8be871a989288d2448d566b51b93
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2008-09-22 16:51:43 +0000

    This commit was manufactured by cvs2svn to create tag 'SEP2008/jmcs'.

---
+++ jmcs/include/jmcsPrivate.h
@@ -0,0 +1,30 @@
+#ifndef jmcsPrivate_H
+#define jmcsPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: jmcsPrivate.h,v 1.1 2007-02-13 10:31:40 ccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * jmcs private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "jmcs"
+
+ 
+
+#endif /*!jmcsPrivate_H*/
+
+/*___oOo___*/

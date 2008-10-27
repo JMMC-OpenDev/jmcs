c0da4fd50b99d4a605350df37e5ef19c7dd41b49
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2008-10-27 15:26:15 +0000

    This commit was manufactured by cvs2svn to create tag 'SC_V4_0/modjava'.

---
+++ modjava/src/fr/jmmc/mcs/modjava/Complex.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: Complex.java,v 1.4 2008-09-22 16:54:05 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.3  2008/07/01 09:12:08  lafrasse
+ * Moved old code into new package tree.
+ *
+ * Revision 1.2  2006/03/07 14:20:58  lafrasse
+ * Corrected typos
+ *
+ * Revision 1.1  2006/03/07 14:13:37  lafrasse
+ * initial release
+ *
+ ******************************************************************************/
+package fr.jmmc.mcs.modjava;
+
+
+/**
+ * Complex numbers.
+ */
+public class Complex
+{
+    /** real part of the complex number */
+    private double real;
+
+    /** imaginary part of the complex number */
+    private double imag;
+
+    /**
+     * Default constructor.
+     */
+    public Complex()
+    {
+        real     = 0.0;
+        imag     = 0.0;
+    }
+
+    /**
+     * Parametred constructor.
+     *
+     * @param _real real part of the complex number.
+     * @param _imag imaginary part of the complex number.
+     */
+    public Complex(double _real, double _imag)
+    {
+        real     = _real;
+        imag     = _imag;
+    }
+
+    /**
+     * Add a complex.
+     *
+     * @param z the complex to be added.
+     *
+     * @return the result of the addition.
+     */
+    public Complex add(Complex z)
+    {
+        Complex temp = new Complex();
+
+        temp.real     = real + z.real;
+        temp.imag     = imag + z.imag;
+
+        return temp;
+    }
+
+    /**
+     * Give back the string representation of a complex.
+     *
+     * @return the representation of the complex as a String object.
+     */
+    public String toString()
+    {
+        String temp = "(" + real + ", " + imag + ")";
+
+        return temp;
+    }
+}
+/*___oOo___*/
---
+++ modjava/src/fr/jmmc/mcs/modjava/Main.java
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: Main.java,v 1.4 2008-10-27 15:26:13 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.3  2008/10/15 12:02:53  mella
+ * rename finnish method into finish
+ *
+ * Revision 1.2  2008/09/22 16:53:50  lafrasse
+ * Moved to new JMCS APIs.
+ *
+ * Revision 1.1  2008/07/01 08:58:13  lafrasse
+ * Added jmcs test application from bcolucci.
+ *
+ ******************************************************************************/
+package fr.jmmc.mcs.modjava;
+
+import fr.jmmc.mcs.gui.*;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;
+
+import java.net.URL;
+
+import java.util.logging.*;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+/**
+ * Class for tests
+ *
+ * @author $author$
+ * @version $Revision: 1.4 $
+ */
+public class Main extends App
+{
+    /** Logger */
+    private static final Logger _logger = Logger.getLogger(Main.class.getName());
+
+    /** Button to launch about box window */
+    private JButton _aboutBoxButton = null;
+
+    /** Button to launch feedback report window */
+    private JButton _feedbackReportButton = null;
+
+    /** Button to launch helpview window */
+    private JButton _helpViewButton = null;
+
+    /** Actions class */
+    Actions _actions;
+
+    /** Constructor */
+    public Main(String[] args)
+    {
+        super(args, false, true, true);
+    }
+
+    /** Initialize application objects */
+    @Override
+    protected void init(String[] args)
+    {
+        _logger.warning("Initialize application objects");
+        _logger.info(args.length + " arguments have been taken");
+
+        _actions                  = new Actions();
+
+        // .. buttons
+        _aboutBoxButton           = new JButton(aboutBoxAction());
+        _feedbackReportButton     = new JButton(feedbackReportAction());
+        _helpViewButton           = new JButton(openHelpFrame());
+
+        // Set borderlayout
+        getFramePanel().setLayout(new BorderLayout());
+
+        // Add buttons to panel
+        getFramePanel().add(_aboutBoxButton, BorderLayout.NORTH);
+        getFramePanel().add(_feedbackReportButton, BorderLayout.CENTER);
+    }
+
+    /** Execute application body */
+    @Override
+    protected void execute()
+    {
+        _logger.info("Execute application body");
+
+        // Set others preferences
+        try
+        {
+            Preferences.getInstance().setPreference("MAIN", "maim");
+        }
+        catch (Exception ex)
+        {
+            _logger.log(Level.WARNING, "Failed setting preference.", ex);
+        }
+
+        // Show the frame
+        getFrame().setVisible(true);
+    }
+
+    /** Execute operations before closing application */
+    @Override
+    protected boolean finish()
+    {
+        _logger.warning("Execute operations before closing application");
+
+        // Quit application
+        return true;
+    }
+
+    /** Open help view action */
+    private Action openHelpFrame()
+    {
+        return new AbstractAction("Open Help Frame")
+            {
+                public void actionPerformed(ActionEvent evt)
+                {
+                    // Instantiate help JFrame
+                    JFrame helpFrame = new JFrame("- Help frame -");
+
+                    // Add buttons to panel
+                    helpFrame.getContentPane()
+                             .add(new JButton(showHelpAction()),
+                        BorderLayout.NORTH);
+                    helpFrame.getContentPane()
+                             .add(new JButton("CENTER"), BorderLayout.CENTER);
+                    helpFrame.getContentPane()
+                             .add(new JButton("WEST"), BorderLayout.WEST);
+                    helpFrame.getContentPane()
+                             .add(new JButton("SOUTH"), BorderLayout.SOUTH);
+
+                    // Set the frame properties
+                    helpFrame.pack();
+                    helpFrame.setLocationRelativeTo(null);
+                    helpFrame.setVisible(true);
+                }
+            };
+    }
+
+    /**
+     * Main
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args)
+    {
+        new Main(args);
+    }
+}
+/*___oOo___*/

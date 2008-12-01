8b6db209320fe3234955d055076d32d36ed39f9d
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2008-12-01 09:34:25 +0000

    rejalopization

--- src/fr/jmmc/mcs/gui/CheckBoxJList.java
+++ src/fr/jmmc/mcs/gui/CheckBoxJList.java
@@ -20,8 +20,8 @@ import javax.swing.event.*;
  * DOCUMENT ME!
  *
  * @author $author$
- * @version $Revision: 1.2 $
-  */
+ * @version $Revision: 1.3 $
+ */
 public class CheckBoxJList extends JList implements ListSelectionListener
 {
     /**
@@ -164,7 +164,7 @@ public class CheckBoxJList extends JList implements ListSelectionListener
                 "Chris", "Joshua", "Daniel", "Michael", "Don", "Kimi", "Kelly",
                 "Keagan"
             };
-        Iterator it        = Arrays.asList(listItems).iterator();
+        Iterator it = Arrays.asList(listItems).iterator();
 
         while (it.hasNext())
         {

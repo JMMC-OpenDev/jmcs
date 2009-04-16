a564c1bfabe2edb48c27991344204ec765ee09e5
sly74fr <slymacdev@free.fr>
2009-04-16 15:44:51 +0000

    Jalopization.

--- src/fr/jmmc/mcs/gui/MainMenuBar.java
+++ src/fr/jmmc/mcs/gui/MainMenuBar.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: MainMenuBar.java,v 1.25 2009-04-14 13:12:04 mella Exp $"
+ * "@(#) $Id: MainMenuBar.java,v 1.26 2009-04-16 15:44:51 lafrasse Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.25  2009/04/14 13:12:04  mella
+ * Fix code that didn't retrieve icon for non MCSAction
+ *
  * Revision 1.24  2008/10/17 10:41:54  lafrasse
  * Added FAQ handling.
  *
@@ -95,17 +98,18 @@ package fr.jmmc.mcs.gui;
 
 import fr.jmmc.mcs.util.*;
 
-import java.net.MalformedURLException;
-import java.util.logging.Level;
 import org.apache.commons.lang.SystemUtils;
 
 import java.awt.Component;
 
 import java.lang.reflect.Method;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+
 import java.util.Hashtable;
 import java.util.Vector;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.AbstractAction;
@@ -555,16 +559,23 @@ public class MainMenuBar extends JMenuBar
 
             // Set icon
             String icon = menu.getIcon();
-            if (icon != null) {
+
+            if (icon != null)
+            {
                 // Open XML file at path
                 URL iconURL = getClass().getResource(icon);
-                if (iconURL != null) {
-                    action.putValue(Action.SMALL_ICON, new ImageIcon(Urls.fixJarURL(iconURL)));
-                } else {
+
+                if (iconURL != null)
+                {
+                    action.putValue(Action.SMALL_ICON,
+                        new ImageIcon(Urls.fixJarURL(iconURL)));
+                }
+                else
+                {
                     _logger.warning("Can't find iconUrl : " + icon);
                 }
             }
-            
+
             _logger.fine("Attributes set on '" + menu.getLabel() + "'.");
         }
     }
--- src/fr/jmmc/mcs/gui/ShowHelpAction.java
+++ src/fr/jmmc/mcs/gui/ShowHelpAction.java
@@ -1,12 +1,21 @@
 package fr.jmmc.mcs.gui;
 
 import fr.jmmc.mcs.util.Urls;
+
 import java.awt.event.ActionEvent;
+
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 
-public class ShowHelpAction extends AbstractAction {
 
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision: 1.4 $
+ */
+public class ShowHelpAction extends AbstractAction
+{
     /** Help id associted to the given label.*/
     private String _helpID;
 
@@ -16,29 +25,42 @@ public class ShowHelpAction extends AbstractAction {
      *
      * @param label the key used to retrieve the documentation page.
      */
-    public ShowHelpAction(String label) {
+    public ShowHelpAction(String label)
+    {
         // Set Icon (without additional label)
-        String icon="/fr/jmmc/mcs/gui/help.png";
-        this.putValue(SMALL_ICON, new ImageIcon(Urls.fixJarURL(getClass().getResource(icon))));
+        String icon = "/fr/jmmc/mcs/gui/help.png";
+        this.putValue(SMALL_ICON,
+            new ImageIcon(Urls.fixJarURL(getClass().getResource(icon))));
 
         // If help is available, then try to get the HelpID that ends with given label
         boolean helpIsAvailable = HelpView.isAvailable();
         setEnabled(helpIsAvailable);
-        if (helpIsAvailable) {
+
+        if (helpIsAvailable)
+        {
             _helpID = HelpView.getHelpID(label);
+
             // If no helpID found, then show one feedback report and disable action
-            if (_helpID == null) {
-                new FeedbackReport(
-                        new Exception("Documentation problem:\nNo helpID found for label '" +
+            if (_helpID == null)
+            {
+                new FeedbackReport(new Exception(
+                        "Documentation problem:\nNo helpID found for label '" +
                         label +
                         "'\nPlease send this feedback to improve documentation."));
                 setEnabled(false);
+
                 return;
             }
         }
     }
 
-    public void actionPerformed(ActionEvent e) {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param e DOCUMENT ME!
+     */
+    public void actionPerformed(ActionEvent e)
+    {
         HelpView.show(_helpID);
     }
 }

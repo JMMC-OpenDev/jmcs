c7f49658bbf6122e18716834f9f78928697ff633
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2009-05-13 09:24:26 +0000

    This commit was manufactured by cvs2svn to create tag 'MAY2009/jmcs'.

---
+++ jmcs/include/jmcs.h
@@ -0,0 +1,27 @@
+#ifndef jmcs_H
+#define jmcs_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: jmcs.h,v 1.1 2007-02-13 10:31:39 ccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * jmcs general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!jmcs_H*/
+
+/*___oOo___*/

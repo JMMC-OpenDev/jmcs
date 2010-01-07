49a9b9a504b4f505ec0482e94acc5613a9b0c648
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2010-01-07 13:03:57 +0000

    add test examples when command is executed without parameters

--- src/fr/jmmc/mcs/astro/LD2UD.java
+++ src/fr/jmmc/mcs/astro/LD2UD.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: LD2UD.java,v 1.3 2010-01-07 12:53:26 mella Exp $"
+ * "@(#) $Id: LD2UD.java,v 1.4 2010-01-07 13:03:57 mella Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2010/01/07 12:53:26  mella
+ * Use command line arguments
+ *
  * Revision 1.2  2010/01/07 10:30:36  lafrasse
  * Added MCS header.
  *
@@ -23,8 +26,12 @@ public class LD2UD {
             System.out.println(ALX.ld2ud(Double.parseDouble(args[0]),args[1]));
         } catch (Exception e) {
             System.err.println("Usage: LD2UD <limb darkened diameter> <spectral type>");
-            System.err.println("Exemple: LD2U '1.185' 'K3III'");
+            System.err.println("Exemple HD199947: LD2U '1.185' 'K3III'");
             System.err.println(ALX.ld2ud(1.185,"K3III"));
+            System.err.println("Exemple HD188154: LD2U '' 'K5III'");
+            System.err.println(ALX.ld2ud(2.604,"K5III"));
+            System.err.println("Exemple HD178524: LD2U '1.754' 'F2II/III'");
+            System.err.println(ALX.ld2ud(1.754,"F2II/III"));
         }        
     }
 }

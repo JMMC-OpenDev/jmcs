86b8fdb366d0eff7a05afcd3993274aca83a4db1
bourgesl <bourges.laurent@gmail.com>
2010-05-11 16:10:06 +0000

    added new models + javadoc

--- jmcs/src/fr/jmmc/mcs/model/function/RingModelFunction.java
+++ jmcs/src/fr/jmmc/mcs/model/function/RingModelFunction.java
@@ -1,29 +1,11 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: DiskModelFunction.java,v 1.7 2010-05-11 16:10:06 bourgesl Exp $"
+ * "@(#) $Id: RingModelFunction.java,v 1.1 2010-05-11 16:10:06 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
- * Revision 1.6  2010/02/18 15:51:18  bourgesl
- * added parameter argument validation and propagation (illegal argument exception)
- *
- * Revision 1.5  2010/02/18 09:59:37  bourgesl
- * new ModelDefinition interface to gather model and parameter types
- *
- * Revision 1.4  2010/02/16 14:44:14  bourgesl
- * getParameter(mode, type) renamed to getParameterValue(model, type)
- *
- * Revision 1.3  2010/02/12 15:52:05  bourgesl
- * refactoring due to changed generated classes by xjc
- *
- * Revision 1.2  2010/02/03 16:05:46  bourgesl
- * Added fast thread interruption checks for asynchronous uv map computation
- *
- * Revision 1.1  2010/01/29 15:52:46  bourgesl
- * Beginning of the Target Model Java implementation = ModelManager and ModelFunction implementations (punct, disk)
- *
  */
 package fr.jmmc.mcs.model.function;
 
@@ -34,27 +16,30 @@ import fr.jmmc.mcs.model.targetmodel.Parameter;
 import org.apache.commons.math.complex.Complex;
 
 /**
- * This ModelFunction implements the disk model
+ * This ModelFunction implements the ring model
  * 
  * @author bourgesl
  */
-public final class DiskModelFunction extends AbstractModelFunction {
+public final class RingModelFunction extends AbstractModelFunction {
 
   /* Model constants */
   /** model description */
-  private static final String MODEL_DESC = "lpb_disk(ufreq, vfreq, flux_weight, x, y, diameter) \n\n" +
+  private static final String MODEL_DESC = "lpb_ring(ufreq, vfreq, flux_weight, x, y, diameter, width) \n\n" +
           "Returns the Fourier transform, at spatial frequencies (UFREQ,VFREQ) \n" +
-          "given in 1/rad, of a normalized uniform disk of diameter \n" +
-          "DIAMETER (milliarcsecond) and centered at coordinates (X,Y) (milliarcsecond). \n" +
+          "given in 1/rad, of a normalized uniform ring with internal \n" +
+          "diameter DIAMETER  (milliarcsecond) and external diameter DIAMETER+WIDTH \n" +
+          "centered at coordinates (X,Y) (milliarcsecond). \n" +
           "FLUX_WEIGHT is the intensity coefficient. FLUX_WEIGHT=1 means total energy is 1. \n" +
-          "The function returns an error if DIAMETER is negative.\n\n" +
+          "The function returns an error if DIAMETER or WIDTH are negative.\n\n" +
           "UFREQ and VFREQ must be conformable. The returned array is always \n" +
           "complex and of dims dimsof(UFREQ,VFREQ). \n";
+  /** Parameter type for the parameter width */
+  public static String PARAM_WIDTH = "width";
 
   /**
    * Constructor
    */
-  public DiskModelFunction() {
+  public RingModelFunction() {
     super();
   }
 
@@ -63,7 +48,7 @@ public final class DiskModelFunction extends AbstractModelFunction {
    * @return model type
    */
   public String getType() {
-    return MODEL_DISK;
+    return MODEL_RING;
   }
 
   /**
@@ -81,8 +66,8 @@ public final class DiskModelFunction extends AbstractModelFunction {
   @Override
   public Model newModel() {
     final Model model = super.newModel();
-    model.setName(MODEL_DISK);
-    model.setType(MODEL_DISK);
+    model.setName(MODEL_RING);
+    model.setType(MODEL_RING);
     model.setDesc(MODEL_DESC);
 
     Parameter param;
@@ -95,6 +80,14 @@ public final class DiskModelFunction extends AbstractModelFunction {
     param.setUnits(UNIT_MAS);
     model.getParameters().add(param);
 
+    param = new Parameter();
+    param.setName(PARAM_WIDTH);
+    param.setType(PARAM_WIDTH);
+    param.setMinValue(0D);
+    param.setValue(0D);
+    param.setUnits(UNIT_MAS);
+    model.getParameters().add(param);
+
     return model;
   }
 
@@ -102,7 +95,7 @@ public final class DiskModelFunction extends AbstractModelFunction {
    * Compute the model function for the given Ufreq, Vfreq arrays and model parameters
    *
    * Returns the Fourier transform, at spatial frequencies (UFREQ,VFREQ)
-   * given in 1/rad, of a normalized uniform disk of diameter at coordinates
+   * given in 1/rad, of a normalized uniform circle of diameter at coordinates
    * (X,Y) given in milliarcsecond.
    *
    * Note : the visibility array is given to add this model contribution to the total visibility
@@ -128,14 +121,19 @@ public final class DiskModelFunction extends AbstractModelFunction {
     final double x = getParameterValue(model, PARAM_X);
     final double y = getParameterValue(model, PARAM_Y);
     final double diameter = getParameterValue(model, PARAM_DIAMETER);
+    final double width = getParameterValue(model, PARAM_WIDTH);
 
     if (diameter < 0d) {
       createParameterException(PARAM_DIAMETER, model, "< 0");
     }
 
+    if (width < 0d) {
+      createParameterException(PARAM_WIDTH, model, "< 0");
+    }
+
     // Compute :
     for (int i = 0; i < size; i++) {
-      vis[i] = vis[i].add(compute_disk(ufreq[i], vfreq[i], flux_weight, x, y, diameter));
+      vis[i] = vis[i].add(compute_ring(ufreq[i], vfreq[i], flux_weight, x, y, diameter, width));
 
       // fast interrupt :
       if (i % stepInterrupt == 0 && currentThread.isInterrupted()) {
@@ -145,51 +143,44 @@ public final class DiskModelFunction extends AbstractModelFunction {
   }
 
   /**
-   * Compute the disk model function for a single UV point
-   *
-   * shift(ufreq, vfreq, x, y) * flux_weight * [ 2 * bessJ1(PI x diameter x norm(uv)) / PI x diameter x norm(uv)]
+   * Compute the ring model function for a single UV point
    *
    * @param ufreq U frequency in rad-1
    * @param vfreq V frequency in rad-1
    * @param flux_weight intensity coefficient
    * @param x x coordinate of the object given in milliarcsecond
    * @param y y coordinate of the object given in milliarcsecond
-   * @param diameter diameter of the disk model given in milliarcsecond
+   * @param diameter diameter of the ring model given in milliarcsecond
+   * @param width width of the ring model given in milliarcsecond
    * @return complex Fourier transform value
    */
-  protected final static Complex compute_disk(final double ufreq, final double vfreq, final double flux_weight,
-                                              final double x, final double y, final double diameter) {
+  private final static Complex compute_ring(final double ufreq, final double vfreq, final double flux_weight,
+                                            final double x, final double y, final double diameter, final double width) {
 
-    return shift(ufreq, vfreq, x, y).multiply(compute_disk_weight(ufreq, vfreq, flux_weight, diameter));
-  }
-
-  /**
-   * Return the weight part of the disk model function for a single UV point
-   *
-   * flux_weight * [ 2 * bessJ1(PI x diameter x norm(uv)) / PI x diameter x norm(uv)]
-   *
-   * @param ufreq U frequency in rad-1
-   * @param vfreq V frequency in rad-1
-   * @param flux_weight intensity coefficient
-   * @param diameter diameter of the uniform disk object given in milliarcsecond
-   * @return weight part of the disk model function
-   */
-  protected final static double compute_disk_weight(final double ufreq, final double vfreq,
-                                                    final double flux_weight, final double diameter) {
+    if (width == 0d) {
+      // Returns the Fourier transform of infinitely thin ring, i.e. a circle.
+      return CircleModelFunction.compute_circle(ufreq, vfreq, flux_weight, x, y, diameter);
+    }
+    if (diameter == 0d) {
+      // Returns the Fourier transform of a disk of radius width.
+      return DiskModelFunction.compute_disk(ufreq, vfreq, flux_weight, x, y, 2d * width);
+    }
 
     // norm of uv :
     final double normUV = Math.sqrt(ufreq * ufreq + vfreq * vfreq);
 
-    final double d = PI * MAS2RAD * diameter * normUV;
+    final double alpha = 1d + width * diameter / 2d;
+
+    final double r = PI * MAS2RAD * diameter * normUV / 2d;
 
     double g;
-    if (d == 0D) {
+    if (r == 0D) {
       g = 1D;
     } else {
-      g = 2D * Bessel.j1(d) / d;
+      g = ((alpha * Bessel.j1(2d * alpha * r) / r) - (Bessel.j1(2d * r) / r)) / (alpha * alpha - 1d);
     }
     g *= flux_weight;
 
-    return g;
+    return shift(ufreq, vfreq, x, y).multiply(flux_weight * g);
   }
 }

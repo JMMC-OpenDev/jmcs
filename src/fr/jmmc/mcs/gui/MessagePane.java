b2086570694b43ee67f5033c914dbd338fc27363
bourgesl <bourges.laurent@gmail.com>
2010-09-30 15:13:03 +0000

    added methods for information and confirmation messages complete javadoc

--- src/fr/jmmc/mcs/gui/MessagePane.java
+++ src/fr/jmmc/mcs/gui/MessagePane.java
@@ -1,14 +1,18 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: MessagePane.java,v 1.1 2010-09-24 15:44:54 bourgesl Exp $"
+ * "@(#) $Id: MessagePane.java,v 1.2 2010-09-30 15:13:03 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/09/24 15:44:54  bourgesl
+ * initial version : utility class to show message dialogs
+ *
  */
 package fr.jmmc.mcs.gui;
 
+import java.awt.Component;
 import java.awt.Frame;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +21,10 @@ import javax.swing.JOptionPane;
 
 /**
  * This class provides utility methods to create message panes (message, error) with/without exceptions
+ *
+ *
+ * TODO : javadoc
+ *
  * @author bourgesl
  */
 public final class MessagePane {
@@ -25,6 +33,8 @@ public final class MessagePane {
   private static final Logger _logger = Logger.getLogger(MessagePane.class.getName());
   /** default title for error messages */
   private final static String TITLE_ERROR = "Error";
+  /** default title for information messages */
+  private final static String TITLE_INFO = "Information";
 
   /**
    * Forbidden constructor
@@ -33,18 +43,41 @@ public final class MessagePane {
     super();
   }
 
+  // --- ERROR MESSAGES --------------------------------------------------------
+  /**
+   * Show an error with the given message
+   * @param message message to display
+   */
   public static void showErrorMessage(final String message) {
     showErrorMessage(message, TITLE_ERROR, null);
   }
 
+  /**
+   * Show an error with the given message plus the exception message (if any)
+   * and log the exception
+   * @param message message to display
+   * @param th exception to use
+   */
   public static void showErrorMessage(final String message, final Throwable th) {
     showErrorMessage(message, TITLE_ERROR, th);
   }
 
+  /**
+   * Show an error with the given message and window title
+   * @param message message to display
+   * @param title window title to use
+   */
   public static void showErrorMessage(final String message, final String title) {
     showErrorMessage(message, title, null);
   }
 
+  /**
+   * Show an error with the given message plus the exception message (if any)
+   * and window title and log the exception
+   * @param message message to display
+   * @param title window title to use
+   * @param th exception to use
+   */
   public static void showErrorMessage(final String message, final String title, final Throwable th) {
 
     if (th != null && _logger.isLoggable(Level.SEVERE)) {
@@ -61,13 +94,70 @@ public final class MessagePane {
     JOptionPane.showMessageDialog(getApplicationFrame(), msg, title, JOptionPane.ERROR_MESSAGE);
   }
 
+  // --- INFO MESSAGES ---------------------------------------------------------
   /**
-   * Return a not null parent Frame for a dialog window
-   * @param frame given frame argument
-   * @return given frame or application frame if the given frame is null
+   * Show an information with the given message
+   * @param message message to display
    */
-  public final static Frame getOwner(final Frame frame) {
-    Frame owner = frame;
+  public static void showMessage(final String message) {
+    showMessage(message, TITLE_INFO);
+  }
+
+  /**
+   * Show an information with the given message and window title
+   * @param message message to display
+   * @param title window title to use
+   */
+  public static void showMessage(final String message, final String title) {
+    JOptionPane.showMessageDialog(getApplicationFrame(), message, title, JOptionPane.INFORMATION_MESSAGE);
+  }
+
+  // --- CONFIRM MESSAGES ------------------------------------------------------
+  /**
+   * Show a confirmation dialog to ask if the user wants to overwrite the file with the same name
+   * @param fileName file name
+   * @return true if the user answers yes
+   */
+  public static boolean showConfirmFileOverwrite(final String fileName) {
+    return showConfirmMessage("File \'" + fileName + "\' already exists.\nDo you want to overwrite this file ?");
+  }
+
+  /**
+   * Show a confirmation dialog to ask the given question
+   * @param message message to ask
+   * @return true if the user answers yes
+   */
+  public static boolean showConfirmMessage(final String message) {
+    return showConfirmMessage(getApplicationFrame(), message);
+  }
+
+  /**
+   * Show a confirmation dialog to ask the given question
+   * @param parentComponent Parent component or null
+   * @param message message to ask
+   * @return true if the user answers yes
+   */
+  public static boolean showConfirmMessage(final Component parentComponent, final String message) {
+    final int answer = JOptionPane.showConfirmDialog(getParent(parentComponent), message);
+
+    return answer == JOptionPane.YES_OPTION;
+  }
+  /**
+   * Return a parent component / owner for a dialog window
+   * @param com component argument
+   * @return given component argument or the application frame if the given component is null
+   */
+  public final static Frame getOwner(final Frame com) {
+    return (Frame)getParent(com);
+  }
+  
+  /**
+   * Return a parent component / owner for a dialog window
+   * @param com component argument
+   * @return given component argument or the application frame if the given component is null
+   */
+  public final static Component getParent(final Component com) {
+    Component owner = com;
     if (owner == null) {
       owner = getApplicationFrame();
     }

730bcb1084c34da95b919ad056666479955b0ff3
bourgesl <bourges.laurent@gmail.com>
2010-10-14 12:12:19 +0000

    minor code cleanup

--- src/fr/jmmc/mcs/gui/SearchField.java
+++ src/fr/jmmc/mcs/gui/SearchField.java
@@ -1,11 +1,15 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: SearchField.java,v 1.4 2009-10-08 14:09:11 lafrasse Exp $"
+ * "@(#) $Id: SearchField.java,v 1.5 2010-10-14 12:12:19 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2009/10/08 14:09:11  lafrasse
+ * Moved form fr.jmmc.mcs.astro.star.StarResolverWidget .
+ * Refined documentation and layout.
+ *
  * Revision 1.3  2009/10/08 09:02:22  lafrasse
  * Added magnifying glass icon on the left.
  *
@@ -18,15 +22,28 @@
  ******************************************************************************/
 package fr.jmmc.mcs.gui;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.event.MouseInputListener;
 import org.apache.commons.lang.SystemUtils;
-
-import java.awt.*;
-import java.awt.event.*;
-
-import javax.swing.*;
-import javax.swing.border.*;
-import javax.swing.event.*;
-
 
 /**
  * A text field for search/filter interfaces. The extra functionality includes
@@ -38,17 +55,16 @@ import javax.swing.event.*;
  * @todo : add a menu of recent searches.
  * @todo : make recent searches persistent.
  */
-public class SearchField extends JTextField
-{
+public class SearchField extends JTextField {
+
+    /** default serial UID for Serializable interface */
+    private static final long serialVersionUID = 1;
     /** The border that draws the magnifying glass and the cancel cross */
     private static final Border CANCEL_BORDER = new CancelBorder();
-
     /** Store wether notifications should be sent every time a key is pressed */
     private boolean sendsNotificationForEachKeystroke = false;
-
     /** Store wether a text should be drawn when nothing else in textfield */
     private boolean showingPlaceholderText = false;
-
     /** store wether the cnacell cross is currently clicked */
     private boolean armed = false;
 
@@ -57,8 +73,7 @@ public class SearchField extends JTextField
      *
      * @param placeholderText the text displayed when nothing in.
      */
-    public SearchField(String placeholderText)
-    {
+    public SearchField(String placeholderText) {
         super(8); // 8 characters wide by default
 
         addFocusListener(new PlaceholderText(placeholderText));
@@ -69,37 +84,32 @@ public class SearchField extends JTextField
     /**
      * Creates a new SearchField object with a default "Search" place holder.
      */
-    public SearchField()
-    {
+    public SearchField() {
         this("Search");
     }
 
     /**
      * Draw the custom widget border.
      */
-    private void initBorder()
-    {
+    private void initBorder() {
         // On Mac OS X, simply use the OS specific search textfield widget
-        if (SystemUtils.IS_OS_MAC_OSX == true)
-        {
+        if (SystemUtils.IS_OS_MAC_OSX == true) {
             // http://developer.apple.com/mac/library/technotes/tn2007/tn2196.html#//apple_ref/doc/uid/DTS10004439
             putClientProperty("JTextField.variant", "search");
             putClientProperty("JTextField.FindAction",
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                        postActionEvent();
-                    }
-                });
+                    new ActionListener() {
+
+                        public void actionPerformed(ActionEvent e) {
+                            postActionEvent();
+                        }
+                    });
             putClientProperty("JTextField.CancelAction",
-                new ActionListener()
-                {
-                    public void actionPerformed(ActionEvent e)
-                    {
-                        cancel();
-                    }
-                });
+                    new ActionListener() {
+
+                        public void actionPerformed(ActionEvent e) {
+                            cancel();
+                        }
+                    });
 
             return;
         }
@@ -125,12 +135,11 @@ public class SearchField extends JTextField
      *
      * @param g the graphical context to draw in.
      */
-    protected void paintComponent(Graphics g)
-    {
+    @Override
+    protected void paintComponent(Graphics g) {
         // On anything but Mac OS X
-        if (SystemUtils.IS_OS_MAC_OSX == false)
-        {
-            int width  = getWidth();
+        if (SystemUtils.IS_OS_MAC_OSX == false) {
+            int width = getWidth();
             int height = getHeight();
 
             // Paint a rounded rectangle in the background surrounded by a black line.
@@ -141,8 +150,8 @@ public class SearchField extends JTextField
             g.fillRoundRect(0, -1, width, height, height, height);
 
             g.setColor(getBackground());
-            g.fillRoundRect(1, 1, width - 2, height - 2, height - 2, height -
-                2);
+            g.fillRoundRect(1, 1, width - 2, height - 2, height - 2, height
+                    - 2);
 
             g.setColor(Color.LIGHT_GRAY);
             g.drawLine(10, 1, width - 10, 1);
@@ -155,29 +164,24 @@ public class SearchField extends JTextField
     /**
      * Follow keystrokes to notify listeners accordinaly.
      */
-    private void initKeyListener()
-    {
-        addKeyListener(new KeyAdapter()
-            {
-                public void keyReleased(KeyEvent e)
-                {
-                    if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
-                    {
-                        cancel();
-                    }
-                    else if (sendsNotificationForEachKeystroke)
-                    {
-                        maybeNotify();
-                    }
+    private void initKeyListener() {
+        addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    cancel();
+                } else if (sendsNotificationForEachKeystroke) {
+                    maybeNotify();
                 }
-            });
+            }
+        });
     }
 
     /**
      * Reset SearchField content and notify listeners.
      */
-    private void cancel()
-    {
+    private void cancel() {
         setText("");
         postActionEvent();
     }
@@ -185,10 +189,8 @@ public class SearchField extends JTextField
     /**
      * Trap notifications when showing place holder.
      */
-    private void maybeNotify()
-    {
-        if (showingPlaceholderText)
-        {
+    private void maybeNotify() {
+        if (showingPlaceholderText) {
             return;
         }
 
@@ -200,31 +202,31 @@ public class SearchField extends JTextField
      *
      * @param eachKeystroke true to notify any key pressed, false otherwise.
      */
-    public void setSendsNotificationForEachKeystroke(boolean eachKeystroke)
-    {
+    public void setSendsNotificationForEachKeystroke(boolean eachKeystroke) {
         this.sendsNotificationForEachKeystroke = eachKeystroke;
     }
 
     /**
      * Draws the cancel button (a gray circle with a white cross) and the magnifying glass icon.
      */
-    static class CancelBorder extends EmptyBorder
-    {
+    private final static class CancelBorder extends EmptyBorder {
+
+        /** default serial UID for Serializable interface */
+        private static final long serialVersionUID = 1;
         private static final Color DISARMED_GRAY = new Color(0.7f, 0.7f, 0.7f);
 
-        CancelBorder()
-        {
+        CancelBorder() {
             super(0, 0, 0, 8);
         }
 
+        @Override
         public void paintBorder(Component c, Graphics oldGraphics, int x,
-            int y, int width, int height)
-        {
-            SearchField field           = (SearchField) c;
-            Color       backgroundColor = field.getBackground();
-            Graphics2D  g               = (Graphics2D) oldGraphics;
+                int y, int width, int height) {
+            SearchField field = (SearchField) c;
+            Color backgroundColor = field.getBackground();
+            Graphics2D g = (Graphics2D) oldGraphics;
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+                    RenderingHints.VALUE_ANTIALIAS_ON);
 
             // Draw magnifying glass lens
             final int diskL = 9;
@@ -238,16 +240,15 @@ public class SearchField extends JTextField
             // Draw magnifying glass handle
             final int downX = (diskX + diskL) - 3;
             final int downY = (diskY + diskL) - 3;
-            final int upX   = downX + 4;
-            final int upY   = downY + 4;
+            final int upX = downX + 4;
+            final int upY = downY + 4;
             g.setColor(Color.DARK_GRAY);
             g.drawLine(downX, downY, upX, upY);
             g.drawLine(downX, downY, upX, upY);
             g.drawLine(downX + 1, downY, upX, upY);
 
-            if (field.showingPlaceholderText ||
-                    (field.getText().length() == 0))
-            {
+            if (field.showingPlaceholderText
+                    || (field.getText().length() == 0)) {
                 // Don't draw the cancel cross
                 return;
             }
@@ -273,14 +274,12 @@ public class SearchField extends JTextField
      * Handles a click on the cancel button by clearing the text and notifying
      * any ActionListeners.
      */
-    class CancelListener extends MouseInputAdapter
-    {
-        private boolean isOverButton(MouseEvent e)
-        {
+    private final class CancelListener extends MouseInputAdapter {
+
+        private boolean isOverButton(MouseEvent e) {
             // If the button is down, we might be outside the component
             // without having had mouseExited invoked.
-            if (contains(e.getPoint()) == false)
-            {
+            if (contains(e.getPoint()) == false) {
                 return false;
             }
 
@@ -292,44 +291,41 @@ public class SearchField extends JTextField
             return (innerArea.contains(e.getPoint()) == false);
         }
 
-        public void mouseDragged(MouseEvent e)
-        {
+        @Override
+        public void mouseDragged(MouseEvent e) {
             arm(e);
         }
 
-        public void mouseEntered(MouseEvent e)
-        {
+        @Override
+        public void mouseEntered(MouseEvent e) {
             arm(e);
         }
 
-        public void mouseExited(MouseEvent e)
-        {
+        @Override
+        public void mouseExited(MouseEvent e) {
             disarm();
         }
 
-        public void mousePressed(MouseEvent e)
-        {
+        @Override
+        public void mousePressed(MouseEvent e) {
             arm(e);
         }
 
-        public void mouseReleased(MouseEvent e)
-        {
-            if (armed)
-            {
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            if (armed) {
                 cancel();
             }
 
             disarm();
         }
 
-        private void arm(MouseEvent e)
-        {
+        private void arm(MouseEvent e) {
             armed = (isOverButton(e) && SwingUtilities.isLeftMouseButton(e));
             repaint();
         }
 
-        private void disarm()
-        {
+        private void disarm() {
             armed = false;
             repaint();
         }
@@ -340,32 +336,28 @@ public class SearchField extends JTextField
      * search field doesn't have the focus. The entered text returns when
      * we get the focus back.
      */
-    class PlaceholderText implements FocusListener
-    {
-        private String placeholderText;
-        private String previousText  = "";
-        private Color  previousColor;
+    private final class PlaceholderText implements FocusListener {
 
-        PlaceholderText(String placeholderText)
-        {
-            this.placeholderText     = placeholderText;
+        private String placeholderText;
+        private String previousText = "";
+        private Color previousColor;
+
+        PlaceholderText(String placeholderText) {
+            this.placeholderText = placeholderText;
             focusLost(null);
         }
 
-        public void focusGained(FocusEvent e)
-        {
+        public void focusGained(FocusEvent e) {
             setForeground(previousColor);
             setText(previousText);
             showingPlaceholderText = false;
         }
 
-        public void focusLost(FocusEvent e)
-        {
-            previousText      = getText();
-            previousColor     = getForeground();
+        public void focusLost(FocusEvent e) {
+            previousText = getText();
+            previousColor = getForeground();
 
-            if (previousText.length() == 0)
-            {
+            if (previousText.length() == 0) {
                 showingPlaceholderText = true;
                 setForeground(Color.GRAY);
                 setText(placeholderText);

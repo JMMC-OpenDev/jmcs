5fe0facd5caffd7bcf8df437b3e2120b5df31860
bourgesl <bourges.laurent@gmail.com>
2010-10-14 12:21:29 +0000

    disable the search field while processing a simbad request enable it again once the notification (error or complete) is received

--- jmcs/src/fr/jmmc/mcs/astro/star/StarResolverWidget.java
+++ jmcs/src/fr/jmmc/mcs/astro/star/StarResolverWidget.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: StarResolverWidget.java,v 1.11 2010-10-13 20:56:30 bourgesl Exp $"
+ * "@(#) $Id: StarResolverWidget.java,v 1.12 2010-10-14 12:21:29 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.11  2010/10/13 20:56:30  bourgesl
+ * corrected concurrency issues (error and query complete notifications) using EDT
+ *
  * Revision 1.10  2010/09/30 13:27:38  bourgesl
  * removed import
  *
@@ -59,132 +62,123 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
 /**
  * Store informations relative to a star.
  */
-public class StarResolverWidget extends SearchField implements Observer
-{
+public class StarResolverWidget extends SearchField implements Observer {
 
     /** default serial UID for Serializable interface */
     private static final long serialVersionUID = 1;
-
     /** Logger - register on fr.jmmc to collect all logs under this path */
     private static final Logger _logger = Logger.getLogger(
             "fr.jmmc.mcs.astro.star.StarResolverWidget");
-
     /** Container to store retrieved star properties */
     private final Star _star;
 
     /**
      * Creates a new StarResolverWidget object.
      */
-    public StarResolverWidget()
-    {
-      this(new Star());
+    public StarResolverWidget() {
+        this(new Star());
     }
-    
+
     /**
      * Creates a new StarResolverWidget object.
      *
      * @param star star model
      */
-    public StarResolverWidget(final Star star)
-    {
+    public StarResolverWidget(final Star star) {
         super("Simbad");
 
         _star = star;
         _star.addObserver(this);
 
-        addActionListener(new ActionListener()
-            {
-                public void actionPerformed(ActionEvent e)
-                {
-                    String starName = e.getActionCommand();
+        addActionListener(new ActionListener() {
 
-                    if (starName.length() > 0)
-                    {
-                        if (_logger.isLoggable(Level.INFO)) {
-                          _logger.info("Searching CDS Simbad data for star '" +
-                            starName + "'.");
-                        }
-                        StatusBar.show("searching CDS Simbad data for star '" +
-                            starName +
-                            "'... (please wait, this may take a while)");
+            public void actionPerformed(final ActionEvent e) {
+                final String starName = e.getActionCommand();
 
-                        // TODO : check concurrency issues (multiple calls running at the same time) :
-                        // TODO : reject concurrent calls on the same object :
-                        StarResolver resolver = new StarResolver(starName, _star);
-                        resolver.resolve();
+                if (starName.length() > 0) {
+                    if (_logger.isLoggable(Level.INFO)) {
+                        _logger.info("Searching CDS Simbad data for star '"
+                                + starName + "'.");
                     }
+                    StatusBar.show("searching CDS Simbad data for star '"
+                            + starName + "'... (please wait, this may take a while)");
+
+                    // Disable search field while request processing to avoid concurrent calls :
+                    setEnabled(false);
+
+                    // TODO : check concurrency issues (multiple calls running at the same time) :
+                    // TODO : reject concurrent calls on the same object :
+                    final StarResolver resolver = new StarResolver(starName, _star);
+                    resolver.resolve();
                 }
-            });
+            }
+        });
     }
 
-  /**
-   * Return the star model
-   * @return star model
-   */
-  public Star getStar() {
-    return _star;
-  }
+    /**
+     * Return the star model
+     * @return star model
+     */
+    public final Star getStar() {
+        return _star;
+    }
 
     /**
      * Automatically called on attached Star changes.
      */
-    public void update(Observable o, Object arg)
-    {
+    public final void update(final Observable o, final Object arg) {
         Star.Notification notification = Star.Notification.UNKNOWN;
 
-        if (arg != null)
-        {
+        if (arg != null) {
             notification = (Star.Notification) arg;
         }
 
-        switch (notification)
-        {
-        case QUERY_COMPLETE:
-            StatusBar.show("CDS Simbad star resolution done.");
+        switch (notification) {
+            case QUERY_COMPLETE:
+                StatusBar.show("CDS Simbad star resolution done.");
+                break;
 
-            break;
+            case QUERY_ERROR:
+                final String errorMessage = _star.consumeCDSimbadErrorMessage();
+                if (errorMessage != null) {
+                    MessagePane.showErrorMessage(
+                            "CDS Simbad problem :\n" + errorMessage);
+                }
 
-        case QUERY_ERROR:
-
-            String errorMessage = _star.consumeCDSimbadErrorMessage();
-
-            if (errorMessage != null) // An error occured
-            {
-                MessagePane.showErrorMessage(
-                        "CDS Simbad problem :\n" + errorMessage);
-            }
-
-            StatusBar.show("CDS Simbad star resolution failed.");
-
-            break;
+                StatusBar.show("CDS Simbad star resolution failed.");
+                break;
         }
+
+        // Enable search field after request processing done :
+        setEnabled(true);
     }
 
     /**
      * Main - for StarResolverWidget demonstration and test only.
+     * @param args unused
      */
-    public static void main(String[] args)
-    {
+    public static void main(final String[] args) {
         // Resolver initialization
         final Star star = new Star();
-        star.addObserver(new Observer()
-            {
-                public void update(Observable o, Object arg)
-                {
-                    _logger.severe("Star changed:\n" + star);
-                }
-            });
+        star.addObserver(new Observer() {
+
+            public void update(Observable o, Object arg) {
+                _logger.severe("Star changed:\n" + star);
+            }
+        });
 
         // GUI initialization
         JFrame frame = new JFrame();
         frame.setTitle("StarResolverWidget Demo");
 
-        Container          container   = frame.getContentPane();
-        JPanel             panel       = new JPanel();
+        // Force to exit when the frame closes :
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container container = frame.getContentPane();
+        JPanel panel = new JPanel();
         StarResolverWidget searchField = new StarResolverWidget(star);
         panel.add(searchField);
         container.add(panel);

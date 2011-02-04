9d25349a5e6ac5e7d0118c3c83a2d09f221387f2
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-02-04 16:21:05 +0000

    Extended SwingWorker to facilitate debugging and support version and Task for cancellation and consistency checks

---
+++ src/fr/jmmc/mcs/gui/task/TaskSwingWorker.java
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: TaskSwingWorker.java,v 1.1 2011-02-04 16:21:05 mella Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ *
+ */
+package fr.jmmc.mcs.gui.task;
+
+import fr.jmmc.mcs.gui.FeedbackReport;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * This class extends SwingWorker backport for Java 5 to :
+ * - define related task to cancel easily the task and its child tasks
+ * - simplify debugging / logging.
+ * Requires library :
+ * swing-worker-1.2.jar (org.jdesktop.swingworker.SwingWorker)
+ *
+ * @author bourgesl
+ *
+ * @param <T> the result type returned by this {@code TaskSwingWorker}
+ */
+public abstract class TaskSwingWorker<T> extends org.jdesktop.swingworker.SwingWorker<T, Void> {
+
+    /** Class logger */
+    protected static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(
+            TaskSwingWorker.class.getName());
+    /** flag to log debugging information */
+    protected final static boolean DEBUG_FLAG = false;
+
+    /* members */
+    /** related task */
+    private final Task task;
+    /** log prefix using the format 'SwingWorker[" + task.name + "]" + logSuffix + "@hashcode' used by debugging statements */
+    protected final String logPrefix;
+
+    /**
+     * Create a new TaskSwingWorker instance
+     * @param task related task
+     * @param logSuffix complementary suffix for log prefix
+     */
+    public TaskSwingWorker(final Task task, final String logSuffix) {
+        this.task = task;
+        this.logPrefix = (DEBUG_FLAG) ? ("SwingWorker[" + task.getName() + "]" + logSuffix + "@" + Integer.toHexString(hashCode())) : "SwingWorker";
+    }
+
+    /**
+     * Return the task related to this SwingWorker
+     * @return related task
+     */
+    public final Task getTask() {
+        return task;
+    }
+
+    @Override
+    public final String toString() {
+        return this.logPrefix;
+    }
+
+    /**
+     * Do some comutation in background
+     * @return data computed data
+     */
+    @Override
+    public final T doInBackground() {
+        if (DEBUG_FLAG) {
+            logger.info(logPrefix + ".doInBackground : START");
+        }
+
+        T data = null;
+
+        // compute the data :
+        data = this.computeInBackground();
+
+        if (isCancelled()) {
+            if (DEBUG_FLAG) {
+                logger.info(logPrefix + ".doInBackground : CANCELLED");
+            }
+            // no result if task was cancelled :
+            data = null;
+        } else {
+            if (DEBUG_FLAG) {
+                logger.info(logPrefix + ".doInBackground : DONE");
+            }
+        }
+        return data;
+    }
+
+    /**
+     * Call the refreshGUI with result if not cancelled.
+     * This code is executed by the Swing Event Dispatcher thread (EDT)
+     */
+    @Override
+    public final void done() {
+        // check if the worker was cancelled :
+        if (isCancelled()) {
+            if (DEBUG_FLAG) {
+                logger.info(logPrefix + ".done : CANCELLED");
+            }
+        } else {
+            try {
+                // Get the computed results :
+                final T data = get();
+
+                if (data == null) {
+                    if (DEBUG_FLAG) {
+                        logger.info(logPrefix + ".done : NO DATA");
+                    }
+                } else {
+                    if (DEBUG_FLAG) {
+                        logger.info(logPrefix + ".done : UI START");
+                    }
+
+                    // refresh UI with data :
+                    this.refreshUI(data);
+
+                    if (DEBUG_FLAG) {
+                        logger.info(logPrefix + ".done : UI DONE");
+                    }
+                }
+
+            } catch (InterruptedException ignore) {
+            } catch (ExecutionException ee) {
+                handleException(ee);
+            }
+        }
+    }
+
+    /**
+     * Compute operation invoked by a Worker Thread (not Swing EDT) in background
+     * Called by @see #doInBackground()
+     * @return computed data
+     */
+    public abstract T computeInBackground();
+
+    /**
+     * Refresh GUI invoked by the Swing Event Dispatcher Thread (Swing EDT)
+     * Called by @see #done()
+     * @param data computed data
+     */
+    public abstract void refreshUI(final T data);
+
+    /**
+     * Handle the execution exception that occured in the compute operation : @see #computeInBackground()
+     * This default implementation opens the feedback report (modal and do not exit on close).
+     *
+     * @param ee execution exception
+     */
+    public void handleException(final ExecutionException ee) {
+        // Show feedback report (modal and do not exit on close) :
+        new FeedbackReport(true, ee.getCause());
+    }
+}

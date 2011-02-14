e532263b4a4d1dc958cd10285f3639d4230625d1
bourgesl <bourges.laurent@gmail.com>
2011-02-14 17:10:26 +0000

    removed identifier

--- jmcs/src/fr/jmmc/mcs/gui/task/Task.java
+++ jmcs/src/fr/jmmc/mcs/gui/task/Task.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: Task.java,v 1.1 2011-02-04 16:21:50 mella Exp $"
+ * "@(#) $Id: Task.java,v 1.2 2011-02-14 17:10:26 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2011/02/04 16:21:50  mella
+ * Generic Task definition (id, name, child tasks)
+ *
  *
  */
 package fr.jmmc.mcs.gui.task;
@@ -14,11 +17,10 @@ package fr.jmmc.mcs.gui.task;
  * This class represents a task with identifier, name and child tasks
  * @author bourgesl
  */
-public final class Task {
+public final class Task
+{
 
     /* members */
-    /** task identifier */
-    private final int id;
     /** task name */
     private final String name;
     /** child tasks */
@@ -26,16 +28,21 @@ public final class Task {
 
     /**
      * Protected constructor
-     * @param id task identifier
      * @param name task name
      */
-    protected Task(final int id, final String name) {
-        this.id = id;
+    public Task(final String name)
+    {
         this.name = name;
     }
 
+    /**
+     * Return true only if the given object is a task and task names are equals
+     * @param obj other object
+     * @return true only if the given object is a task and task names are equals
+     */
     @Override
-    public boolean equals(final Object obj) {
+    public boolean equals(final Object obj)
+    {
         if (obj == null) {
             return false;
         }
@@ -43,51 +50,53 @@ public final class Task {
             return false;
         }
         final Task other = (Task) obj;
-        if (this.id != other.id) {
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
             return false;
         }
         return true;
     }
 
-    @Override
-    public int hashCode() {
-        return this.id;
-    }
-
-    @Override
-    public String toString() {
-        return "Task(" + id + ")[" + name + ']';
-    }
-
     /**
-     * Return the task identifier
-     * @return task identifier
+     * Return the hash code based on the task name
+     * @return hash code based on the task name
      */
-    public int getId() {
-        return id;
+    @Override
+    public int hashCode()
+    {
+        return (this.name != null ? this.name.hashCode() : 0);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Task[" + getName() + ']';
     }
 
     /**
      * Return the task name
      * @return task name
      */
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
     /**
-     * Return the array of child tasks
+     * Return the array of child tasks (read-only)
      * @return child tasks
      */
-    public Task[] getChildTasks() {
+    public Task[] getChildTasks()
+    {
         return childTasks;
     }
 
     /**
-     * Define the array of child tasks
+     * Define the array of child tasks.
+     * Only visible to this package
      * @param childTasks child tasks
      */
-    protected void setChildTasks(final Task[] childTasks) {
+    void setChildTasks(final Task[] childTasks)
+    {
         this.childTasks = childTasks;
     }
 }

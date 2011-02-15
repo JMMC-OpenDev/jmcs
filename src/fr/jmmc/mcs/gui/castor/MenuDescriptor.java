95921df4b5965bb197e22ccc4a80d0211b659231
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-02-15 17:01:58 +0000

    add license, jars / iconlink attribute

--- src/fr/jmmc/mcs/gui/castor/MenuDescriptor.java
+++ src/fr/jmmc/mcs/gui/castor/MenuDescriptor.java
@@ -2,7 +2,7 @@
  * This class was automatically generated with 
  * <a href="http://www.castor.org">Castor 0.9.4.3</a>, using an XML
  * Schema.
- * $Id: MenuDescriptor.java,v 1.1 2010-06-18 08:32:50 mella Exp $
+ * $Id: MenuDescriptor.java,v 1.2 2011-02-15 17:01:57 mella Exp $
  */
 
 package fr.jmmc.mcs.gui.castor;
@@ -19,7 +19,7 @@ import org.exolab.castor.xml.validators.*;
 /**
  * Class MenuDescriptor.
  * 
- * @version $Revision: 1.1 $ $Date: 2010-06-18 08:32:50 $
+ * @version $Revision: 1.2 $ $Date: 2011-02-15 17:01:57 $
  */
 public class MenuDescriptor extends org.exolab.castor.xml.util.XMLClassDescriptorImpl {
 
--- src/fr/jmmc/mcs/gui/castor/PackageDescriptor.java
+++ src/fr/jmmc/mcs/gui/castor/PackageDescriptor.java
@@ -2,7 +2,7 @@
  * This class was automatically generated with 
  * <a href="http://www.castor.org">Castor 0.9.4.3</a>, using an XML
  * Schema.
- * $Id: PackageDescriptor.java,v 1.1 2010-06-18 08:32:50 mella Exp $
+ * $Id: PackageDescriptor.java,v 1.2 2011-02-15 17:01:57 mella Exp $
  */
 
 package fr.jmmc.mcs.gui.castor;
@@ -19,7 +19,7 @@ import org.exolab.castor.xml.validators.*;
 /**
  * Class PackageDescriptor.
  * 
- * @version $Revision: 1.1 $ $Date: 2010-06-18 08:32:50 $
+ * @version $Revision: 1.2 $ $Date: 2011-02-15 17:01:57 $
  */
 public class PackageDescriptor extends org.exolab.castor.xml.util.XMLClassDescriptorImpl {
 
@@ -162,6 +162,78 @@ public class PackageDescriptor extends org.exolab.castor.xml.util.XMLClassDescri
         addFieldDescriptor(desc);
         
         //-- validation code for: _link
+        fieldValidator = new org.exolab.castor.xml.FieldValidator();
+        { //-- local scope
+            StringValidator typeValidator = new StringValidator();
+            typeValidator.setWhiteSpace("preserve");
+            fieldValidator.setValidator(typeValidator);
+        }
+        desc.setValidator(fieldValidator);
+        //-- _jars
+        desc = new org.exolab.castor.xml.util.XMLFieldDescriptorImpl(java.lang.String.class, "_jars", "jars", org.exolab.castor.xml.NodeType.Attribute);
+        desc.setImmutable(true);
+        handler = (new org.exolab.castor.xml.XMLFieldHandler() {
+            public java.lang.Object getValue( java.lang.Object object ) 
+                throws IllegalStateException
+            {
+                Package target = (Package) object;
+                return target.getJars();
+            }
+            public void setValue( java.lang.Object object, java.lang.Object value) 
+                throws IllegalStateException, IllegalArgumentException
+            {
+                try {
+                    Package target = (Package) object;
+                    target.setJars( (java.lang.String) value);
+                }
+                catch (java.lang.Exception ex) {
+                    throw new IllegalStateException(ex.toString());
+                }
+            }
+            public java.lang.Object newInstance( java.lang.Object parent ) {
+                return null;
+            }
+        } );
+        desc.setHandler(handler);
+        addFieldDescriptor(desc);
+        
+        //-- validation code for: _jars
+        fieldValidator = new org.exolab.castor.xml.FieldValidator();
+        { //-- local scope
+            StringValidator typeValidator = new StringValidator();
+            typeValidator.setWhiteSpace("preserve");
+            fieldValidator.setValidator(typeValidator);
+        }
+        desc.setValidator(fieldValidator);
+        //-- _license
+        desc = new org.exolab.castor.xml.util.XMLFieldDescriptorImpl(java.lang.String.class, "_license", "license", org.exolab.castor.xml.NodeType.Attribute);
+        desc.setImmutable(true);
+        handler = (new org.exolab.castor.xml.XMLFieldHandler() {
+            public java.lang.Object getValue( java.lang.Object object ) 
+                throws IllegalStateException
+            {
+                Package target = (Package) object;
+                return target.getLicense();
+            }
+            public void setValue( java.lang.Object object, java.lang.Object value) 
+                throws IllegalStateException, IllegalArgumentException
+            {
+                try {
+                    Package target = (Package) object;
+                    target.setLicense( (java.lang.String) value);
+                }
+                catch (java.lang.Exception ex) {
+                    throw new IllegalStateException(ex.toString());
+                }
+            }
+            public java.lang.Object newInstance( java.lang.Object parent ) {
+                return null;
+            }
+        } );
+        desc.setHandler(handler);
+        addFieldDescriptor(desc);
+        
+        //-- validation code for: _license
         fieldValidator = new org.exolab.castor.xml.FieldValidator();
         { //-- local scope
             StringValidator typeValidator = new StringValidator();

fba6651533838f32a8a4a3d284387d7e00e61af8
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2011-02-28 14:47:48 +0000

    This commit was manufactured by cvs2svn to create tag 'MAR2011/modsh'.

---
+++ modsh/src/modshExample2.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: modshExample2.sh,v 1.1 2004-09-09 09:01:52 gluck Exp $"
+#
+# who       when         what
+# --------  -----------  -------------------------------------------------------
+# gluck     09-Sep-2004  Created
+#
+#
+#*******************************************************************************
+
+#/**
+# \file
+# brief description of the shell script, which ends at this dot.
+#
+# \synopsis
+# \b \<Command Name\> [\e \<param1\> ... \e \<paramN\>] 
+#                     [\e \<option1\> ... \e \<optionN\>] 
+#
+# \param param1 : description of parameter 1, if it exists
+# \param paramN : description of parameter N, if it exists
+#
+# \n
+# \opt
+# \optname option1 : description of option 1, if it exists
+# \optname optionN : description of option N, if it exists
+# 
+# \n
+# \details
+# OPTIONAL detailed description of the shell script follows here.
+# 
+# \usedfiles
+# OPTIONAL. If files are used, for each one, name, and usage description.
+# \filename fileName1 :  usage description of fileName1
+# \filename fileName2 :  usage description of fileName2
+#
+# \n
+# \env
+# OPTIONAL. If needed, environmental variables accessed by the program. For
+# each variable, name, and usage description, as below.
+# \envvar envVar1 :  usage description of envVar1
+# \envvar envVar2 :  usage description of envVar2
+# 
+# \n
+# \warning OPTIONAL. Warning if any (software requirements, ...)
+#
+# \n
+# \ex
+# OPTIONAL. Command example if needed
+# \n Brief example description.
+# \code
+# Insert your command example here
+# \endcode
+#
+# \sa OPTIONAL. See also section, in which you can refer other documented
+# entities. Doxygen will create the link automatically. For example, 
+# \sa modshExample2.sh
+# 
+# \bug OPTIONAL. Known bugs list if it exists.
+# \bug Bug 1 : bug 1 description
+#
+# \todo OPTIONAL. Things to forsee list.
+# \todo Action 1 : action 1 description
+# 
+# */
+
+# signal trap (if any)
+
+echo "This is a second example"
+
+
+#___oOo___

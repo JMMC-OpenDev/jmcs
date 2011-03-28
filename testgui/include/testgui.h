6a227bdd7acf6f7cca9a515a8e49c484e1192998
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2011-03-28 13:47:13 +0000

    Fix directory structure and add additional files

---
+++ testgui/include/testgui.h
@@ -0,0 +1,27 @@
+#ifndef testgui_H
+#define testgui_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: testgui.h,v 1.1 2011-03-28 13:47:13 ccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * testgui general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!testgui_H*/
+
+/*___oOo___*/
---
+++ testgui/include/testguiPrivate.h
@@ -0,0 +1,30 @@
+#ifndef testguiPrivate_H
+#define testguiPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: testguiPrivate.h,v 1.1 2011-03-28 13:47:13 ccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * testgui private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+define MODULE_ID "testgui"
+
+ 
+
+#endif /*!testguiPrivate_H*/
+
+/*___oOo___*/

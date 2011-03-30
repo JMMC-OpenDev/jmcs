3b3544c6c5e1e73645be2c963f6f158b38cb8913
bourgesl <bourges.laurent@gmail.com>
2011-03-30 08:30:33 +0000

    added test libraries (fest) and custom junit templates

---
+++ testgui/src/fest/common/JmcsApplicationSetup.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: JmcsApplicationSetup.java,v 1.1 2011-03-30 08:30:33 bourgesl Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2011/03/11 12:55:35  bourgesl
+ * added fest-swing test cases for Aspro 2
+ *
+ */
+package fest.common;
+
+import fr.jmmc.mcs.gui.App;
+
+/**
+ * This utility class is useful to define the main class to launch the Swing application
+ * @author bourgesl
+ */
+public final class JmcsApplicationSetup {
+
+  /**
+   * Define the application class to launch
+   */
+  static Class<? extends App> applicationClass = null;
+  /**
+   * Define the application class to launch
+   */
+  static String[] arguments = null;
+
+  /**
+   * Define the main class to launch the Swing application without arguments
+   * @param appClass App subclass
+   */
+  public static void define(final Class<? extends App> appClass) {
+    applicationClass = appClass;
+  }
+
+  /**
+   * Define the main class to launch the Swing application with arguments
+   * @param appClass App subclass
+   * @param args main method arguments
+   */
+  public static void define(final Class<? extends App> appClass, final String... args) {
+    applicationClass = appClass;
+    arguments = args;
+  }
+
+  /**
+   * Forbidden constructor (utility class)
+   */
+  private JmcsApplicationSetup() {
+    super();
+  }
+}
---
+++ testgui/src/fest/common/JmcsFestSwingJUnitTestCase.java
@@ -0,0 +1,220 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: JmcsFestSwingJUnitTestCase.java,v 1.1 2011-03-30 08:30:33 bourgesl Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.3  2011/03/17 15:42:22  bourgesl
+ * do not use window.cleanUp as robot is already cleaned up
+ *
+ * Revision 1.2  2011/03/14 14:47:18  bourgesl
+ * added emergency abort listener associated to 'Ctrl + Shift + A' key combination
+ *
+ * Revision 1.1  2011/03/11 12:55:35  bourgesl
+ * added fest-swing test cases for Aspro 2
+ *
+ */
+package fest.common;
+
+import static org.fest.swing.launcher.ApplicationLauncher.*;
+import fr.jmmc.mcs.gui.App;
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.logging.Level;
+import org.fest.swing.core.EmergencyAbortListener;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JOptionPaneFixture;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * This class extends FestSwingCustomJUnitTestCase to start / stop one Jmcs application
+ * @author bourgesl
+ */
+public class JmcsFestSwingJUnitTestCase extends FestSwingCustomJUnitTestCase {
+
+  /* members */
+  /** application window fixture */
+  protected FrameFixture window;
+  /** emergency abort listener associated to 'Ctrl + Shift + A' key combination */
+  private EmergencyAbortListener listener;
+
+  /**
+   * Public constructor required by JUnit
+   */
+  public JmcsFestSwingJUnitTestCase() {
+    super();
+  }
+
+  /**
+   * Set up the Swing application.
+   * This method is called <strong>after</strong> executing <code>{@link #setUpOnce()}</code>.
+   */
+  @BeforeClass
+  public static void onSetUpOnce() {
+
+    // Use main thread to start Jmcs application using subclass.main() :
+    if (App.getSharedInstance() == null) {
+
+      // disable use of System.exit() :
+      App.setAvoidSystemExit(true);
+
+      if (JmcsApplicationSetup.applicationClass != null) {
+        if (logger.isLoggable(Level.INFO)) {
+          logger.info("onSetUpOnce : starting application : " + JmcsApplicationSetup.applicationClass);
+        }
+
+        if (JmcsApplicationSetup.arguments != null) {
+          if (logger.isLoggable(Level.INFO)) {
+            logger.info("onSetUpOnce : using arguments      : " + Arrays.toString(JmcsApplicationSetup.arguments));
+          }
+
+          application(JmcsApplicationSetup.applicationClass).withArgs(JmcsApplicationSetup.arguments).start();
+
+        } else {
+
+          application(JmcsApplicationSetup.applicationClass).start();
+        }
+      }
+
+      if (logger.isLoggable(Level.INFO)) {
+        logger.info("onSetUpOnce : started application = " + App.getSharedInstance());
+      }
+
+      if (App.getSharedInstance() == null) {
+        throw new RuntimeException("unable to start application : " + JmcsApplicationSetup.applicationClass);
+      }
+    }
+  }
+
+  /**
+   * Free other resources like the Swing application.
+   * This method is called <strong>after</strong> executing <code>{@link #tearDownOnce()}</code>.
+   */
+  @AfterClass
+  public static void onTearDownOnce() {
+    final App app = App.getSharedInstance();
+    if (app != null) {
+      if (logger.isLoggable(Level.INFO)) {
+        logger.info("onTearDownOnce : exit application = " + app);
+      }
+
+      app.onFinish();
+    }
+  }
+
+  /**
+   * Return the application
+   * @return application
+   */
+  protected static App getApplication() {
+    return App.getSharedInstance();
+  }
+
+  /**
+   * Define the window fixture before each JUnit Test method.
+   */
+  @Override
+  protected void onSetUp() {
+    listener = EmergencyAbortListener.registerInToolkit();
+
+    // IMPORTANT: note the call to 'robot()'
+    // we must use the Robot from FestSwingCustomTestCaseTemplate
+    window = new FrameFixture(robot(), App.getFrame());
+
+    // shows the frame to test
+    window.show();
+    window.moveToFront();
+  }
+
+  /**
+   * Clean up resources of the window fixture after each JUnit Test method.
+   */
+  @Override
+  protected void onTearDown() {
+    listener.unregister();
+
+    // robot is already cleaned up in FestSwingCustomTestCaseTemplate
+  }
+
+
+  /*
+  --- Utility methods  ---------------------------------------------------------
+   */
+  /**
+   * Close File overwrite confirm dialog clicking on "Replace" button
+   */
+  protected final void confirmDialogFileOverwrite() {
+    try {
+      // if file already exists, a confirm message appears :
+      final JOptionPaneFixture optionPane = window.optionPane();
+
+      if (optionPane != null) {
+        // confirm file overwrite :
+        optionPane.buttonWithText("Replace").click();
+      }
+
+    } catch (RuntimeException re) {
+      // happens when the confirm message does not occur :
+      if (logger.isLoggable(Level.FINE)) {
+        logger.log(Level.FINE, "lookup failure : ", re);
+      }
+    }
+  }
+
+  /**
+   * Close Save confirm dialog clicking on "Don't Save" button
+   */
+  protected final void confirmDialogDontSave() {
+    // close confirm dialog :
+    window.optionPane().buttonWithText("Don't Save").click();
+  }
+
+  /**
+   * Close any option pane
+   * @return true if a message was closed
+   */
+  protected final boolean closeMessage() {
+    try {
+      // if a message appears :
+      final JOptionPaneFixture optionPane = window.optionPane();
+
+      if (optionPane != null) {
+        // click OK :
+        optionPane.okButton().click();
+
+        return true;
+      }
+
+    } catch (RuntimeException re) {
+      // happens when the confirm message does not occur :
+      if (logger.isLoggable(Level.FINE)) {
+        logger.log(Level.FINE, "lookup failure : ", re);
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Capture a screenshot of the application window, crop it and save it using the given file name
+   * @param fileName the file name (including the png extension)
+   * @param x the X coordinate of the upper-left corner of the
+   *          specified rectangular region
+   * @param y the Y coordinate of the upper-left corner of the
+   *          specified rectangular region
+   * @param w the width of the specified rectangular region (<=0 indicates to use the width of screenshot image)
+   * @param h the height of the specified rectangular region (<=0 indicates to use the height of screenshot image)
+   */
+  protected final void saveCroppedScreenshotOf(final String fileName, final int x, final int y, final int w, final int h) {
+    final BufferedImage image = takeScreenshotOf(window);
+
+    final int width = (w <= 0) ? image.getWidth() : w;
+    final int height = (h <= 0) ? image.getHeight() : h;
+
+    final BufferedImage croppedImage = image.getSubimage(x, y, width, height);
+
+    saveImage(croppedImage, fileName);
+  }
+}

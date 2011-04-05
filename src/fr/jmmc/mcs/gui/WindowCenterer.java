0d4580014f78ef2e17c0a6f39101ab91b59ff0bd
bourgesl <bourges.laurent@gmail.com>
2011-04-05 15:21:18 +0000

    exception handling minor coding fixes

--- src/fr/jmmc/mcs/gui/WindowCenterer.java
+++ src/fr/jmmc/mcs/gui/WindowCenterer.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: WindowCenterer.java,v 1.9 2008-06-20 08:41:45 bcolucci Exp $"
+ * "@(#) $Id: WindowCenterer.java,v 1.10 2011-04-05 15:21:18 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.9  2008/06/20 08:41:45  bcolucci
+ * Remove unused imports and add class comments.
+ *
  * Revision 1.8  2008/06/17 07:50:52  bcolucci
  * Take a Window object instead of a JFrame object in order to use it on JDialog.
  *
@@ -43,37 +46,32 @@ import java.awt.Window;
 
 import java.util.logging.Logger;
 
-
 /**
  * Facility static class to properly center a window
  * on the main screen (handle multiple screen setups).
  */
 public class WindowCenterer
 {
+
     /** Logger */
     private static final Logger _logger = Logger.getLogger(WindowCenterer.class.getName());
-
-    /** X position */
-    public static int _XPOSITION = 0;
-
-    /** Y position */
-    public static int _YPOSITION = 0;
-
     /** Screen width */
     public static int _screenWidth = 0;
-
     /** Screen height */
     public static int _screenHeight = 0;
 
-    /** Get screen properties */
+    /**
+     * Get screen properties
+     * @throws NullPointerException on some platform (virtual box)
+     */
     public static void getScreenProperties() throws NullPointerException
     {
         // Get main screen size
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice      gs = ge.getDefaultScreenDevice();
-        DisplayMode         dm = gs.getDisplayMode();
-        _screenWidth           = dm.getWidth();
-        _screenHeight          = dm.getHeight();
+        GraphicsDevice gs = ge.getDefaultScreenDevice();
+        DisplayMode dm = gs.getDisplayMode();
+        _screenWidth = dm.getWidth();
+        _screenHeight = dm.getHeight();
     }
 
     /**
@@ -83,13 +81,11 @@ public class WindowCenterer
      *
      * @param frameToCenter the JFrame we want to center
      */
-    public static void centerOnMainScreen(Window frameToCenter)
-        throws NullPointerException
+    public static void centerOnMainScreen(final Window frameToCenter)
     {
         // next try catch is mandatory to catach null pointer excpetion that
         // can occure on some virtual machine emulation (at least virtualBox)
-        try
-        {
+        try {
             getScreenProperties();
 
             // Dimension of the JFrame
@@ -99,31 +95,40 @@ public class WindowCenterer
             Point point = getCenteringPoint(frameSize);
 
             frameToCenter.setLocation(point);
+
             _logger.fine("The window has been centered");
-        }
-        catch (Exception ex)
-        {
+
+        } catch (NullPointerException npe) {
             _logger.warning("Could not center window");
         }
     }
 
     /**
-     * Returns the centered point in order
-     * to center a frame on the screen
+     * Returns the centered point in order to center a frame on the screen
+     * @param frameDimension frame size
      * @return centered point
+     * @throws NullPointerException on some platform (virtual box)
      */
-    public static Point getCenteringPoint(Dimension frameDimension)
-        throws NullPointerException
+    public static Point getCenteringPoint(final Dimension frameDimension)
+            throws NullPointerException
     {
         getScreenProperties();
 
-        int _XPOSITION = (_screenWidth - frameDimension.width) / 2;
-        _XPOSITION = Math.max(_XPOSITION, 0);
+        int x = (_screenWidth - frameDimension.width) / 2;
+        x = Math.max(x, 0);
 
-        int _YPOSITION = (_screenHeight - frameDimension.height) / 2;
-        _YPOSITION = Math.max(_YPOSITION, 0);
+        int y = (_screenHeight - frameDimension.height) / 2;
+        y = Math.max(y, 0);
 
-        return new Point(_XPOSITION, _YPOSITION);
+        return new Point(x, y);
+    }
+
+    /**
+     * Private constructor
+     */
+    private WindowCenterer()
+    {
+        super();
     }
 }
 /*___oOo___*/

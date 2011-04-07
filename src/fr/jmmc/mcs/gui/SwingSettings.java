214e2bf44b1d2a43b0aaa52e23a51b2bd672339c
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-04-07 14:26:02 +0000

    Perform real job only on first call, just ignore following

--- src/fr/jmmc/mcs/gui/SwingSettings.java
+++ src/fr/jmmc/mcs/gui/SwingSettings.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: SwingSettings.java,v 1.4 2011-04-07 14:08:27 mella Exp $"
+ * "@(#) $Id: SwingSettings.java,v 1.5 2011-04-07 14:26:02 mella Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2011/04/07 14:08:27  mella
+ * install ExceptionHandler into setup() call
+ *
  * Revision 1.3  2011/04/07 13:53:32  mella
  * Javadoc
  *
@@ -46,10 +49,18 @@ public class SwingSettings {
     /** logger */
     private final static Logger logger = Logger.getLogger(SwingSettings.class.getName());
 
+    /** flag to prevent multiple code execution */
+    private static boolean alreadyDone=false;
+
     /**
      * Init maximum of things to get uniform application running inn the scientific context.
+     * Init are done only on the first call of this method ( which should be from a main method )
      */
     public static void setup() {
+
+        if(alreadyDone){
+            return;
+        }
 
         setMandatory();
         setSwingDefaults();
@@ -58,13 +69,7 @@ public class SwingSettings {
         // Install exception handlers :
         MCSExceptionHandler.installSwingHandler();
 
-        /* in the future ?
-        initLookAndFeel();
-
-        changeUIDefaults();
-
-        changeUIFont();
-         */
+        alreadyDone=true;
     }
 
     /**
@@ -122,10 +127,7 @@ public class SwingSettings {
             }
         }
 
-        if (SystemUtils.IS_OS_MAC_OSX) {
-            // TODO application name : (if it is not too early in the startup phases ??
-            //System.setProperty("com.apple.mrj.application.apple.menu.about.name", App.xxx);
-
+        if (SystemUtils.IS_OS_MAC_OSX) {            
             // always use screen menuBar :
             System.setProperty("apple.laf.useScreenMenuBar", "true");
             if (logger.isLoggable(Level.FINE)) {
@@ -134,88 +136,5 @@ public class SwingSettings {
         }
 
     }
-    /**
-     * 2 - Initialises the swing look and feel (using the Configuration.FORCE_LOOK_AND_FEEL [force.laf])
-     * and use GTK for the GUI on linux platform (if the Configuration.LINUX_FORCE_GTK [linux.forceGTK] = true)
-     * @param conf global configuration
-
-    private static void initLookAndFeel() {
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.initLookAndFeel : enter");
-    }
-    String laf = conf.getProperty(Configuration.FORCE_LOOK_AND_FEEL);
-    if (laf == null) {
-    if (conf.isLinux() && conf.getBoolean(Configuration.LINUX_FORCE_GTK)) {
-    laf = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
-    }
-    }
-    if (laf != null) {
-    if (log.isInfoEnabled()) {
-    log.info("initLookAndFeel : " + laf);
-    }
-    try {
-    UIManager.setLookAndFeel(laf);
-    } catch (Exception e) {
-    log.error("initLookAndFeel : unable to set look and feel : " + laf, e);
-    }
-    }
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.initLookAndFeel : exit");
-    }
-    }
-
-    /**
-     * 3 - 
-     *
-    private static void changeUIDefaults() {
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.changeUIDefaults : enter");
-    }
-
-    JFrame.setDefaultLookAndFeelDecorated(false);
-
-    ToolTipManager.sharedInstance().setInitialDelay(100);
-    ToolTipManager.sharedInstance().setDismissDelay(30000);
-
-    // Mode TEST = developper tests
-    SimpleFrame.setTestMode(conf.getBoolean(Configuration.MODE_TEST));
-
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.changeUIDefaults : exit");
-    }
-    }
-
-    /**
-     * 4 - Use small fonts if the Configuration.FONT_SMALL [font.small] = true
-     * @param conf global configuration
-     *
-    private static void changeUIFont() {
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.changeUIFont : enter");
-    }
-
-    if (conf.getBoolean(Configuration.FONT_SMALL)) {
-    final Enumeration e = UIManager.getDefaults().keys();
-
-    String s;
-    if (log.isDebugEnabled()) {
-    log.debug("UIManager defaults :");
-    }
-    while (e.hasMoreElements()) {
-    s = e.nextElement().toString();
-    if (s.matches("[a-zA-z]+.font")) {
-    UIManager.put(s, new Font("Bitstream Vera Sans", Font.PLAIN, 10));
-    }
-    if (log.isDebugEnabled()) {
-    log.debug(s + " = " + UIManager.getString(s));
-    }
-    }
-    UIManager.put("ComboBox.font", new Font("Bitstream Sans Mono", Font.PLAIN, 11));
-    }
-    if (log.isDebugEnabled()) {
-    log.debug("SwingAdapter.changeUIFont : exit");
-    }
-    }
-
-     */
+  
 }

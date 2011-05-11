6eceb092f9172e0e9c60d1f7309fa1d912eead9f
sly74fr <slymacdev@free.fr>
2011-05-11 13:18:32 +0000

    Strenghten reliability.

--- ctoo/src/ctooCleanStandardHeader.sh
+++ ctoo/src/ctooCleanStandardHeader.sh
@@ -28,26 +28,39 @@
 # @sa http://stackoverflow.com/questions/151677/tool-for-adding-license-headers-to-source-files
 # */
 
-# Print usage
+# Print script usage.
 function printUsage () {
     scriptName=`basename $0 .sh`
     echo -e "Usage: $scriptName module"
     exit 1;
 }
 
-# Cut $1 file content between $2 and $3 line numbers, and add $4 header instead
+# Cut $1 file content between $2 and $3 lines, and add $4 header instead.
 function fileCut () {
     printf "Removing header from %-50s ... " "'$FILE'"
 
+    # If any function args is missing
     if [ $# -ne 4 ]
     then
         echo "SKIPPED (header not found)."
-        return
+        return 1
+    fi
+
+    if [ ! -f $4 ]
+    then
+        echo "ERROR (template not found)."
+        exit 2
+    fi
+
+    if [ ! -f $1 ]
+    then
+        echo "ERROR (file not found)."
+        exit 3
     fi
 
     TMP_FILE=`mktemp`
 
-    # Keep stuff above previous header
+    # Save any stuff above previous header
     START_LINE=`expr $2 - 1`
     if [ $START_LINE -ne 0 ]
     then
@@ -57,32 +70,33 @@ function fileCut () {
     # Add new header
     cat $4 >> $TMP_FILE
 
-    # Keep below previous header
+    # Save all the stuff below previous header
     END_LINE=`expr $3 + 1`
     tail --lines=+$END_LINE $FILE >> $TMP_FILE
 
+    # Replace old file with new cleaned file
     mv -f $TMP_FILE $1
 
     echo "DONE (lines $2 to $3)."
 
-    return;
+    return 0
 }
 
 
-# Test parameter validity
+# Test CLI parameter validity.
 if [ ! -d $1 ]
 then
     printUsage
 fi
 
-# Usage warning
+# Usage warning.
 echo -e "WARNING : This script overwrites source files."
 echo -e "          Be sure to have backups at hand if anything goes wrong !!!"
 echo
 echo -e "Press enter to continue or ^C to abort."
 read
 
-# Find Templates directory
+# Find Templates directory.
 if [ -d ../templates/forCoding ]
 then
     TEMPLATES=../templates
@@ -93,43 +107,46 @@ else
     TEMPLATES=$MCSROOT/templates
 fi
 
-# C/C++/Java/module.doc files handling
+# C/C++/Java/module.doc files handling.
 NEW_HEADER=$TEMPLATES/forCoding/svn-header.template
 FILELIST=`find "$1" -name \*.h -print -or -name \*.c -print -or -name \*.cpp -print -or -name \*.java -print -or -name \*.doc -print`
 for FILE in $FILELIST;
 do
-    # Find '/***...***'
+    # Find first '/***...***'
     START_LINE=`grep -hn "^/\*\{70,\}$" $FILE | cut -d: -f1`
-    # Find '[ |*]***...**/'
+
+    # Find second ' ***...**/' or '***...**/'
     END_LINE=`grep -hn "^[ ,\*]\*\{70,\}/$" $FILE | cut -d: -f1`
 
     fileCut $FILE $START_LINE $END_LINE $NEW_HEADER
 done
 
-# Shell Scripts/Makefile/Config handling
+# Shell Scripts/Makefile/Config handling.
 NEW_HEADER=$TEMPLATES/forMakefile/svn-header.template
 FILELIST=`find "$1" -name \*.sh -print -or -name \Makefile -print -or -name \*.cfg -print`
 for FILE in $FILELIST;
 do
-    # Find '#***...***'
+    # Find first '#***...***'
     START_LINE=`grep -hn "^#\*\{70,\}$" $FILE | head -1 | cut -d: -f1`
-    # Find '#***...**[*|/]'
+
+    # Find second '#***...***' or  '#***...**/'
     END_LINE=`grep -hn "^#\*\{70,\}[*,/]$" $FILE | tail -1 | cut -d: -f1`
 
     fileCut $FILE $START_LINE $END_LINE $NEW_HEADER
 done
 
-# XML handling
+# XML/CDF handling.
 NEW_HEADER=$TEMPLATES/forDocumentation/svn-header.template
 FILELIST=`find "$1" -name \*.xml -print -or -name \*.cdf -print`
 for FILE in $FILELIST;
 do
     # Find first '***...***'
     START_LINE=`grep -hn "^\*\{70,\}$" $FILE | head -1 | cut -d: -f1`
+
     # Find second '***...***'
     END_LINE=`grep -hn "^\*\{70,\}$" $FILE | tail -1 | cut -d: -f1`
 
-    # If no closing '***...***' found (CDF case)
+    # If no closing '***...***' found (CDF file case)
     if [ -n "$START_LINE" ]
     then
         if [ "$START_LINE" -eq "$END_LINE" ]
@@ -142,5 +159,8 @@ do
 
     fileCut $FILE $START_LINE $END_LINE $NEW_HEADER
 done
+
+# Everything went fine !
+exit 0
 
 #___oOo___

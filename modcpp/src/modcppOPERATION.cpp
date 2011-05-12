f60c4cbbc0c2b26c4b4ffe902c208bedaff904c8
sly74fr <slymacdev@free.fr>
2011-05-12 12:13:19 +0000

    Replaced CVS file headers.

--- modcpp/src/modcppOPERATION.cpp
+++ modcpp/src/modcppOPERATION.cpp
@@ -1,53 +1,5 @@
 /*******************************************************************************
- * JMMC project
- *
- * "@(#) $Id: modcppOPERATION.cpp,v 1.7 2006-05-11 13:04:56 mella Exp $"
- *
- * History
- * -------
- * $Log: not supported by cvs2svn $
- * Revision 1.6  2006/02/15 15:10:56  mella
- * Add missing ; (
- *
- * Revision 1.5  2006/02/15 15:10:00  mella
- * Return mcsFAILURE after divededbyzero error and changed errAdd parameter
- *
- * Revision 1.4  2005/02/22 09:28:40  gluck
- * Updated \return comment
- *
- * Revision 1.3  2005/02/15 10:48:15  gzins
- * Changed SUCCESS/FAILURE to mcsSUCCESS/mcsFAILURE
- *
- * Revision 1.2  2005/02/15 10:40:17  gzins
- * Added CVS log as file modification history
- *
- * gluck     06-Jul-2004  Created
- *
- * IMPORTANT :
- * To make your own documentation, you have to substitute the general or
- * example comments, with your specific comments.
- * 
- * IMPORTANT:
- * To make AUTOMATIC DOCUMENTATION GENERATION by doxygen, you have to insert
- * your code documentation (about file, methods, define, enumeration, ...) as
- * shown below, in the special documentation blocks (beginning with 1 slash and
- * 2 stars), adding or deleting markers as needed.
- * Nevertheless, you also have to comment the code as usually.  For more
- * informations, you can report to Programming Standards (JRA4-PRO-2000-0001),
- * or doxygen documentation.
- * 
- * IMPORTANT
- * Each time (except in certain case) there is a brief and a detailed
- * description, THE BRIEF DESCRIPTION IS A UNIQUE SENTENCE, WHICH ENDS AT THE
- * FIRST DOT FOLLOWED BY A SPACE OR A NEWLINE.
- *
- * REMARKS
- * The documentation below, shows some possibilities of doxygen. The general
- * format of this documentation is recommended to make the documentation
- * easily. Some documentation lines are strongly recommended to get rapidly a
- * quite good documentation. Some others are optinonal, depending on the need.
- * They will be pointed out with the word OPTIONAL.
- *
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
  ******************************************************************************/
 
 /**

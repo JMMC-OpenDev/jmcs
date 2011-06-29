508bbd75bc4db9f47e8c3b25c234dd416d241304
bourgesl <bourges.laurent@gmail.com>
2011-06-29 14:16:51 +0000

    fixed const parameters

--- misc/include/miscFile.h
+++ misc/include/miscFile.h
@@ -28,35 +28,35 @@ extern "C" {
  * Pubic functions declaration
  */
  
-mcsCOMPL_STAT miscGetEnvVarValue    (const char       *envVarName,
-                                     char             *envVarValueBuffer,
-                                     mcsUINT32         envVarValueBufferLength);
+mcsCOMPL_STAT miscGetEnvVarValue    (const char*  envVarName,
+                                     char*        envVarValueBuffer,
+                                     mcsUINT32    envVarValueBufferLength);
 
-mcsCOMPL_STAT miscGetEnvVarIntValue (const char       *envVarName,
-                                     mcsINT32         *envVarIntValue);
+mcsCOMPL_STAT miscGetEnvVarIntValue (const char*  envVarName,
+                                     mcsINT32*    envVarIntValue);
 
-char *        miscGetFileName       (const char       *fullPath);
+char*         miscGetFileName       (const char*  fullPath);
 
-char *        miscGetExtension      (char             *fullPath);
+char*         miscGetExtension      (const char*  fullPath);
 
-mcsCOMPL_STAT miscYankExtension     (char             *fullPath,
-                                     char             *extension);
+mcsCOMPL_STAT miscYankExtension     (char*        fullPath,
+                                     const char*  extension);
 
-mcsCOMPL_STAT miscYankLastPath      (char             *path);
+mcsCOMPL_STAT miscYankLastPath      (char*        path);
 
-char*         miscResolvePath       (const char       *orginalPath);
+char*         miscResolvePath       (const char*  orginalPath);
 
-mcsLOGICAL    miscFileExists        (const char       *fullPath,
-                                     mcsLOGICAL        addError);
+mcsLOGICAL    miscFileExists        (const char*  fullPath,
+                                     mcsLOGICAL   addError);
 
-char*         miscLocateFileInPath  (const char       *path,
-                                     const char       *fileName);
+char*         miscLocateFileInPath  (const char*  path,
+                                     const char*  fileName);
 
-char*         miscLocateFile        (const char       *fileName);
+char*         miscLocateFile        (const char*  fileName);
 
-char*         miscLocateDir         (const char       *dirName);
+char*         miscLocateDir         (const char*  dirName);
 
-char*         miscLocateExe         (const char       *exeName);
+char*         miscLocateExe         (const char*  exeName);
 
 #ifdef __cplusplus
 }

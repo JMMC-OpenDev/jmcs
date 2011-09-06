bbfd04f3bbc9e3bcd870b3611ee66dc3317d0510
sly74fr <slymacdev@free.fr>
2011-09-06 13:50:14 +0000

    Refactoring.

--- test/fr/jmmc/jmcs/util/TestFileUtils.java
+++ test/fr/jmmc/jmcs/util/TestFileUtils.java
@@ -3,7 +3,6 @@
  ******************************************************************************/
 package fr.jmmc.jmcs.util;
 
-import fr.jmmc.mcs.util.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;

1305eb37d8f30cf5e405ac7be2809027d98f2acc
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-09-13 07:10:12 +0000

    add javac compiler version

--- mkf/src/mkfMakeJarDependencies.sh
+++ mkf/src/mkfMakeJarDependencies.sh
@@ -102,7 +102,8 @@ then
     echo "../lib/${jarName}.jar: ${oList}"
     echo "	@echo \"== Making ${jarName}.jar\""
     echo "	\$(AT)mkdir -p ${destDir}"
-    echo "	\$(AT)echo  \" with javac compiler flags : '${compilerFlags}'\""
+    echo "	\$(AT)javac -version"
+    echo "	\$(AT)echo  \" with compiler flags : '${compilerFlags}'\""
     echo "	\$(AT)${PURIFY_ADDITION} CLASSPATH=`mkfMakeJavaClasspath`:.; export CLASSPATH ; ${JAVAC} ${compilerFlags} -d ${destDir} \$?"
     echo "	\$(AT)(cd ${destDir}; jar cf ../../lib/${jarName}.jar ${cList} )"
 

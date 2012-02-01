1e15930a3a384e8d83efca39c4a83313d3366d6f
bourgesl <bourges.laurent@gmail.com>
2012-02-01 10:32:48 +0000

    added methods getPreference(key, ignoreMissing) to avoid throwing MissingPreferenceException when the preference value is missing fixed DismissableMessagePane to ignore missing preference values

--- src/fr/jmmc/jmcs/gui/DismissableMessagePane.java
+++ src/fr/jmmc/jmcs/gui/DismissableMessagePane.java
@@ -31,13 +31,12 @@ public final class DismissableMessagePane {
      * @param preferences Reference to the dedicated Preferences singleton
      * @param preferenceName Name of the preference related to this message
      */
-    public final static void show(final String message,
-            final Preferences preferences, final String preferenceName) {
+    public static void show(final String message, final Preferences preferences, final String preferenceName) {
 
-        final String dontShowPreferenceName = "MCSGUI.DismissableMessagePane."
-                + preferenceName + ".dontShow";
+        final String dontShowPreferenceName = "MCSGUI.DismissableMessagePane." + preferenceName + ".dontShow";
         try {
-            boolean dontShow = preferences.getPreferenceAsBoolean(dontShowPreferenceName);
+            // return false if the preference value is missing:
+            boolean dontShow = preferences.getPreferenceAsBoolean(dontShowPreferenceName, true);
 
             if (!dontShow) {
                 final JCheckBox checkbox = new JCheckBox("Do not show this message again.");

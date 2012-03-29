ce63afd5ee04b9e4a8d1219ae4da59f2e481dcfc
sly74fr <slymacdev@free.fr>
2012-03-29 14:28:59 +0000

    Added meta data labels.

--- jmcs/src/fr/jmmc/jmcs/network/interop/SampMetaData.java
+++ jmcs/src/fr/jmmc/jmcs/network/interop/SampMetaData.java
@@ -15,35 +15,35 @@ public enum SampMetaData {
 
     // Standard SAMP meta data identifiers
     /** A one word title for the application */
-    NAME("samp.name"),
+    NAME("samp.name", "Name"),
     /** A short description of the application, in plain text */
-    DESCRIPTION_TEXT("samp.description.text"),
+    DESCRIPTION_TEXT("samp.description.text", "Description"),
     /** A description of the application, in HTML */
-    DESCRIPTION_HTML("samp.description.html"),
+    DESCRIPTION_HTML("samp.description.html", "Description"),
     /** The URL of an icon in png, gif or jpeg format */
-    ICON_URL("samp.icon.url"),
+    ICON_URL("samp.icon.url", "Icon"),
     /** The URL of a documentation web page */
-    DOCUMENTATION_URL("samp.documentation.url"),
+    DOCUMENTATION_URL("samp.documentation.url", "Documentation"),
     // Extended SAMP meta data identifiers
     /** Get the application JNLP URL */
-    JNLP_URL("x-samp.jnlp.url"),
+    JNLP_URL("x-samp.jnlp.url", "JNLP"),
     /** Get the application home page URL */
-    HOMEPAGE_URL("x-samp.homepage.url"),
+    HOMEPAGE_URL("x-samp.homepage.url", "Homepage"),
     /** Get the application release notes URL */
-    RELEASENOTES_URL("x-samp.releasenotes.url"),
+    RELEASENOTES_URL("x-samp.releasenotes.url", "Release Notes"),
     /** Get the application RSS feed URL */
-    RSS_URL("x-samp.rss.url"),
+    RSS_URL("x-samp.rss.url", "Hot News"),
     /** Get the application FAQ URL */
-    FAQ_URL("x-samp.faq.url"),
+    FAQ_URL("x-samp.faq.url", "FAQ"),
     /** Get the application authors */
-    AUTHORS("x-samp.authors"),
+    AUTHORS("x-samp.authors", "Authors"),
     /** Get the application release version */
-    RELEASE_VERSION("x-samp.release.version"),
+    RELEASE_VERSION("x-samp.release.version", "Version"),
     /** Get the application release date */
-    RELEASE_DATE("x-samp.release.date"),
+    RELEASE_DATE("x-samp.release.date", "Release Date"),
     // Private JMMC SAMP capabilities are prefixed with application name:
     /** Undefined */
-    UNKNOWN("UNKNOWN");
+    UNKNOWN("UNKNOWN", null);
 
     /** Blanking value for undefined Strings (null, ...) */
     public static final String UNKNOWN_METADATA_ID = "UNKNOWN";
@@ -64,22 +64,33 @@ public enum SampMetaData {
     /* members */
     /** Store the SAMP raw meta data identifier */
     private final String _metaDataId;
+    /** Store the SAMP raw meta data human-readable label */
+    private final String _label;
 
     /**
      * Constructor
      * @param metaDataId samp meta data identifier
      */
-    SampMetaData(final String metaDataId) {
+    SampMetaData(final String metaDataId, final String label) {
         _metaDataId = (metaDataId == null) ? UNKNOWN_METADATA_ID : metaDataId;
+        _label = label;
         SampMetaData.SampMetaDataNastyTrick.TYPES.put(_metaDataId, this);
     }
 
     /**
-     * Return the samp meta data identifier
+     * Return the SAMP meta data identifier
      * @return samp meta data identifier
      */
     public String id() {
         return _metaDataId;
+    }
+
+    /**
+     * Return the SAMP meta data human-readable label
+     * @return samp meta data label, or null otherwise
+     */
+    public String getLabel() {
+        return _label;
     }
 
     /**

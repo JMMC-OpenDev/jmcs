37900c90511ba9384261b21e7baa523e7938a1f2
sly74fr <slymacdev@free.fr>
2012-03-30 09:27:40 +0000

    Moved GenericJTree to fr.jmmc.jmcs.gui.component.

--- jmcs/src/fr/jmmc/jmcs/gui/component/GenericJTree.java
+++ jmcs/src/fr/jmmc/jmcs/gui/component/GenericJTree.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
  ******************************************************************************/
-package fr.jmmc.jmcs.gui;
+package fr.jmmc.jmcs.gui.component;
 
 import java.util.Enumeration;
 import org.slf4j.Logger;

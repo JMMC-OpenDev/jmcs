e5ad5eb24c1c343d622fc0046e3d3c07d6a94519
sly74fr <slymacdev@free.fr>
2012-04-12 13:04:02 +0000

    Automatically tagged AppLauncher in version AL_V1_0b11 (mkfRepoUtil)

---
+++ jmcs/src/fr/jmmc/jmcs/network/interop/SampMetaData.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.network.interop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Enumeration of all different SAMP meta data identifiers.
+ * 
+ * @author Sylvain LAFRASSE.
+ */
+public enum SampMetaData {
+
+    // Standard SAMP meta data identifiers
+    /** A one word title for the application */
+    NAME("samp.name", "Name"),
+    /** A short description of the application, in plain text */
+    DESCRIPTION_TEXT("samp.description.text", "Description"),
+    /** A description of the application, in HTML */
+    DESCRIPTION_HTML("samp.description.html", "Description"),
+    /** The URL of an icon in png, gif or jpeg format */
+    ICON_URL("samp.icon.url", "Icon"),
+    /** The URL of a documentation web page */
+    DOCUMENTATION_URL("samp.documentation.url", "Documentation"),
+    // Extended SAMP meta data identifiers
+    /** Get the application JNLP URL */
+    JNLP_URL("x-samp.jnlp.url", "JNLP"),
+    /** Get the application home page URL */
+    HOMEPAGE_URL("x-samp.homepage.url", "Homepage"),
+    /** Get the application release notes URL */
+    RELEASENOTES_URL("x-samp.releasenotes.url", "Release Notes"),
+    /** Get the application RSS feed URL */
+    RSS_URL("x-samp.rss.url", "Hot News"),
+    /** Get the application FAQ URL */
+    FAQ_URL("x-samp.faq.url", "FAQ"),
+    /** Get the application authors */
+    AUTHORS("x-samp.authors", "Authors"),
+    /** Get the application release version */
+    RELEASE_VERSION("x-samp.release.version", "Version"),
+    /** Get the application release date */
+    RELEASE_DATE("x-samp.release.date", "Release Date"),
+    // Private JMMC SAMP capabilities are prefixed with application name:
+    /** Undefined */
+    UNKNOWN("UNKNOWN", null);
+    /** Blanking value for undefined Strings (null, ...) */
+    public static final String UNKNOWN_METADATA_ID = "UNKNOWN";
+    /** AppLauncher prefix for custom samp meta data id */
+    public final static String APP_LAUNCHER_PREFIX = "fr.jmmc.applauncher.";
+    /** special value for our client stubs */
+    public final static String STUB_TOKEN = "STUB";
+
+    /**
+     * Return the Samp meta data key 'fr.jmmc.applauncher.<clientName>'
+     * @param name client name
+     * @return 'fr.jmmc.applauncher.<clientName>'
+     */
+    public static String getStubMetaDataId(final String name) {
+        return APP_LAUNCHER_PREFIX + name;
+    }
+
+    /* members */
+    /** Store the SAMP raw meta data identifier */
+    private final String _metaDataId;
+    /** Store the SAMP raw meta data human-readable label */
+    private final String _label;
+
+    /**
+     * Constructor
+     * @param metaDataId samp meta data identifier
+     */
+    SampMetaData(final String metaDataId, final String label) {
+        _metaDataId = (metaDataId == null) ? UNKNOWN_METADATA_ID : metaDataId;
+        _label = label;
+        SampMetaData.SampMetaDataNastyTrick.TYPES.put(_metaDataId, this);
+    }
+
+    /**
+     * Return the SAMP meta data identifier
+     * @return samp meta data identifier
+     */
+    public String id() {
+        return _metaDataId;
+    }
+
+    /**
+     * Return the SAMP meta data human-readable label
+     * @return samp meta data label, or null otherwise
+     */
+    public String getLabel() {
+        return _label;
+    }
+
+    /**
+     * Gives back the SAMP meta data of the corresponding identifier.
+     *
+     * For example:
+     * SampMetaData.fromMetaDataId("samp.name") == SampMetaData.NAME;
+     * SampMetaData.fromMetaDataId("toto") == SampMetaData.UNKNOWN;
+     * SampMetaData.fromMetaDataId(null) == SampMetaData.UNKNOWN;
+     *
+     * @param metaDataId metaDataId of the sought SampCapability.
+     *
+     * @return the SampMetaData object corresponding to the given meta data id, SampMetaData.UNKNOWN otherwise.
+     */
+    public static SampMetaData fromMetaDataId(final String metaDataId) {
+        if (metaDataId == null) {
+            return UNKNOWN;
+        }
+
+        final SampMetaData metaData = SampMetaData.SampMetaDataNastyTrick.TYPES.get(metaDataId);
+        if (metaData == null) {
+            return UNKNOWN;
+        }
+
+        return metaData;
+    }
+
+    /**
+     * To get over Java 1.5 limitation prohibiting static members in enum (initialization order hazard).
+     *
+     * @sa http://www.velocityreviews.com/forums/t145807-an-enum-mystery-solved.html
+     * @sa http://www.jroller.com/ethdsy/entry/static_fields_in_enum
+     */
+    private final static class SampMetaDataNastyTrick {
+
+        /** cached map of SampMetaData keyed by id */
+        static final Map<String, SampMetaData> TYPES = new HashMap<String, SampMetaData>(16);
+
+        /**
+         * Forbidden constructor : utility class
+         */
+        private SampMetaDataNastyTrick() {
+        }
+    }
+
+    /**
+     * For test and debug purpose only.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        // For each catalog in the enum
+        for (SampMetaData metaData : SampMetaData.values()) {
+            String id = metaData.id();
+            String label = metaData.getLabel();
+            System.out.println("SampMetaData '" + metaData + "' has id '" + id + "' and label '" + label + "' : match '" + (metaData == SampMetaData.fromMetaDataId(id) ? "OK" : "FAILED") + "'.");
+        }
+
+        SampMetaData tmp;
+        String id;
+
+        id = "toto";
+        tmp = SampMetaData.fromMetaDataId(id);
+        System.out.println("'" + id + "' => '" + tmp + "'.");
+        id = null;
+        tmp = SampMetaData.fromMetaDataId(id);
+        System.out.println("'" + id + "' => '" + tmp + "'.");
+    }
+}
---
+++ jmcs/test/fr/jmmc/jmcs/util/TestJide.java
@@ -0,0 +1,302 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.util;
+
+/*
+ * @(#)CheckBoxTreeDemo.java 9/10/2005
+ *
+ * Copyright 2002 - 2005 JIDE Software Inc. All rights reserved.
+ */
+import com.jidesoft.icons.IconsFactory;
+import com.jidesoft.plaf.LookAndFeelFactory;
+import com.jidesoft.range.Category;
+import com.jidesoft.swing.*;
+
+import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * Demoed Component: {@link com.jidesoft.swing.CheckBoxTree} <br> Required jar files: jide-common.jar, jide-grids.jar
+ * <br> Required L&F: any L&F
+ */
+public class TestJide extends JFrame {
+
+    private static final long serialVersionUID = 6399345108812286281L;
+    private CheckBoxTree _tree;
+    private static final String SINGLE_SELECTION = "Single Selection";
+    private static final String CONTIGUOUS_SELECTION = "Contiguous Selection";
+    private static final String DISCONTIGUOUS_SELECTION = "Discontiguous Selection";
+
+    public TestJide() {
+        super("TestJide");
+        setLayout(new BorderLayout());
+        add(getDemoPanel(), BorderLayout.WEST);
+        add(getOptionsPanel(), BorderLayout.CENTER);
+        pack();
+        setVisible(true);
+
+    }
+
+    public Component getOptionsPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new JideBoxLayout(panel, BoxLayout.Y_AXIS, 3));
+
+        final JButton selectAll = new JButton(new AbstractAction("Select All") {
+
+            private static final long serialVersionUID = -5580913906799074020L;
+
+            public void actionPerformed(ActionEvent e) {
+                if (_tree.getCheckBoxTreeSelectionModel().isDigIn()) {
+                    _tree.getCheckBoxTreeSelectionModel().setSelectionPath(new TreePath(_tree.getModel().getRoot()));
+                }
+            }
+        });
+        JButton clearAll = new JButton(new AbstractAction("Clear All") {
+
+            private static final long serialVersionUID = -2500587806953898010L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.getCheckBoxTreeSelectionModel().clearSelection();
+            }
+        });
+
+        final JCheckBox digIn = new JCheckBox("Dig In");
+        digIn.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = 3184279982208173561L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.getCheckBoxTreeSelectionModel().setDigIn(digIn.isSelected());
+                selectAll.setEnabled(digIn.isSelected());
+            }
+        });
+        digIn.setSelected(_tree.getCheckBoxTreeSelectionModel().isDigIn());
+
+        final JCheckBox checkBoxEnabled = new JCheckBox("CheckBox Enabled");
+        checkBoxEnabled.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = 7752042312121853308L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.setCheckBoxEnabled(checkBoxEnabled.isSelected());
+            }
+        });
+        checkBoxEnabled.setSelected(_tree.isCheckBoxEnabled());
+
+        final JCheckBox clickInCheckBoxOnly = new JCheckBox("Click only valid in CheckBox");
+        clickInCheckBoxOnly.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = 5234198740430142668L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.setClickInCheckBoxOnly(clickInCheckBoxOnly.isSelected());
+            }
+        });
+        clickInCheckBoxOnly.setSelected(_tree.isClickInCheckBoxOnly());
+
+        final JCheckBox treeEnabled = new JCheckBox("Tree Enabled ");
+        treeEnabled.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = -1027526532901305794L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.setEnabled(treeEnabled.isSelected());
+            }
+        });
+        treeEnabled.setSelected(_tree.isEnabled());
+
+        final JCheckBox selectPartialFirst = new JCheckBox("Select Partial ");
+        selectPartialFirst.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = 6687098092701174807L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.setSelectPartialOnToggling(selectPartialFirst.isSelected());
+            }
+        });
+        selectPartialFirst.setSelected(_tree.isSelectPartialOnToggling());
+
+        String[] selectionModes = new String[]{
+            TestJide.SINGLE_SELECTION,
+            TestJide.CONTIGUOUS_SELECTION,
+            TestJide.DISCONTIGUOUS_SELECTION
+        };
+
+        JComboBox comboBox = new JComboBox(selectionModes);
+        comboBox.addItemListener(new ItemListener() {
+
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED && e.getItem() instanceof String) {
+                    if ((e.getItem()).equals(TestJide.SINGLE_SELECTION)) {
+                        _tree.getCheckBoxTreeSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+                    } else if ((e.getItem()).equals(TestJide.CONTIGUOUS_SELECTION)) {
+                        _tree.getCheckBoxTreeSelectionModel().setSelectionMode(TreeSelectionModel.CONTIGUOUS_TREE_SELECTION);
+                    } else {
+                        _tree.getCheckBoxTreeSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+                    }
+                }
+            }
+        });
+        int mode = _tree.getCheckBoxTreeSelectionModel().getSelectionMode();
+        switch (mode) {
+            case TreeSelectionModel.SINGLE_TREE_SELECTION:
+                comboBox.setSelectedIndex(0);
+                break;
+            case TreeSelectionModel.CONTIGUOUS_TREE_SELECTION:
+                comboBox.setSelectedIndex(1);
+                break;
+            case TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION:
+                comboBox.setSelectedIndex(2);
+                break;
+        }
+
+        final JCheckBox singleEventMode = new JCheckBox("Single Event Mode");
+        singleEventMode.addActionListener(new AbstractAction() {
+
+            private static final long serialVersionUID = -8967823755465307651L;
+
+            public void actionPerformed(ActionEvent e) {
+                _tree.getCheckBoxTreeSelectionModel().setSingleEventMode(singleEventMode.isSelected());
+            }
+        });
+        singleEventMode.setSelected(_tree.getCheckBoxTreeSelectionModel().isSingleEventMode());
+
+        panel.add(new JLabel("Set Selection Mode:"));
+        panel.add(comboBox);
+        panel.add(Box.createVerticalStrut(3));
+        panel.add(digIn);
+        panel.add(singleEventMode);
+        panel.add(checkBoxEnabled);
+        panel.add(clickInCheckBoxOnly);
+        panel.add(treeEnabled);
+        panel.add(selectPartialFirst);
+        panel.add(Box.createVerticalStrut(3));
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 0, 6, 6));
+        buttonPanel.add(selectAll);
+        buttonPanel.add(clearAll);
+        panel.add(buttonPanel);
+        panel.add(Box.createGlue());
+        return panel;
+    }
+
+    public Component getDemoPanel() {
+        JPanel panel = new JPanel(new BorderLayout(6, 6));
+
+        JPanel treePanel = new JPanel(new BorderLayout(2, 2));
+        treePanel.setBorder(BorderFactory.createCompoundBorder(new JideTitledBorder(new PartialEtchedBorder(PartialEtchedBorder.LOWERED, PartialSide.NORTH), "Albums", JideTitledBorder.LEADING, JideTitledBorder.ABOVE_TOP),
+                BorderFactory.createEmptyBorder(6, 0, 0, 0)));
+        _tree = new CheckBoxTree(createSongTreeModel()) {
+
+            @Override
+            public Dimension getPreferredScrollableViewportSize() {
+                return new Dimension(400, 400);
+            }
+        };
+        _tree.setRootVisible(false);
+        _tree.setShowsRootHandles(true);
+        DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) _tree.getActualCellRenderer();
+
+        SearchableUtils.installSearchable(_tree);
+
+        JPanel listsPanel = new JPanel(new GridLayout(1, 2));
+
+        final JList selectedList = new JList();
+        final JList eventsList = new JList();
+        final DefaultListModel eventsModel = new DefaultListModel();
+        _tree.getCheckBoxTreeSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
+
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                TreePath[] paths = e.getPaths();
+                for (TreePath path : paths) {
+                    eventsModel.addElement((e.isAddedPath(path) ? "Added - " : "Removed - ") + path);
+                }
+                eventsModel.addElement("---------------");
+                eventsList.ensureIndexIsVisible(eventsModel.size() - 1);
+
+                TreePath[] treePaths = _tree.getCheckBoxTreeSelectionModel().getSelectionPaths();
+                DefaultListModel selectedModel = new DefaultListModel();
+                if (treePaths != null) {
+                    for (TreePath path : treePaths) {
+                        selectedModel.addElement(path);
+                    }
+                }
+                selectedList.setModel(selectedModel);
+            }
+        });
+        eventsList.setModel(eventsModel);
+
+        selectedList.setVisibleRowCount(8);
+        eventsList.setVisibleRowCount(8);
+        JPanel selectedPanel = new JPanel(new BorderLayout());
+        selectedPanel.setBorder(BorderFactory.createCompoundBorder(new JideTitledBorder(new PartialEtchedBorder(PartialEtchedBorder.LOWERED, PartialSide.NORTH), "Selected Songs", JideTitledBorder.LEADING, JideTitledBorder.ABOVE_TOP),
+                BorderFactory.createEmptyBorder(6, 0, 0, 0)));
+        selectedPanel.add(new JScrollPane(selectedList));
+
+        JPanel eventsPanel = new JPanel(new BorderLayout());
+        eventsPanel.setBorder(BorderFactory.createCompoundBorder(new JideTitledBorder(new PartialEtchedBorder(PartialEtchedBorder.LOWERED, PartialSide.NORTH), "Event Fired", JideTitledBorder.LEADING, JideTitledBorder.ABOVE_TOP),
+                BorderFactory.createEmptyBorder(6, 0, 0, 0)));
+        eventsPanel.add(new JScrollPane(eventsList));
+
+        listsPanel.add(selectedPanel);
+        listsPanel.add(eventsPanel);
+
+        treePanel.add(listsPanel, BorderLayout.AFTER_LAST_LINE);
+        treePanel.add(new JScrollPane(_tree));
+        panel.add(treePanel);
+
+        return panel;
+    }
+
+    static public void main(String[] s) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                LookAndFeelFactory.installDefaultLookAndFeelAndExtension();
+                new TestJide();
+            }
+        });
+    }
+
+    private DefaultMutableTreeNode createSongTreeModel() {
+
+        DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Root");
+        DefaultMutableTreeNode categoryNode;
+        DefaultMutableTreeNode applicationNode;
+
+        categoryNode = new DefaultMutableTreeNode("123");
+        rootNode.add(categoryNode);
+        applicationNode = new DefaultMutableTreeNode("abc");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("def");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("ghi");
+        categoryNode.add(applicationNode);
+
+        categoryNode = new DefaultMutableTreeNode("456");
+        rootNode.add(categoryNode);
+        applicationNode = new DefaultMutableTreeNode("klm");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("nop");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("qrs");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("tuv");
+        categoryNode.add(applicationNode);
+        applicationNode = new DefaultMutableTreeNode("wxyz");
+        categoryNode.add(applicationNode);
+
+        return rootNode;
+    }
+}

bc1ca2a6dcac1b136c8b5ed279100ded2c7ec351
sly74fr <slymacdev@free.fr>
2012-04-16 15:00:12 +0000

    Moved all reusable GUI code from DependenciesView to ResizableTextViewFactory.

--- src/fr/jmmc/jmcs/gui/DependenciesView.java
+++ src/fr/jmmc/jmcs/gui/DependenciesView.java
@@ -3,28 +3,13 @@
  ******************************************************************************/
 package fr.jmmc.jmcs.gui;
 
-import fr.jmmc.jmcs.gui.util.WindowUtils;
 import fr.jmmc.jmcs.App;
 import fr.jmmc.jmcs.data.ApplicationDataModel;
 import fr.jmmc.jmcs.data.model.Package;
-import fr.jmmc.jmcs.network.BrowserLauncher;
+import fr.jmmc.jmcs.gui.component.ResizableTextViewFactory;
 import fr.jmmc.jmcs.util.FileUtils;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map.Entry;
-import javax.swing.JComponent;
-import javax.swing.JEditorPane;
-import javax.swing.JScrollPane;
-import javax.swing.KeyStroke;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,24 +17,20 @@ import org.slf4j.LoggerFactory;
  *
  * @author lafrasse
  */
-public class DependenciesView extends javax.swing.JFrame implements HyperlinkListener {
+public class DependenciesView {
 
     /** jMCS license files resource path constant */
     private static final String JMCS_LICENSE_CONTENT_FILE_PATH = "fr/jmmc/jmcs/resource/license/";
+    private static final String WINDOW_TITLE = "jMCS Dependencies";
     /** Logger */
     private static final Logger _logger = LoggerFactory.getLogger(DependenciesView.class.getName());
     /** Singleton instance */
     private static DependenciesView _instance = null;
     // Class members
-    private JEditorPane _editorPane;
-    private JScrollPane _scrollPane;
-    private HashMap<String, String> _licenseContent;
+    private static String _windowContent;
 
     private DependenciesView() {
-        _licenseContent = new HashMap<String, String>();
-        initComponents();
-        generateContent();
-        finsihLayout();
+        _windowContent = generateHtml();
     }
 
     /** Show the dependencies window */
@@ -57,30 +38,12 @@ public class DependenciesView extends javax.swing.JFrame implements HyperlinkLis
         if (_instance == null) {
             _instance = new DependenciesView();
         }
-        _instance.setVisible(true);
+        ResizableTextViewFactory.createHtmlWindow(_windowContent, WINDOW_TITLE);
     }
 
-    private void initComponents() {
+    private String generateHtml() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("jMCS Dependencies");
-        setAlwaysOnTop(true);
-
-        _editorPane = new JEditorPane();
-        _editorPane.setEditable(false);
-        _editorPane.setMargin(new Insets(5, 5, 5, 5));
-        _editorPane.setContentType("text/html");
-        _editorPane.addHyperlinkListener(this);
-
-        _scrollPane = new JScrollPane();
-        _scrollPane.setViewportView(_editorPane);
-
-        // Window layout
-        Container contentPane = getContentPane();
-        contentPane.add(_scrollPane, BorderLayout.CENTER);
-    }
-
-    private void generateContent() {
+        HashMap<String, String> licenseContent = new HashMap<String, String>();
 
         // Get jMCS data
         final ApplicationDataModel data = App.getJMcsApplicationDataModel();
@@ -117,12 +80,12 @@ public class DependenciesView extends javax.swing.JFrame implements HyperlinkLis
 
             // Process each license only once (when referenced several times)
             String licenseLabel = null;
-            if (!_licenseContent.containsKey(file)) {
+            if (!licenseContent.containsKey(file)) {
                 licenseLabel = name + " license (" + licenseName + ")";
             } else {
                 licenseLabel = licenseName + " license";
             }
-            _licenseContent.put(file, licenseLabel);
+            licenseContent.put(file, licenseLabel);
 
             // Add dependency link only if available
             if (link == null) {
@@ -135,7 +98,7 @@ public class DependenciesView extends javax.swing.JFrame implements HyperlinkLis
         }
 
         // Add every license at the bottom of the pane
-        for (Entry<String, String> currentLicense : _licenseContent.entrySet()) {
+        for (Entry<String, String> currentLicense : licenseContent.entrySet()) {
 
             final String licenseName = currentLicense.getValue();
             final String licenseFilename = currentLicense.getKey();
@@ -146,8 +109,8 @@ public class DependenciesView extends javax.swing.JFrame implements HyperlinkLis
             // Try to load license content
             final String licenseResourcePath = JMCS_LICENSE_CONTENT_FILE_PATH + licenseFilename;
             try {
-                final String licenseContent = FileUtils.readFile(licenseResourcePath);
-                generatedHtml.append("<pre>").append(licenseContent).append("</pre>");
+                final String licenseText = FileUtils.readFile(licenseResourcePath);
+                generatedHtml.append("<pre>").append(licenseText).append("</pre>");
             } catch (IllegalStateException ise) {
                 _logger.error("Could not load '{}' resource.", licenseResourcePath, ise);
                 generatedHtml.append("<i>License file unavailable at the moment.</i><br/>");
@@ -155,40 +118,6 @@ public class DependenciesView extends javax.swing.JFrame implements HyperlinkLis
         }
         generatedHtml.append("</body></html>");
 
-        _editorPane.setText(generatedHtml.toString());
-    }
-
-    private void finsihLayout() {
-        _editorPane.setCaretPosition(0); // Move back focus at the top of the content
-        setPreferredSize(new Dimension(700, 600));
-        WindowUtils.setClosingKeyboardShortcuts(getRootPane(), this);
-        pack();
-        WindowUtils.centerOnMainScreen(this);
-    }
-
-    /**
-     * Handle URL link clicked in the JEditorPane.
-     *
-     * @param event the received event.
-     */
-    @Override
-    public void hyperlinkUpdate(HyperlinkEvent event) {
-        // When a link is clicked
-        if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-
-            // Get the clicked URL
-            final URL url = event.getURL();
-
-            // If it is valid
-            if (url != null) {
-                // Get it in the good format
-                final String clickedURL = url.toExternalForm();
-                // Open the url in web browser
-                BrowserLauncher.openURL(clickedURL);
-            } else { // Assume it was an anchor
-                String anchor = event.getDescription();
-                _editorPane.scrollToReference(anchor);
-            }
-        }
+        return generatedHtml.toString();
     }
 }

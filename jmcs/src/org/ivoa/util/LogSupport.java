6de5ccbf32dea37645c974081a02fde84ce6bf35
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-04-18 13:17:29 +0000

    Automatically tagged LITpro in version LITPRO_V1_0_11 (mkfRepoUtil)

---
+++ jmcs/src/org/ivoa/util/LogSupport.java
@@ -0,0 +1,21 @@
+package org.ivoa.util;
+
+import org.slf4j.Logger;
+
+/**
+ * Log Support class to manage LogUtil references and classLoader issues
+ *
+ * @author Laurent Bourges (voparis) / Gerard Lemson (mpe)
+ */
+public class LogSupport {
+
+    /** 
+     * Main Logger for the application
+     * @see org.ivoa.bean.LogSupport
+     */
+    protected final static Logger logger = LogUtil.getLogger();
+
+    /** Hidden constructor */
+    protected LogSupport() {
+    }
+}

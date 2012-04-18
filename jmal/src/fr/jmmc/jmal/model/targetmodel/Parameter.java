fe14268699eb1909dda4fde856f2db69d2c1d32d
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-04-18 13:17:26 +0000

    Automatically tagged LITpro in version LITPRO_V1_0_11 (mkfRepoUtil)

---
+++ jmal/src/fr/jmmc/jmal/model/targetmodel/Parameter.java
@@ -0,0 +1,336 @@
+
+package fr.jmmc.jmal.model.targetmodel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlID;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import fr.jmmc.jmal.model.CloneableObject;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;all>
+ *         &lt;element name="desc" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="units" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="value" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *         &lt;element name="minValue" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
+ *         &lt;element name="maxValue" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
+ *         &lt;element name="scale" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/>
+ *         &lt;element name="hasFixedValue" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
+ *         &lt;element name="editable" type="{http://www.w3.org/2001/XMLSchema}boolean" minOccurs="0"/>
+ *       &lt;/all>
+ *       &lt;attGroup ref="{http://www.jmmc.fr/jmcs/models/0.1}ParameterAttributes"/>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+
+})
+@XmlRootElement(name = "parameter")
+public class Parameter
+    extends CloneableObject
+{
+
+    protected String desc;
+    protected String units;
+    protected double value;
+    protected Double minValue;
+    protected Double maxValue;
+    protected Double scale;
+    protected boolean hasFixedValue;
+    protected Boolean editable;
+    @XmlAttribute(name = "name", required = true)
+    protected String name;
+    @XmlAttribute(name = "type", required = true)
+    protected String type;
+    @XmlAttribute(name = "id")
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+    @XmlID
+    @XmlSchemaType(name = "ID")
+    protected String id;
+
+    /**
+     * Gets the value of the desc property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * Sets the value of the desc property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDesc(String value) {
+        this.desc = value;
+    }
+
+    /**
+     * Gets the value of the units property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUnits() {
+        return units;
+    }
+
+    /**
+     * Sets the value of the units property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUnits(String value) {
+        this.units = value;
+    }
+
+    /**
+     * Gets the value of the value property.
+     * 
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     * 
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the value of the minValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getMinValue() {
+        return minValue;
+    }
+
+    /**
+     * Sets the value of the minValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setMinValue(Double value) {
+        this.minValue = value;
+    }
+
+    /**
+     * Gets the value of the maxValue property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getMaxValue() {
+        return maxValue;
+    }
+
+    /**
+     * Sets the value of the maxValue property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setMaxValue(Double value) {
+        this.maxValue = value;
+    }
+
+    /**
+     * Gets the value of the scale property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getScale() {
+        return scale;
+    }
+
+    /**
+     * Sets the value of the scale property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setScale(Double value) {
+        this.scale = value;
+    }
+
+    /**
+     * Gets the value of the hasFixedValue property.
+     * 
+     */
+    public boolean isHasFixedValue() {
+        return hasFixedValue;
+    }
+
+    /**
+     * Sets the value of the hasFixedValue property.
+     * 
+     */
+    public void setHasFixedValue(boolean value) {
+        this.hasFixedValue = value;
+    }
+
+    /**
+     * Gets the value of the editable property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Boolean }
+     *     
+     */
+    public Boolean isEditable() {
+        return editable;
+    }
+
+    /**
+     * Sets the value of the editable property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Boolean }
+     *     
+     */
+    public void setEditable(Boolean value) {
+        this.editable = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the type property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the value of the type property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setType(String value) {
+        this.type = value;
+    }
+
+    /**
+     * Gets the value of the id property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setId(String value) {
+        this.id = value;
+    }
+    
+//--simple--preserve
+
+  /**
+   * Define the name and type to the same value
+   * @param value value to set
+   */
+  public void setNameAndType(final String value) {
+    this.name = value;
+    this.type = value;
+  }
+
+  @Override
+  public String toString() {
+    return "Parameter [" + this.name + " | " + this.type + "] = " + this.value;
+  }
+
+//--simple--preserve
+
+}

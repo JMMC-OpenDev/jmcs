bc03ead50fa45844f95dad438006cab37bcacc14
sly74fr <slymacdev@free.fr>
2012-04-18 13:58:09 +0000

    Added preliminary status history window.

--- jmcs/src/fr/jmmc/jmcs/gui/component/StatusBar.java
+++ jmcs/src/fr/jmmc/jmcs/gui/component/StatusBar.java
@@ -7,20 +7,32 @@ import fr.jmmc.jmcs.gui.util.SwingUtils;
 import fr.jmmc.jmcs.data.ApplicationDataModel;
 import fr.jmmc.jmcs.network.BrowserLauncher;
 import fr.jmmc.jmcs.App;
+import fr.jmmc.jmcs.gui.util.WindowUtils;
 import fr.jmmc.jmcs.util.ImageUtils;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
 import org.apache.commons.lang.SystemUtils;
 
 import java.awt.Font;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  * A status bar that can be shared all along an application.
@@ -35,6 +47,8 @@ public class StatusBar extends JPanel {
     private static final Logger _logger = LoggerFactory.getLogger(StatusBar.class.getName());
     /** Status label */
     private static final JLabel _statusLabel = new JLabel();
+    /** Store all status messages */
+    private static final List<String> _history = new ArrayList<String>(100);
 
     /**
      * Constructor.
@@ -48,15 +62,12 @@ public class StatusBar extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
         // Create logo
-        JLabel jmmcLogo = new JLabel();
-
-        //jmmcLogo.setIcon(new ImageIcon(getClass().getResource("/fr/jmmc/jmcs/resource/logo_small.png")));
-        //final ImageIcon imageIcon = new ImageIcon(getClass().getResource("/fr/jmmc/jmcs/resource/logo.png"));
         String logoURL = App.getSharedApplicationDataModel().getCompanyLogoResourcePath();
         final ImageIcon imageIcon = new ImageIcon(getClass().getResource(logoURL));
-        ImageIcon scladImageIcon = ImageUtils.getScaledImageIcon(imageIcon, 17, 0);
-        jmmcLogo.setIcon(scladImageIcon);
-        jmmcLogo.setVisible(true);
+        JLabel logo = new JLabel();
+        ImageIcon scaledImageIcon = ImageUtils.getScaledImageIcon(imageIcon, 17, 0);
+        logo.setIcon(scaledImageIcon);
+        logo.setVisible(true);
 
         // Create text logo
         JLabel textStatusBar = new JLabel();
@@ -64,13 +75,24 @@ public class StatusBar extends JPanel {
         textStatusBar.setFont(new Font("Comic Sans MS", 2, 10));
         textStatusBar.setVisible(true);
 
+        JButton historyButton = new JButton("Status :");
+        historyButton.setToolTipText("Click to view status history");
+        historyButton.addActionListener(new AbstractAction() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showStatusHistoryWindow();
+            }
+        });
+
         // StatusBar elements placement
         Box hBox = Box.createHorizontalBox();
-        hBox.add(new JLabel("Status : "));
+        hBox.add(historyButton);
+        //hBox.add(new JLabel("Status : "));
         hBox.add(_statusLabel);
         hBox.add(Box.createHorizontalGlue());
         hBox.add(textStatusBar);
-        hBox.add(jmmcLogo);
+        hBox.add(logo);
 
         /*
          * Add a space on the right bottom angle because Mac OS X corner is
@@ -85,7 +107,7 @@ public class StatusBar extends JPanel {
         // Get application data model to launch the default browser with the given link
         final ApplicationDataModel applicationDataModel = App.getSharedApplicationDataModel();
         if (applicationDataModel != null) {
-            jmmcLogo.addMouseListener(new MouseAdapter() {
+            logo.addMouseListener(new MouseAdapter() {
 
                 @Override
                 public void mouseClicked(MouseEvent evt) {
@@ -141,6 +163,7 @@ public class StatusBar extends JPanel {
      */
     private static void setStatusLabel(final String message) {
         _statusLabel.setText(message);
+        _history.add(message);
     }
 
     /**
@@ -150,6 +173,31 @@ public class StatusBar extends JPanel {
      */
     private static String getStatusLabel() {
         return _statusLabel.getText();
+    }
+
+    private void showStatusHistoryWindow() {
+        // TODO :
+        // - auto-refresh;
+        // - add event time of each entry;
+        // - put in LogbackGui ???
+        JFrame frame = new JFrame("Status History");
+
+        JList _columnList = new JList(_history.toArray());
+        _columnList.setCellRenderer(new AlternateRawColorCellRenderer());
+
+        // Add scrolling capacity to the list
+        JScrollPane scrollingList = new JScrollPane(_columnList);
+        scrollingList.setPreferredSize(new Dimension(400, 250));
+        scrollingList.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollingList.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollingList.getVerticalScrollBar().setFocusable(false);
+        scrollingList.setBorder(BorderFactory.createLoweredBevelBorder());
+        frame.add(scrollingList);
+
+        frame.pack();
+        WindowUtils.centerOnMainScreen(frame);
+        WindowUtils.setClosingKeyboardShortcuts(null, frame);
+        frame.setVisible(true);
     }
 }
 /*___oOo___*/

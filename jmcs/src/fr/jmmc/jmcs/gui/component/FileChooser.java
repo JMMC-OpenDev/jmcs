27185b6acaad8ce35b6bc283b1da4edd4818652b
sly74fr <slymacdev@free.fr>
2012-04-27 09:06:14 +0000

    Automatically tagged AppLauncher in version AL_V1_0b14 (mkfRepoUtil)

---
+++ jmcs/src/fr/jmmc/jmcs/gui/component/FileChooser.java
@@ -0,0 +1,281 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.gui.component;
+
+import fr.jmmc.jmcs.App;
+import fr.jmmc.jmcs.data.preference.FileChooserPreferences;
+import fr.jmmc.jmcs.util.FileUtils;
+import fr.jmmc.jmcs.util.MimeType;
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.io.File;
+import javax.swing.JFileChooser;
+import org.apache.commons.lang.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class provides utility methods to create file and directory choosers
+ * 
+ * TODO: use FilePreferences to get/set current directory preference (per mime type) ...
+ * 
+ * @author Laurent BOURGES, Sylvain LAFRASSE, Guillaume MELLA.
+ */
+public final class FileChooser {
+
+    /** Logger */
+    private static final Logger _logger = LoggerFactory.getLogger(FileChooser.class.getName());
+    /** apple specific property to force awt FileDialog work on directories only */
+    public final static String MAC_FILE_DIALOG_DIRECTORY = "apple.awt.fileDialogForDirectories";
+    /** use native file chooser i.e. awt.FileDialog (Mac OS X) */
+    private final static boolean USE_DIALOG_FOR_FILE_CHOOSER = SystemUtils.IS_OS_MAC_OSX;
+
+    /**
+     * Show the directory chooser using following properties:
+     * @param title dialog title
+     * @param givenDirectory optional current directory as file (last one used for given mime type if null)
+     * @return File instance or null if dialog was discarded
+     */
+    public static File showDirectoryChooser(final String title, final File givenDirectory, final MimeType mimeType) {
+
+        File preselectedDirectory = retrieveLastDirectoryForMimeType(givenDirectory, mimeType);
+        File selectedDirectory = null;
+
+        // If running under Mac OS X
+        if (SystemUtils.IS_OS_MAC_OSX) {
+            final FileDialog fileDialog = new FileDialog(App.getFrame(), title);
+            if (preselectedDirectory != null) {
+                fileDialog.setDirectory(preselectedDirectory.getParent());
+                fileDialog.setFile(preselectedDirectory.getName());
+            }
+
+            // force the file dialog to use directories only:
+            System.setProperty(MAC_FILE_DIALOG_DIRECTORY, "true");
+
+            try {
+                // waits for dialog inputs:
+                fileDialog.setVisible(true);
+            } finally {
+                // restore system property:
+                System.setProperty(MAC_FILE_DIALOG_DIRECTORY, "false");
+            }
+
+            // note: this avoid to choose the root folder '/':
+            if (fileDialog.getFile() != null && fileDialog.getDirectory() != null) {
+                selectedDirectory = new File(fileDialog.getDirectory(), fileDialog.getFile());
+            }
+        } else {
+            final JFileChooser fileChooser = new JFileChooser();
+            if (preselectedDirectory != null) {
+                fileChooser.setCurrentDirectory(preselectedDirectory);
+            }
+
+            // select one directory:
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            fileChooser.setDialogTitle(title);
+
+            final int returnVal = fileChooser.showSaveDialog(App.getFrame());
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                selectedDirectory = fileChooser.getSelectedFile();
+            }
+        }
+        if (selectedDirectory != null) {
+            if (!selectedDirectory.isDirectory()) {
+                _logger.warn("Expected directory: {}", selectedDirectory);
+                selectedDirectory = null;
+            } else {
+                if (!selectedDirectory.exists()) {
+                    if (MessagePane.showConfirmDirectoryCreation(selectedDirectory.getAbsolutePath())) {
+                        selectedDirectory.mkdirs();
+                    } else {
+                        StatusBar.show("directory creation cancelled.");
+                        selectedDirectory = null;
+                    }
+                }
+            }
+        }
+        if (selectedDirectory != null) {
+            final String directory = selectedDirectory.getPath();
+            FileChooserPreferences.setCurrentDirectoryForMimeType(mimeType, directory);
+        }
+        return selectedDirectory;
+    }
+
+    /**
+     * Show the Open File Dialog using following properties:
+     * @param title dialog title
+     * @param givenDirectory optional current directory as file (last one used for given mime type if null)
+     * @param mimeType optional file mime type used to get both file extension(s) and file chooser filter
+     * @param defaultFileName optional default file name
+     * @return File instance or null if dialog was discarded
+     */
+    public static File showOpenFileChooser(final String title, final File givenDirectory, final MimeType mimeType, final String defaultFileName) {
+
+        File preselectedDirectory = retrieveLastDirectoryForMimeType(givenDirectory, mimeType);
+        File selectedFile = null;
+
+        if (USE_DIALOG_FOR_FILE_CHOOSER) {
+            final FileDialog fileDialog = new FileDialog(App.getFrame(), title, FileDialog.LOAD);
+            if (preselectedDirectory != null) {
+                fileDialog.setDirectory(preselectedDirectory.getAbsolutePath());
+            }
+            if (mimeType != null) {
+                fileDialog.setFilenameFilter(mimeType.getFileFilter());
+            }
+            if (defaultFileName != null) {
+                fileDialog.setFile(defaultFileName);
+            }
+
+            // waits for dialog inputs:
+            fileDialog.setVisible(true);
+
+            if (fileDialog.getFile() != null && fileDialog.getDirectory() != null) {
+                selectedFile = new File(fileDialog.getDirectory(), fileDialog.getFile());
+            }
+
+        } else {
+            final JFileChooser fileChooser = new JFileChooser();
+            if (preselectedDirectory != null) {
+                fileChooser.setCurrentDirectory(preselectedDirectory);
+            }
+
+            // select one file:
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            if (mimeType != null) {
+                fileChooser.setFileFilter(mimeType.getFileFilter());
+            }
+
+            if (defaultFileName != null) {
+                fileChooser.setSelectedFile(new File(fileChooser.getCurrentDirectory(), defaultFileName));
+            }
+
+            fileChooser.setDialogTitle(title);
+
+            final int returnVal = fileChooser.showOpenDialog(App.getFrame());
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+            }
+        }
+        if (selectedFile != null) {
+            // Mac OS X can return application packages:
+            if (SystemUtils.IS_OS_MAC_OSX && selectedFile.isDirectory()) {
+                _logger.warn("Selected file is an application package: {}", selectedFile);
+                selectedFile = null;
+            } else {
+                if (!selectedFile.exists()) {
+                    _logger.warn("Selected file does not exist: {}", selectedFile);
+
+                    if (mimeType == null) {
+                        selectedFile = null;
+                    } else if (FileUtils.getExtension(selectedFile) == null) {
+                        // try using the same file name with extension :
+                        selectedFile = mimeType.checkFileExtension(selectedFile);
+                        // check again if that file exists :
+                        if (!selectedFile.exists()) {
+                            selectedFile = null;
+                        }
+                    }
+                }
+            }
+        }
+        if (selectedFile != null) {
+            final String directory = selectedFile.getParent();
+            FileChooserPreferences.setCurrentDirectoryForMimeType(mimeType, directory);
+        }
+        return selectedFile;
+    }
+
+    /**
+     * Show the Save File Dialog using following properties:
+     * @param title dialog title
+     * @param givenDirectory optional current directory as file (last one used for given mime type if null)
+     * @param mimeType optional file mime type used to get both file extension(s) and file chooser filter
+     * @param defaultFileName optional default file name
+     * @return File instance or null if dialog was discarded
+     */
+    public static File showSaveFileChooser(final String title, final File givenDirectory, final MimeType mimeType, final String defaultFileName) {
+
+        File preselectedDirectory = retrieveLastDirectoryForMimeType(givenDirectory, mimeType);
+        File selectedFile = null;
+
+        if (USE_DIALOG_FOR_FILE_CHOOSER) {
+            final FileDialog fileDialog = new FileDialog(App.getFrame(), title, FileDialog.SAVE);
+            if (preselectedDirectory != null) {
+                fileDialog.setDirectory(preselectedDirectory.getAbsolutePath());
+            }
+            if (mimeType != null) {
+                fileDialog.setFilenameFilter(mimeType.getFileFilter());
+            }
+            if (defaultFileName != null) {
+                fileDialog.setFile(defaultFileName);
+            }
+
+            // waits for dialog inputs:
+            fileDialog.setVisible(true);
+
+            if (fileDialog.getFile() != null && fileDialog.getDirectory() != null) {
+                selectedFile = new File(fileDialog.getDirectory(), fileDialog.getFile());
+            }
+
+        } else {
+            final JFileChooser fileChooser = new JFileChooser();
+            if (preselectedDirectory != null) {
+                fileChooser.setCurrentDirectory(preselectedDirectory);
+            }
+
+            // select one file:
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            if (mimeType != null) {
+                fileChooser.setFileFilter(mimeType.getFileFilter());
+            }
+
+            if (defaultFileName != null) {
+                fileChooser.setSelectedFile(new File(fileChooser.getCurrentDirectory(), defaultFileName));
+            }
+
+            fileChooser.setDialogTitle(title);
+
+            final int returnVal = fileChooser.showSaveDialog(App.getFrame());
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+            }
+        }
+        if (selectedFile != null) {
+            if (mimeType != null) {
+                selectedFile = mimeType.checkFileExtension(selectedFile);
+            }
+
+            // Mac OS X already handles file overwrite confirmation:
+            if (!SystemUtils.IS_OS_MAC_OSX && selectedFile.exists()) {
+                if (!MessagePane.showConfirmFileOverwrite(selectedFile.getName())) {
+                    StatusBar.show("overwritting cancelled.");
+                    selectedFile = null;
+                }
+            }
+        }
+        if (selectedFile != null) {
+            final String directory = selectedFile.getParent();
+            FileChooserPreferences.setCurrentDirectoryForMimeType(mimeType, directory);
+        }
+        return selectedFile;
+    }
+
+    private static File retrieveLastDirectoryForMimeType(final File givenDirectory, final MimeType mimeType) {
+        File preselectedDirectory = givenDirectory;
+        if (preselectedDirectory == null) {
+            preselectedDirectory = FileChooserPreferences.getLastDirectoryForMimeTypeAsFile(mimeType);
+        }
+        return preselectedDirectory;
+    }
+
+    /**
+     * Forbidden constructor
+     */
+    private FileChooser() {
+        super();
+    }
+}
---
+++ jmcs/src/fr/jmmc/jmcs/util/FileFilterRepository.java
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.util;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * FileFilterRepository singleton class.
+ * 
+ * @author Sylvain LAFRASSE, Laurent BOURGES, Guillaume MELLA.
+ */
+public final class FileFilterRepository {
+
+    /** Logger */
+    private static final Logger _logger = LoggerFactory.getLogger(FileFilterRepository.class.getName());
+    /** Singleton instance */
+    private static FileFilterRepository _instance = null;
+    /**
+     * Hashtable to associate string keys like
+     * "application-x/scvot-file" to FileFilterRepository instances.
+     */
+    private static final HashMap<String, GenericFileFilter> _repository = new HashMap<String, GenericFileFilter>(16);
+
+    /** Hidden constructor */
+    private FileFilterRepository() {
+        super();
+    }
+
+    /** 
+     * Return the singleton instance 
+     * @return singleton instance 
+     */
+    public static synchronized FileFilterRepository getInstance() {
+        // DO NOT MODIFY !!!
+        if (_instance == null) {
+            _instance = new FileFilterRepository();
+        }
+
+        return _instance;
+
+        // DO NOT MODIFY !!!
+    }
+
+    /**
+     * Register a file filter in the repository.
+     *
+     * @param mimeType the mime type of the file, like "application-x/scvot-file".
+     * @param fileExtension the file extensions associated to the mime type.
+     * @param description the humanly readable description for the mime type.
+     *
+     * @return the previous registered file filter, null otherwise.
+     */
+    public GenericFileFilter put(final String mimeType, final String fileExtension, final String description) {
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("FileFilterRepository - put(mimeType = '{}', fileExtension = '{}', description = '{}')",
+                    new Object[]{mimeType, fileExtension, description});
+        }
+
+        return put(mimeType, new String[]{fileExtension}, description);
+    }
+
+    /**
+     * Register a file filter in the repository.
+     *
+     * @param mimeType the mime type of the file, like "application-x/scvot-file".
+     * @param fileExtensions an array of file extensions associated to the mime type.
+     * @param description the humanly readable description for the mime type.
+     *
+     * @return the previous registered file filter, null otherwise.
+     */
+    public GenericFileFilter put(final String mimeType, final String[] fileExtensions, final String description) {
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("FileFilterRepository - put(mimeType = '{}', fileExtensions[] = '{}', description = '{}')",
+                    new Object[]{mimeType, Arrays.toString(fileExtensions), description});
+        }
+
+        final GenericFileFilter filter = new GenericFileFilter(fileExtensions, description);
+
+        final GenericFileFilter previousFilter = _repository.put(mimeType, filter);
+
+        if (previousFilter == null) {
+            _logger.trace("Registered '{}' filter for the first time.", mimeType);
+        } else if (previousFilter != filter) {
+            _logger.debug("Overwritten the previously registered '{}' file filter.", mimeType);
+        } else {
+            if (_logger.isDebugEnabled()) {
+                _logger.debug("Registered '{}' mimeType associated with file extensions '{}' ({}) succesfully.",
+                        new Object[]{mimeType, Arrays.toString(fileExtensions), description});
+            }
+        }
+
+        return previousFilter;
+    }
+
+    /**
+     * Return the previously registered file filter for the given mime type.
+     *
+     * @param mimeType the mime type of the file filter, like "application-x/scvot-file".
+     *
+     * @return the retrieved registered file filter, null otherwise.
+     */
+    public static GenericFileFilter get(final String mimeType) {
+        final GenericFileFilter retrievedFilter = _repository.get(mimeType);
+
+        if (retrievedFilter == null) {
+            _logger.error("Cannot find '{}' file filter.", mimeType);
+        } else {
+            _logger.debug("Retrieved '{}' file filter succesfully.", mimeType);
+        }
+
+        return retrievedFilter;
+    }
+
+    /**
+     * Return the previously registered file filter for the given mime type as enum.
+     *
+     * @param mimeType MimeType enum value
+     *
+     * @return the retrieved registered file filter, null otherwise.
+     */
+    public static GenericFileFilter get(final MimeType mimeType) {
+        return get(mimeType.getMimeType());
+    }
+
+    /**
+     * Return the content of the object as a String for output.
+     *
+     * @return the content of the object as a String for output.
+     */
+    @Override
+    public String toString() {
+        if (_repository == null) {
+            return "No file filter registered yet.";
+        }
+
+        return _repository.toString();
+    }
+}
+/*___oOo___*/

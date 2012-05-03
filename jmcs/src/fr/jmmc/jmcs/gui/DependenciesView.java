ccc28246013f44388649a22ec2156615defb6f15
sly74fr <slymacdev@free.fr>
2012-05-03 13:15:21 +0000

    Automatically tagged AppLauncher in version AL_V1_0b15 (mkfRepoUtil)

---
+++ jmcs/src/fr/jmmc/jmcs/gui/DependenciesView.java
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.gui;
+
+import fr.jmmc.jmcs.App;
+import fr.jmmc.jmcs.data.ApplicationDataModel;
+import fr.jmmc.jmcs.data.model.Package;
+import fr.jmmc.jmcs.gui.component.ResizableTextViewFactory;
+import fr.jmmc.jmcs.util.FileUtils;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Show the list of jMCS dependencies in a dedicated window.
+ * @author Sylvain LAFRASSE
+ */
+public class DependenciesView {
+
+    /** jMCS license files resource path constant */
+    private static final String JMCS_LICENSE_CONTENT_FILE_PATH = "fr/jmmc/jmcs/resource/license/";
+    private static final String WINDOW_TITLE = "jMCS Dependencies";
+    /** Logger */
+    private static final Logger _logger = LoggerFactory.getLogger(DependenciesView.class.getName());
+    /** Singleton instance */
+    private static DependenciesView _instance = null;
+    // Class members
+    private static String _windowContent;
+
+    private DependenciesView() {
+        _windowContent = generateHtml();
+    }
+
+    /** Show the dependencies window */
+    public static void display() {
+        if (_instance == null) {
+            _instance = new DependenciesView();
+        }
+        ResizableTextViewFactory.createHtmlWindow(_windowContent, WINDOW_TITLE, false);
+    }
+
+    private String generateHtml() {
+
+        HashMap<String, String> licenseContent = new HashMap<String, String>();
+
+        // Get jMCS data
+        final ApplicationDataModel data = App.getJMcsApplicationDataModel();
+        final String jMcsName = data.getProgramName();
+        final String jmmcLogoURL = getClass().getResource(data.getCompanyLogoResourcePath()).toString();
+        final String jmmcUrl = data.getMainWebPageURL();
+        final String jmmcName = data.getShortCompanyName();
+        final String jmmcLongName = data.getLegalCompanyName();
+        final String jMcsUrl = data.getLinkValue();
+
+        // Compose jMCS header
+        final StringBuilder generatedHtml = new StringBuilder(4096);
+        generatedHtml.append("<html><head></head><body>");
+        generatedHtml.append("<center><b>").append(jMcsName).append(" Acknowledgments</b></center><br/>");
+        generatedHtml.append("<center><a href='").append(jmmcUrl).append("'><img src='").append(jmmcLogoURL).append("'/></a></center><br/><br/>");
+        generatedHtml.append("<i>").append(App.getSharedApplicationDataModel().getProgramName()).append("</i>");
+        generatedHtml.append(" make extensive use of the <a href = '").append(jMcsUrl).append("'>").append(jMcsName).append("</a> provided by the ").append(jmmcLongName).append(" (").append(jmmcName).append(").<br/><br/>");
+        generatedHtml.append(jMcsName).append(" dependencies include:<br/>");
+
+        // Compose each package informations
+        for (Package dependency : data.getPackages()) {
+            final String name = dependency.getName();
+            final String link = dependency.getLink();
+            final String description = dependency.getDescription();
+            final String jars = dependency.getJars();
+            final String licenseName = dependency.getLicense().value();
+
+            // Compute default license filename if none provided
+            String file = dependency.getFile();
+            if (file == null) {
+                file = licenseName.replace(' ', '_');
+                file += ".txt";
+            }
+
+            // Process each license only once (when referenced several times)
+            String licenseLabel = null;
+            if (!licenseContent.containsKey(file)) {
+                licenseLabel = name + " license (" + licenseName + ")";
+            } else {
+                licenseLabel = licenseName + " license";
+            }
+            licenseContent.put(file, licenseLabel);
+
+            // Add dependency link only if available
+            if (link == null) {
+                generatedHtml.append(name);
+            } else {
+                generatedHtml.append("<a href='").append(link).append("'>").append(name).append("</a>");
+            }
+            generatedHtml.append(":<br/><i>").append(description).append("</i><br/>");
+            generatedHtml.append("(composed of ").append(jars).append(", distirbuted under <a href='#").append(file).append("'>").append(licenseName).append(" license</a>)<br/><br/>");
+        }
+
+        // Add every license at the bottom of the pane
+        for (Entry<String, String> currentLicense : licenseContent.entrySet()) {
+
+            final String licenseName = currentLicense.getValue();
+            final String licenseFilename = currentLicense.getKey();
+
+            // Compose license title (with anchor)
+            generatedHtml.append("<br/><a name='#").append(licenseFilename).append("'/><hr/><center><b>").append(licenseName).append("</b></center><hr/>");
+
+            // Try to load license content
+            final String licenseResourcePath = JMCS_LICENSE_CONTENT_FILE_PATH + licenseFilename;
+            try {
+                final String licenseText = FileUtils.readFile(licenseResourcePath);
+                generatedHtml.append("<pre>").append(licenseText).append("</pre>");
+            } catch (IllegalStateException ise) {
+                _logger.error("Could not load '{}' resource.", licenseResourcePath, ise);
+                generatedHtml.append("<i>License file unavailable at the moment.</i><br/>");
+            }
+        }
+        generatedHtml.append("</body></html>");
+
+        return generatedHtml.toString();
+    }
+}
---
+++ jmcs/src/fr/jmmc/jmcs/util/MimeType.java
@@ -0,0 +1,152 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.util;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * List the mime types that are used by multiples applications using jMCS.
+ * It is also acceptable to define here the mime types specific to one
+ * application.
+ * 
+ * @author Guillaume MELLA, Laurent BOURGES.
+ */
+public enum MimeType {
+
+    /** MimeType associated to SearchCal calibrator list */
+    SEARCHCAL_CALIBRATORLIST("application/x-searchcal+votable+xml", "SearchCal Calibrator List", "scvot"),
+    /** MimeType associated to Observation settings */
+    ASPRO_OBSERVATION("application/x-aspro+xml", "Aspro Observation Settings", "asprox"),
+    /** MimeType associated to xml LITpro settings */
+    LITPRO_SETTINGS("application/vnd.jmmc.litpro+xml", "LITpro xml Settings", "litprox", "xml"),
+    /** MimeType associated to P2PP Observing blocks */
+    OBX("application/obx", "Observing Blocks", "obx"),
+    /** MimeType associated to OIFITS format */
+    OIFITS("application/oifits", "Optical Interferometry FITS", "fits", "oifits"),
+    /** MimeType associated to FITS format */
+    FITS_IMAGE("application/fits", "FITS Image", "fits", "fits.gz"),
+    /** MimeType associated to PDF documents */
+    PDF("application/pdf", "Portable Document Format", "pdf"),
+    /** MimeType associated to VEGA Star Lists */
+    STAR_LIST("text/plain", "Star Lists", "txt"),
+    /** MimeType associated to Character-Separated Values format */
+    CSV("text/csv", "CSV", "txt"),
+    /** MimeType associated to HTML format */
+    HTML("text/html", "HTML", "html"),
+    /** MimeType associated to Text files */
+    PLAIN_TEXT("text/plain", "Text files", "txt"),
+    /** MimeType associated to URL */
+    URL("text/plain", "URL", "url");
+
+    /**
+     * Custom constructor
+     * @param mimeType mime type name
+     * @param name short description
+     * @param extensions accepted extensions
+     */
+    private MimeType(final String mimeType, final String name, final String... extensions) {
+        _mimeType = mimeType;
+        _name = name;
+        _fullDescription = name + ' ' + Arrays.toString(extensions);
+        _extensions = Arrays.asList(extensions);
+        FileFilterRepository.getInstance().put(mimeType, extensions, this._fullDescription);
+    }
+
+    /* members */
+    /** mime-type */
+    private final String _mimeType;
+    /** mime-type name */
+    private final String _name;
+    /** full mime-type description */
+    private final String _fullDescription;
+    /** list of accepted extensions */
+    private final List<String> _extensions;
+
+    /**
+     * Return the mime-type name
+     * @return mime-type name
+     */
+    public String getName() {
+        return _name;
+    }
+
+    /**
+     * Return the short mime-type description
+     * @return short mime-type description
+     */
+    public String getDescription() {
+        return _fullDescription;
+    }
+
+    /**
+     * Return all accepted extensions
+     * @return accepted extensions as list
+     */
+    public List<String> getExtensions() {
+        return _extensions;
+    }
+
+    /** 
+     * Return the first accepted extension
+     * @return first accepted extension
+     */
+    public String getExtension() {
+        if (_extensions.isEmpty()) {
+            return null;
+        }
+        return _extensions.get(0);
+    }
+
+    /**
+     * Return the mime-type
+     * @return mime-type
+     */
+    public String getMimeType() {
+        return _mimeType;
+    }
+
+    /**
+     * Return the last registered file filter for the given mime type.
+     *
+     * @return the retrieved registered file filter.
+     */
+    public GenericFileFilter getFileFilter() {
+        return FileFilterRepository.get(this);
+    }
+
+    /**
+     * Check if the given file has an accepted extension. 
+     * If not, return a new file with the first accepted extension
+     * @param file file to check
+     * @return given file or new file with the first accepted extension
+     */
+    public File checkFileExtension(final File file) {
+        final String ext = FileUtils.getExtension(file);
+
+        if (ext == null || !getExtensions().contains(ext)) {
+            // add or replace current extension by the first accepted extension:
+            final String fileNamePart = FileUtils.getFileNameWithoutExtension(file);
+            return new File(file.getParentFile(), fileNamePart + '.' + getExtension());
+        }
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        return _mimeType + ", matching " + _fullDescription + " file extension(s).";
+    }
+
+    /**
+     * For test and debug purpose only.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        // For each catalog in the enum
+        for (MimeType mimeType : MimeType.values()) {
+            System.out.println("MimeType '" + mimeType._name + "' = '" + mimeType.toString() + "'.");
+        }
+    }
+}

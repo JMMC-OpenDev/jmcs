4c9e09ea2703a13c20c187ff5ef9f79723099385
sly74fr <slymacdev@free.fr>
2012-05-14 09:07:00 +0000

    Circumvent BrowserLauncher NPE when running under Oracle 1.7u4 (and later ???) release for Mac OS X

--- src/fr/jmmc/jmcs/network/BrowserLauncher.java
+++ src/fr/jmmc/jmcs/network/BrowserLauncher.java
@@ -28,7 +28,18 @@ public class BrowserLauncher {
      * @return BrowserLauncher instance
      */
     private static edu.stanford.ejalbert.BrowserLauncher getLauncher() {
+
         if (_launcher == null) {
+            // Circumvent BrowserLauncher NPE when running under Oracle 1.7u4 (and later ???) release for Mac OS X
+            String osName = System.getProperty("os.name");
+            final String mrjVersionSystemProperty = System.getProperty("mrj.version");
+            if (osName.startsWith("Mac OS")) {
+                if (mrjVersionSystemProperty == null) {
+                    System.setProperty("mrj.version", "9999.999");
+                    _logger.debug("Probably running Oracle JVM nuder MAc OS X, faking missing 'mrj.version' system property.");
+                }
+            }
+
             try {
                 _launcher = new edu.stanford.ejalbert.BrowserLauncher();
 
@@ -37,14 +48,19 @@ public class BrowserLauncher {
             } catch (BrowserLaunchingInitializingException bie) {
                 _logger.warn("Cannot initialize browser launcher : ", bie);
             }
+
+            // Circumvent BrowserLauncher NPE when running under Oracle 1.7u4 (and later ???) release for Mac OS X
+            if (mrjVersionSystemProperty == null) {
+                System.setProperty("mrj.version", "");
+            }
         }
         return _launcher;
     }
 
     /**
-     * Open in web browser the url passed in argument.
+     * Open in web browser the URL passed in argument.
      *
-     * @param url url to open in web browser.
+     * @param url URL to open in web browser.
      */
     public static void openURL(String url) {
         final edu.stanford.ejalbert.BrowserLauncher launcher = getLauncher();

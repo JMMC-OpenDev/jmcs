ef30a3642d7130036f22408662efc2bd92be8865
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-08-01 14:00:13 +0000

    fix NPE related to focus lost on empty cells

--- src/fr/jmmc/jmcs/gui/component/NumericJTable.java
+++ src/fr/jmmc/jmcs/gui/component/NumericJTable.java
@@ -4,9 +4,10 @@
 package fr.jmmc.jmcs.gui.component;
 
 import fr.jmmc.jmcs.gui.util.SwingUtils;
+import java.awt.Color;
 import java.awt.Component;
-import javax.swing.DefaultCellEditor;
-import javax.swing.ListSelectionModel;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
 import javax.swing.table.TableCellEditor;
 import javax.swing.text.JTextComponent;
 
@@ -28,8 +29,13 @@ public class NumericJTable extends javax.swing.JTable {
     public NumericJTable() {
         super();
 
+        // use custom double editor to fix focus problems:
+        final DoubleEditor editor = new DoubleEditor();
+
         // set one click edition on following table and show all decimals in numerical values
-        ((DefaultCellEditor) getDefaultEditor(Double.class)).setClickCountToStart(1);
+        editor.setClickCountToStart(1);
+
+        setDefaultEditor(Double.class, editor);
 
         // single table selection :
         getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -53,5 +59,58 @@ public class NumericJTable extends javax.swing.JTable {
             });
         }
         return c;
+    }
+
+    /**
+     * Custom Number editor fixing focus lost problems on empty cell
+     */
+    static class DoubleEditor extends DefaultCellEditor {
+
+        private Double value;
+
+        public DoubleEditor() {
+            super(new JTextField());
+            ((JTextField) getComponent()).setHorizontalAlignment(JTextField.RIGHT);
+        }
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value,
+                boolean isSelected,
+                int row, int column) {
+            this.value = null;
+
+            ((JComponent) getComponent()).setBorder(new LineBorder(Color.black));
+
+            return super.getTableCellEditorComponent(table, value, isSelected, row, column);
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+            return value;
+        }
+
+        @Override
+        public boolean stopCellEditing() {
+            final String s = (String) super.getCellEditorValue();
+            // Here we are dealing with the case where a user
+            // has deleted the string value in a cell, possibly
+            // after a failed validation. Return null, so that
+            // they have the option to replace the value with
+            // null or use escape to restore the original.
+            // For Strings, return "" for backward compatibility.
+            if ("".equals(s)) {
+                // Fix focus lost problem:
+                value = null;
+                return super.stopCellEditing();
+            }
+
+            try {
+                value = new Double(s);
+            } catch (Exception e) {
+                ((JComponent) getComponent()).setBorder(new LineBorder(Color.red));
+                return false;
+            }
+            return super.stopCellEditing();
+        }
     }
 }

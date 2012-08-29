896dedba9036b038fdda7a969cfd1a1380a3d649
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-08-29 14:53:29 +0000

    Jaxb utility class to load and save data with a given jaxbfactory

---
+++ jmcs/src/fr/jmmc/jmcs/jaxb/JAXBUtils.java
@@ -0,0 +1,187 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmcs.jaxb;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URL;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.UnmarshalException;
+import javax.xml.bind.Unmarshaller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class used to perform generic marshalling and unmarshalling operations.
+ * @author bourgesl, mella
+ */
+public class JAXBUtils {
+
+    /* members */
+    /** Logger */
+    private final static Logger logger = LoggerFactory.getLogger(JAXBUtils.class);
+
+    /** 
+     * Private constructor for utility class 
+     */
+    private JAXBUtils() {
+    }
+    
+    /**
+     * Load on object from url.
+     * @param inputUrl File to load
+     * @param jbf JAXBFactory
+     * @return unmarshalled object
+     *
+     * @throws IOException if an I/O exception occured
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws XmlBindException if a JAXBException was caught while creating an unmarshaller
+     */
+    public static Object loadObject(URL inputUrl, JAXBFactory jbf) throws IOException, IllegalStateException, XmlBindException {
+        Object result = null;
+
+        logger.warn("JAXBUtils.loadObject() from url : {}", inputUrl);
+
+        try {
+            result = jbf.createUnMarshaller().unmarshal(new BufferedInputStream(inputUrl.openStream()));
+        } catch (JAXBException ex) {
+            handleException("Loading object from " + inputUrl, ex);
+        }
+
+        return result;
+    }
+
+    /**
+     * Protected load method
+     * @param inputFile File to load
+     * @param jbf jaxb factory instance
+     * @return unmarshalled object
+     *
+     * @throws IOException if an I/O exception occured
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws XmlBindException if a JAXBException was caught while creating an unmarshaller
+     */
+    public static Object loadObject(final File inputFile, JAXBFactory jbf)
+            throws IOException, IllegalStateException, XmlBindException {
+
+        Object result = null;
+        try {
+
+            final long start = System.nanoTime();
+
+            final Unmarshaller u = jbf.createUnMarshaller();
+
+            result = u.unmarshal(inputFile);
+
+            if (logger.isInfoEnabled()) {
+                logger.info("unmarshall : duration = {} ms.", 1e-6d * (System.nanoTime() - start));
+            }
+
+        } catch (JAXBException je) {
+            handleException("Load failure on " + inputFile, je);
+        }
+        return result;
+    }
+
+    /**
+     * Protected load method
+     * @param reader any reader
+     * @param jbf jaxb factory instance
+     * @return unmarshalled object
+     * 
+     *
+     * @throws IOException if an I/O exception occured
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws XmlBindException if a JAXBException was caught while creating an unmarshaller
+     */
+    protected static Object loadObject(final Reader reader, JAXBFactory jbf)
+            throws IOException, IllegalStateException, XmlBindException {
+
+        Object result = null;
+        try {
+
+            final long start = System.nanoTime();
+
+            final Unmarshaller u = jbf.createUnMarshaller();
+
+            result = u.unmarshal(reader);
+
+            if (logger.isInfoEnabled()) {
+                logger.info("unmarshall : duration = {} ms.", 1e-6d * (System.nanoTime() - start));
+            }
+
+        } catch (JAXBException je) {
+            handleException("Load failure on " + reader, je);
+        }
+        return result;
+    }
+
+    /**
+     * Protected save method
+     * @param outputFile File to save
+     * @param object to marshall
+     * @param jbf jaxb factory instance
+     *
+     * @throws IOException if an I/O exception occured
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws XmlBindException if a JAXBException was caught while creating an marshaller
+     */
+    public static void saveObject(final File outputFile, final Object object, JAXBFactory jbf)
+            throws IOException, IllegalStateException {
+        try {
+
+            final long start = System.nanoTime();
+
+            jbf.createMarshaller().marshal(object, outputFile);
+
+            if (logger.isInfoEnabled()) {
+                logger.info("marshall : duration = {} ms.", 1e-6d * (System.nanoTime() - start));
+            }
+
+        } catch (JAXBException je) {
+            handleException("Save failure on " + outputFile, je);
+        }
+    }
+
+    /**
+     * Public save method
+     * @param writer writer to use
+     * @param object to marshall
+     * @param jbf jaxb factory instance
+     *
+     * @throws IllegalStateException if an unexpected exception occured
+     */
+    public static void saveObject(final Writer writer, final Object object, JAXBFactory jbf)
+            throws IllegalStateException {
+        try {
+            jbf.createMarshaller().marshal(object, writer);
+        } catch (JAXBException je) {
+            throw new IllegalStateException("Serialization failure", je);
+        }
+    }
+
+    /**
+     * Handle JAXB Exception to extract IO Exception or unexpected exceptions
+     * @param message message
+     * @param je jaxb exception
+     * 
+     * @throws IllegalStateException if an unexpected exception occured
+     * @throws IOException if an I/O exception occured
+     */
+    protected static void handleException(final String message, final JAXBException je) throws IllegalStateException, IOException {
+        final Throwable cause = je.getCause();
+        if (cause != null) {
+            if (cause instanceof IOException) {
+                throw (IOException) cause;
+            }
+        }
+        if (je instanceof UnmarshalException) {
+            throw new IllegalArgumentException("The loaded file does not correspond to a valid file", je);
+        }
+        throw new IllegalStateException(message, je);
+    }    
+}

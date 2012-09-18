2d5c7f51e5e809ace1bae7ac64e6a745a060b720
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-09-18 06:41:23 +0000

    Automatically tagged MCS in version SEP2012 from trunk@13578  (mkfRepoUtil)

---
+++ testgui/include/testguiPrivate.h
@@ -0,0 +1,24 @@
+#ifndef testguiPrivate_H
+#define testguiPrivate_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * @file
+ * testgui private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+define MODULE_ID "testgui"
+
+ 
+
+#endif /*!testguiPrivate_H*/
+
+/*___oOo___*/

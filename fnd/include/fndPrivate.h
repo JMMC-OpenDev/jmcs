10035c306eed1a436c2c2f84c10895ec243ed9b1
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-09-18 06:41:11 +0000

    Automatically tagged MCS in version SEP2012 from trunk@13578  (mkfRepoUtil)

---
+++ fnd/include/fndPrivate.h
@@ -0,0 +1,35 @@
+#ifndef fndPrivate_H
+#define fndPrivate_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ * Private header file of the fnd library.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "fnd"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!fndPrivate_H*/
+
+/*___oOo___*/

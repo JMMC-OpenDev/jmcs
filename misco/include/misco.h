18aa7a90fe77da66c56ecd3d71cc8d7eab094258
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-09-18 06:41:12 +0000

    Automatically tagged MCS in version SEP2012 from trunk@13578  (mkfRepoUtil)

---
+++ misco/include/misco.h
@@ -0,0 +1,24 @@
+#ifndef misco_H
+#define misco_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ * Main header file, grouping all the public headers of this module library.
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+/*
+ * Local headers
+ */
+#include "miscoDYN_BUF.h"
+#include "miscoXML_ELEMENT.h"
+ 
+#endif /*!misco_H*/
+
+/*___oOo___*/
---
+++ misco/include/miscoPrivate.h
@@ -0,0 +1,35 @@
+#ifndef miscoPrivate_H
+#define miscoPrivate_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ * misco private header file.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "misco"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!miscoPrivate_H*/
+
+/*___oOo___*/

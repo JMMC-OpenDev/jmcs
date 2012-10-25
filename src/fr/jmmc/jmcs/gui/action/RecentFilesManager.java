d193fc6ed3855eea5fd32453d618488c361f4be0
sly74fr <slymacdev@free.fr>
2012-10-25 08:31:32 +0000

    Added "Clean History" action in "Open Recent" menu. Refactoring.

--- src/fr/jmmc/jmcs/gui/action/RecentFilesManager.java
+++ src/fr/jmmc/jmcs/gui/action/RecentFilesManager.java
@@ -12,20 +12,21 @@ import java.util.Map;
 import javax.swing.AbstractAction;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * RecentlyOpenedFilesManager singleton class.
+ * RecentFilesManager singleton class.
  * 
  * @author Sylvain LAFRASSE.
  */
-public class RecentlyOpenedFilesManager {
+public class RecentFilesManager {
 
     /** Logger */
-    private static final Logger _logger = LoggerFactory.getLogger(RecentlyOpenedFilesManager.class.getName());
+    private static final Logger _logger = LoggerFactory.getLogger(RecentFilesManager.class.getName());
     /** Singleton instance */
-    private static volatile RecentlyOpenedFilesManager _instance = null;
+    private static volatile RecentFilesManager _instance = null;
     /** Hook to the "Open Recent" sub-menu */
     private static volatile JMenu _menu = null;
     /* JMenu to Action relations */
@@ -37,10 +38,10 @@ public class RecentlyOpenedFilesManager {
      * Return the singleton instance
      * @return singleton instance
      */
-    private static synchronized RecentlyOpenedFilesManager getInstance() {
+    private static synchronized RecentFilesManager getInstance() {
         // DO NOT MODIFY !!!
         if (_instance == null) {
-            _instance = new RecentlyOpenedFilesManager();
+            _instance = new RecentFilesManager();
         }
 
         return _instance;
@@ -51,7 +52,7 @@ public class RecentlyOpenedFilesManager {
     /**
      * Hidden constructor
      */
-    protected RecentlyOpenedFilesManager() {
+    protected RecentFilesManager() {
         _registrar = ActionRegistrar.getInstance();
         // @TODO : the same for shared prefs.
     }
@@ -65,14 +66,7 @@ public class RecentlyOpenedFilesManager {
         getInstance();
 
         _menu = new JMenu("Open Recent");
-
         populateMenuFromSharedPreferences();
-        // If no recent files registered at all
-        if (_files.isEmpty()) {
-            // Make the "Open Recent" sub-menu disabled
-            _menu.setEnabled(false);
-        }
-
         addCleanAction();
 
         return _menu;
@@ -83,6 +77,18 @@ public class RecentlyOpenedFilesManager {
      * @TODO : Add a "Clear" item at end below a separator
      */
     private static void addCleanAction() {
+
+        final AbstractAction cleanAction = new AbstractAction("Clear History") {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                _menu.removeAll();
+                _files.clear();
+                _menu.setEnabled(false);
+            }
+        };
+
+        _menu.add(new JSeparator());
+        _menu.add(new JMenuItem(cleanAction));
     }
 
     /**
@@ -96,37 +102,57 @@ public class RecentlyOpenedFilesManager {
          addFile("/tmp/tutu", "tutu");
          addFile("/Users/lafrasse/test.scvot", "test.scvot");
          */
-    }
-
-    public static synchronized void addFile(final File file) {
-        try {
-            addFile(file.getCanonicalPath(), file.getName());
-        } catch (IOException ex) {
-            _logger.warn("Could not resole file path", ex);
+        // If no recent files registered at all
+        if (_files.isEmpty()) {
+            // Make the "Open Recent" sub-menu disabled
+            _menu.setEnabled(false);
         }
     }
 
-    private static synchronized void addFile(final String path, final String name) {
-
+    public static synchronized void addFile(final File file) {
         getInstance();
 
-        // TODO : check params validity (null, ...)
+        // Check parameter validity
+        if (!file.canRead()) {
+            _logger.warn("Could not read file " + file);
+            return;
+        }
 
+        // Check file path
+        String path = null;
+        try {
+            path = file.getCanonicalPath();
+        } catch (IOException ex) {
+            _logger.warn("Could not resolve file path", ex);
+            return;
+        }
+        if ((path == null) || (path.length() == 0)) {
+            _logger.warn("Could not resolve empty file path");
+            return;
+        }
+
+        // Check file name
+        String name = file.getName();
+        // If no name found
+        if ((name == null) || (name.length() == 0)) {
+            name = path; // Use path instead
+        }
+
+        // Store new recent file
         _files.put(path, name);
 
-        // Clean, then fill sub-menu
+        // Clean, then re-fill sub-menu
         _menu.removeAll();
         for (final String currentPath : _files.keySet()) {
 
             final String currentName = _files.get(currentPath);
 
             final AbstractAction currentAction = new AbstractAction(currentName) {
+                @Override
                 public void actionPerformed(ActionEvent ae) {
                     _registrar.getOpenAction().actionPerformed(new ActionEvent(_registrar, 0, currentPath));
                 }
             };
-
-            // TODO : disable action if file vanished
 
             _menu.add(new JMenuItem(currentAction));
         }

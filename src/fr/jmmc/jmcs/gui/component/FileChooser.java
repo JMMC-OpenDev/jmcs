d48183e5f771f0a951dcb41aed9541566ec7e492
sly74fr <slymacdev@free.fr>
2012-11-26 10:11:51 +0000

    Removed hard-linked recent file archiving.

--- src/fr/jmmc/jmcs/gui/component/FileChooser.java
+++ src/fr/jmmc/jmcs/gui/component/FileChooser.java
@@ -5,7 +5,6 @@ package fr.jmmc.jmcs.gui.component;
 
 import fr.jmmc.jmcs.App;
 import fr.jmmc.jmcs.data.preference.FileChooserPreferences;
-import fr.jmmc.jmcs.gui.action.RecentFilesManager;
 import fr.jmmc.jmcs.util.FileUtils;
 import fr.jmmc.jmcs.util.MimeType;
 import java.awt.FileDialog;
@@ -241,13 +240,6 @@ public final class FileChooser {
 
         if (selectedFiles != null) {
             updateDirectoryForMimeType(mimeType, selectedFiles[0].getParent());
-
-            final RecentFilesManager rfm = RecentFilesManager.getInstance();
-
-            // Add each file to the recently opened file list
-            for (File file : selectedFiles) {
-                rfm.addFile(file);
-            }
         }
 
         return selectedFiles;

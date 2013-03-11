ad5fd694b1951a398671f91aee3985f25d501765
sly74fr <slymacdev@free.fr>
2013-03-11 10:10:42 +0000

    Moved (soon-to-be-removed) Deque and ArrayDeque Sun JDK 1.6 sources to fr.jmmc.jmcs.collection.

--- src/fr/jmmc/jmcs/collection/ArrayDeque.java
+++ src/fr/jmmc/jmcs/collection/ArrayDeque.java
@@ -4,7 +4,7 @@
  * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package org.ivoa.util.concurrent;
+package fr.jmmc.jmcs.collection;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
--- src/fr/jmmc/jmcs/collection/Deque.java
+++ src/fr/jmmc/jmcs/collection/Deque.java
@@ -4,7 +4,7 @@
  * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-package org.ivoa.util.concurrent;
+package fr.jmmc.jmcs.collection;
 
 import java.util.Iterator;
 import java.util.Queue;

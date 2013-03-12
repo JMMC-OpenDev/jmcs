72f898a1be95cc427fefa5cb99c9ad5a1fc06f18
sly74fr <slymacdev@free.fr>
2013-03-12 13:49:01 +0000

    Added jMCS web site button. Fixed icon URLs.

--- modjava/src/fr/jmmc/mcs/modjava/Main.java
+++ modjava/src/fr/jmmc/mcs/modjava/Main.java
@@ -6,11 +6,11 @@ package fr.jmmc.mcs.modjava;
 import fr.jmmc.jmcs.App;
 import fr.jmmc.jmcs.Bootstrapper;
 import fr.jmmc.jmcs.gui.action.RegisteredAction;
-import fr.jmmc.jmcs.gui.action.internal.InternalActionFactory;
 import fr.jmmc.jmcs.gui.component.DismissableMessagePane;
 import fr.jmmc.jmcs.gui.component.MessagePane;
 import fr.jmmc.jmcs.gui.component.StatusBar;
 import fr.jmmc.jmcs.gui.util.WindowUtils;
+import fr.jmmc.jmcs.network.BrowserLauncher;
 import fr.jmmc.jmcs.network.interop.SampCapability;
 import fr.jmmc.jmcs.network.interop.SampMessageHandler;
 import fr.jmmc.jmcs.util.RecentFilesManager;
@@ -23,7 +23,6 @@ import java.util.Map;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import org.astrogrid.samp.Message;
 import org.astrogrid.samp.SampUtils;
 import org.slf4j.Logger;
@@ -36,15 +35,13 @@ public class Main extends App {
 
     /** Logger */
     private static final Logger _logger = LoggerFactory.getLogger(Main.class.getName());
-    /** Button to launch feedback report window */
-    private JButton _feedbackReportButton = null;
-    /** Button to launch help view window */
-    private JButton _helpViewButton = null;
+    /** Button to open jMCS web site in the default web browser */
+    private JButton _openBrowserButton = null;
     /** Actions class */
     public RegisteredAction _openAction;
-    private Actions _actions = null;
     /** Test button */
     private JButton _testDismissableMessagePane = null;
+    private Actions _actions = null;
 
     /**
      * Constructor.
@@ -74,8 +71,7 @@ public class Main extends App {
         _openAction = openAction();
 
         // Buttons
-        _feedbackReportButton = new JButton(InternalActionFactory.showFeedbackReportAction());
-        _helpViewButton = new JButton(openHelpFrame());
+        _openBrowserButton = new JButton(openJMcsWebSite());
         _testDismissableMessagePane = new JButton(dismissableMessagePaneAction());
 
         final Container framePanel = getFramePanel();
@@ -84,7 +80,7 @@ public class Main extends App {
         framePanel.setLayout(new BorderLayout());
 
         // Add buttons to panel
-        framePanel.add(_feedbackReportButton, BorderLayout.NORTH);
+        framePanel.add(_openBrowserButton, BorderLayout.NORTH);
         framePanel.add(_testDismissableMessagePane, BorderLayout.CENTER);
         framePanel.add(new StatusBar(), BorderLayout.SOUTH);
 
@@ -141,33 +137,21 @@ public class Main extends App {
     /** application cleanup */
     @Override
     protected void cleanup() {
-        _feedbackReportButton = null;
-        _helpViewButton = null;
+        _openBrowserButton = null;
         _openAction = null;
         _actions = null;
         _testDismissableMessagePane = null;
     }
 
     /** Open help view action */
-    private Action openHelpFrame() {
-        return new AbstractAction("Open Help Frame") {
+    private Action openJMcsWebSite() {
+        return new AbstractAction("Open jMCS Web Site") {
             private static final long serialVersionUID = 1L;
 
             @Override
             public void actionPerformed(ActionEvent evt) {
-                // Instantiate help JFrame
-                JFrame helpFrame = new JFrame("- Help frame -");
-
-                // Add buttons to panel
-                helpFrame.getContentPane().add(new JButton(InternalActionFactory.showHelpAction()), BorderLayout.NORTH);
-                helpFrame.getContentPane().add(new JButton("CENTER"), BorderLayout.CENTER);
-                helpFrame.getContentPane().add(new JButton("WEST"), BorderLayout.WEST);
-                helpFrame.getContentPane().add(new JButton("SOUTH"), BorderLayout.SOUTH);
-
-                // Set the frame properties
-                helpFrame.pack();
-                helpFrame.setLocationRelativeTo(null);
-                helpFrame.setVisible(true);
+                BrowserLauncher.openURL("http://www.jmmc.fr/dev/jmcs");
+                StatusBar.show("jMCS web site open.");
             }
         };
     }

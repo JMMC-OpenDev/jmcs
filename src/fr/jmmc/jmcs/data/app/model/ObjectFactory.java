1090b0672bceaa2487fa125185acd1d9700ad3b7
sly74fr <slymacdev@free.fr>
2013-03-14 13:55:15 +0000

    Moved all application XML description stuff in a dedicated package.

--- src/fr/jmmc/jmcs/data/app/model/ObjectFactory.java
+++ src/fr/jmmc/jmcs/data/app/model/ObjectFactory.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlRegistry;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the fr.jmmc.jmcs.data.model package. 
+ * generated in the fr.jmmc.jmcs.data.app.model package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -23,7 +23,7 @@ public class ObjectFactory {
 
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: fr.jmmc.jmcs.data.model
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: fr.jmmc.jmcs.data.app.model
      * 
      */
     public ObjectFactory() {

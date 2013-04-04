8fd9b6cfe0e16667ee976ef8c889c69ba0d080f5
sly74fr <slymacdev@free.fr>
2013-04-04 16:43:46 +0000

    Fixed headers.

--- src/fr/jmmc/jmcs/gui/action/ActionRegistrar.java
+++ src/fr/jmmc/jmcs/gui/action/ActionRegistrar.java
@@ -2,7 +2,7 @@
  *                 jMCS project ( http://www.jmmc.fr/dev/jmcs )
  *******************************************************************************
  * Copyright (c) 2013, CNRS. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     - Redistributions of source code must retain the above copyright
@@ -13,7 +13,7 @@
  *     - Neither the name of the CNRS nor the names of its contributors may be
  *       used to endorse or promote products derived from this software without
  *       specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE

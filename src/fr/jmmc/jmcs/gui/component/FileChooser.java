a3d68a19085566b44a7619d5a3a9215e0ad57d77
sly74fr <slymacdev@free.fr>
2013-04-18 15:12:03 +0000

    Renamed SessionPersistencePreferences to SessionSettingsPreferences.

--- src/fr/jmmc/jmcs/gui/component/FileChooser.java
+++ src/fr/jmmc/jmcs/gui/component/FileChooser.java
@@ -28,7 +28,7 @@
 package fr.jmmc.jmcs.gui.component;
 
 import fr.jmmc.jmcs.App;
-import fr.jmmc.jmcs.data.preference.SessionPersistencePreferences;
+import fr.jmmc.jmcs.data.preference.SessionSettingsPreferences;
 import fr.jmmc.jmcs.util.FileUtils;
 import fr.jmmc.jmcs.data.MimeType;
 import java.awt.FileDialog;
@@ -354,7 +354,7 @@ public final class FileChooser {
     private static File retrieveLastDirectoryForMimeType(final File givenDirectory, final MimeType mimeType) {
         File preselectedDirectory = givenDirectory;
         if (preselectedDirectory == null && mimeType != null) {
-            preselectedDirectory = SessionPersistencePreferences.getLastDirectoryForMimeTypeAsFile(mimeType);
+            preselectedDirectory = SessionSettingsPreferences.getLastDirectoryForMimeTypeAsFile(mimeType);
         }
         return preselectedDirectory;
     }
@@ -366,7 +366,7 @@ public final class FileChooser {
      */
     private static void updateDirectoryForMimeType(final MimeType mimeType, final String path) {
         if (path != null && mimeType != null) {
-            SessionPersistencePreferences.setCurrentDirectoryForMimeType(mimeType, path);
+            SessionSettingsPreferences.setCurrentDirectoryForMimeType(mimeType, path);
         }
     }
 

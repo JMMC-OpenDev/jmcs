375bf1d212b71aff5d0cc3d27dd9dc1a09dd6f4c
bourgesl <bourges.laurent@gmail.com>
2014-03-31 14:23:22 +0000

    catch any exception thrown by App.cleanup() and open a feedback report (once) avoid reentrance in stopApp() that can be called in many places (quitAction, window closing listeners, feedback report ...)

--- src/main/java/fr/jmmc/jmcs/Bootstrapper.java
+++ src/main/java/fr/jmmc/jmcs/Bootstrapper.java
@@ -275,7 +275,7 @@ public final class Bootstrapper {
      * @throws IllegalStateException
      */
     public static boolean launchApp(final App application, final boolean waitBeforeExecution, final boolean exitWhenClosed,
-            final boolean shouldShowSplashScreen) throws IllegalStateException {
+                                    final boolean shouldShowSplashScreen) throws IllegalStateException {
 
         return ___internalLaunch(application, exitWhenClosed, shouldShowSplashScreen);
     }
@@ -333,14 +333,18 @@ public final class Bootstrapper {
             MessagePane.showErrorMessage("An error occured while initializing the application");
 
             // Add last chance tip if this exception appears in an inited state but before being ready. (cf. trac #458)
+            final Throwable throwable;
             if (stateOnError.after(ApplicationState.ENV_INIT) && stateOnError.before(ApplicationState.APP_READY)) {
                 final String warningMessage = "The application did not start properly. Please try first to start it again from the website:\n"
                         + ApplicationDescription.getInstance().getLinkValue()
                         + "\nIf this operation does not fix the problem, please send us a feedback report!\n\n";
-                FeedbackReport.openDialog(true, new Throwable(warningMessage, th));
+
+                throwable = new Throwable(warningMessage, th);
             } else {
-                FeedbackReport.openDialog(true, th);
+                throwable = th;
             }
+            /* use invokeAndWaitEDT ie blocking the current thread */
+            FeedbackReport.openDialog(true, throwable);
         }
 
         return launchDone;
@@ -454,7 +458,7 @@ public final class Bootstrapper {
      * @param evt the triggering event if any, null otherwise.
      */
     public static void quitApp(final ActionEvent evt) {
-        _jmmcLogger.info("Application quitting.");
+        _jmmcLogger.info("Quitting the application ...");
 
         // Mac OS X Quit action handler
         final QuitResponse response;
@@ -516,51 +520,81 @@ public final class Bootstrapper {
      * @param statusCode status code to return
      */
     public static void stopApp(final int statusCode) {
-        _jmmcLogger.info("Stopping the application.");
-        try {
-            if (_application != null) {
+        if (isInState(ApplicationState.JAVA_LIMB)) {
+            return;
+        }
+        boolean appCleanupFailed = false;
 
+        // Avoid reentrance if an exception occured (Feedback Report):
+        if (_application != null && getState().before(ApplicationState.APP_CLEANUP)) {
+            _jmmcLogger.info("Stopping the application ...");
+            try {
                 setState(ApplicationState.APP_CLEANUP);
                 _application.cleanup();
 
-                setState(ApplicationState.ENV_CLEANUP);
-                ___internalStop();
-            }
-        } finally {
-            setState(ApplicationState.APP_DEAD);
-            App.___internalSingletonCleanup();
-            if (!_avoidSystemExit) {
-                _jmmcLogger.info("Exiting with status code '{}'.", statusCode);
-                System.exit(statusCode);
+            } catch (Throwable th) {
+                appCleanupFailed = true;
+                setState(ApplicationState.APP_CLEANUP_FAIL);
+
+                /* use invokeAndWaitEDT ie blocking the current thread */
+                FeedbackReport.openDialog(true, th);
             }
         }
-        setState(ApplicationState.JAVA_LIMB);
+
+        // Avoid reentrance if an exception occured (Feedback Report):
+        if (appCleanupFailed
+                || (!isInState(ApplicationState.APP_CLEANUP_FAIL) && getState().before(ApplicationState.ENV_CLEANUP))) {
+
+            setState(ApplicationState.ENV_CLEANUP);
+            // should not throw runtime exception:
+            ___internalStop();
+
+            setState(ApplicationState.APP_DEAD);
+            App.___internalSingletonCleanup();
+
+            if (!_avoidSystemExit) {
+                /* fix status code if an exception occured during application cleanup */
+                final int exitCode = (appCleanupFailed) ? -1 : statusCode;
+
+                _jmmcLogger.info("Exiting with status code '{}'.", exitCode);
+
+                System.exit(exitCode);
+            }
+            setState(ApplicationState.JAVA_LIMB);
+        }
     }
 
     /**
      * Internal: Stop services
      */
     private static void ___internalStop() {
-        // Save session settings if needed
-        SessionSettingsPreferences.saveToFileIfNeeded();
+        _jmmcLogger.info("Stopping internal services ...");
+        try {
+            // Save session settings if needed:
+            SessionSettingsPreferences.saveToFileIfNeeded();
 
-        // Stop the job runner (if any)
-        LocalLauncher.shutdown();
+            // Stop the job runner (if any)
+            LocalLauncher.shutdown();
 
-        // Stop the task executor (if any)
-        TaskSwingWorkerExecutor.shutdown();
+            // Stop the task executor (if any)
+            TaskSwingWorkerExecutor.shutdown();
 
-        // Stop the parallel job executor (if any)
-        ParallelJobExecutor.shutdown();
+            // Stop the parallel job executor (if any)
+            ParallelJobExecutor.shutdown();
 
-        // Disconnect from SAMP Hub (if any)
-        SampManager.shutdown();
+            // Disconnect from SAMP Hub (if any)
+            SampManager.shutdown();
 
-        // Close all HTTP connections (http client) (if any)
-        MultiThreadedHttpConnectionManager.shutdownAll();
+            // Close all HTTP connections (http client) (if any)
+            MultiThreadedHttpConnectionManager.shutdownAll();
 
-        // Switch to logging exception handler:
-        MCSExceptionHandler.installLoggingHandler();
+            // Switch to logging exception handler:
+            MCSExceptionHandler.installLoggingHandler();
+
+        } catch (RuntimeException re) {
+            // should not happen but anyway log any potential exception:
+            _jmmcLogger.warn("A runtime exception occured while stopping services: ", re);
+        }
     }
 
     /**

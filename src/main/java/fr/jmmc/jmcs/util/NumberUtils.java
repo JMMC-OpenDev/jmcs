b31460fd6926e73b7e0e32223616605de327788a
bourgesl <bourges.laurent@gmail.com>
2016-05-02 09:07:21 +0000

    added compare(int, int) for JDK < 1.7

--- src/main/java/fr/jmmc/jmcs/util/NumberUtils.java
+++ src/main/java/fr/jmmc/jmcs/util/NumberUtils.java
@@ -245,6 +245,26 @@ public final class NumberUtils {
     }
 
     /**
+     * From OpenJDK7 Integer class:
+     *
+     * Compares two {@code int} values numerically.
+     * The value returned is identical to what would be returned by:
+     * <pre>
+     *    Integer.valueOf(x).compareTo(Integer.valueOf(y))
+     * </pre>
+     *
+     * @param  x the first {@code int} to compare
+     * @param  y the second {@code int} to compare
+     * @return the value {@code 0} if {@code x == y};
+     *         a value less than {@code 0} if {@code x < y}; and
+     *         a value greater than {@code 0} if {@code x > y}
+     * @since 1.7
+     */
+    public static int compare(final int x, final int y) {
+        return (x < y) ? -1 : ((x == y) ? 0 : 1);
+    }
+
+    /**
      * Returns an {@code Integer} instance representing the specified
      * {@code int} value.  If a new {@code Integer} instance is not
      * required, this method should generally be used in preference to

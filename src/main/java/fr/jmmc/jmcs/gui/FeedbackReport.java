2d43fe1c98711c2ff7d0d0080ea8e1571226a12c
bourgesl <bourges.laurent@gmail.com>
2016-12-14 10:55:57 +0000

    added environment settings in system logs

--- src/main/java/fr/jmmc/jmcs/gui/FeedbackReport.java
+++ src/main/java/fr/jmmc/jmcs/gui/FeedbackReport.java
@@ -654,8 +654,12 @@ public class FeedbackReport extends javax.swing.JDialog implements KeyListener {
      */
     private static String getSystemConfig() {
         final StringBuilder sb = new StringBuilder(16384);
-        sb.append(JVMUtils.getMemoryInfo()).append("\n\n");
+        sb.append(JVMUtils.getMemoryInfo());
 
+        sb.append("\n\nEnvironment settings:\n");
+        Preferences.dumpProperties(System.getenv(), sb);
+
+        sb.append("\n\nSystem properties:\n");
         // Get all informations about the system running the application
         return Preferences.dumpProperties(System.getProperties(), sb).toString();
     }

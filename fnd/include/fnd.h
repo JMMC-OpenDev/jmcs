ea2f9fc81bd03b0c0a66fb83ecb4d59158d83ac7
sly74fr <slymacdev@free.fr>
2017-06-01 14:35:17 +0000

    Automatically tagged MCS in version SC_V5_0_0 from trunk  (admManager.sh)

---
+++ fnd/include/fnd.h
@@ -0,0 +1,18 @@
+#ifndef fnd_H
+#define fnd_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ * Main header file of the fnd library
+ */
+
+#include "fndOBJECT.h"
+#include "fndMVC_MODEL.h"
+#include "fndMVC_VIEW.h"
+
+#endif /*!fnd_H*/
+
+/*___oOo___*/
---
+++ fnd/src/fndOBJECT.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ * Definition of fndOBJECT class.
+ */
+
+
+/* 
+ * System Headers 
+ */
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "fndOBJECT.h"
+#include "fndPrivate.h"
+
+
+/*
+ * Class constructor
+ */
+fndOBJECT::fndOBJECT()
+{
+}
+
+/*
+ * Class destructor
+ */
+fndOBJECT::~fndOBJECT()
+{
+}
+
+/*___oOo___*/

11ca946d352186a9cd8c2c64da34faa52cf67550
sly74fr <slymacdev@free.fr>
2017-06-30 12:59:47 +0000

    Automatically tagged MCS in version SC_V5_0_1 from trunk  (admManager.sh)

---
+++ mcscfg/src/mcscfgShow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#*******************************************************************************
+# JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+#*******************************************************************************
+
+#/**
+# \file
+# This script show the current MCS configuration. 
+#
+# \synopsis
+# mcscfgShow
+#
+# */
+
+echo "MCSTOP     = $MCSTOP";
+echo "MCSDATA    = $MCSDATA";
+echo "MCSRELEASE = $MCSRELEASE";
+echo "MCSROOT    = $MCSROOT";
+echo "MCSENV     = $MCSENV";
+echo "INTROOT    = $INTROOT"
+
+#___oOo___

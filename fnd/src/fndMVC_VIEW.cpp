8d5c0821a522a4afbe036979368fd78906e4dc8e
sly74fr <slymacdev@free.fr>
2017-06-30 12:59:52 +0000

    Automatically tagged MCS in version SC_V5_0_1 from trunk  (admManager.sh)

---
+++ fnd/src/fndMVC_VIEW.cpp
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ *  Definition of fndMVC_VIEW class.
+ */
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+/*
+ * Local Headers 
+ */
+#include "fndMVC_VIEW.h"
+#include "fndPrivate.h"
+
+/**
+ * Class constructor
+ */
+fndMVC_VIEW::fndMVC_VIEW()
+{
+}
+
+/**
+ * Class destructor
+ */
+fndMVC_VIEW::~fndMVC_VIEW()
+{
+}
+
+/*___oOo___*/

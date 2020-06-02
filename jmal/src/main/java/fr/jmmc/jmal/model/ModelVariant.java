d62a481f91e9db2a7f85fda3598623fedb27ece9
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2020-06-02 07:12:10 +0000

    Automatically tagged OiDB-deps in version OIDB_V1_10-beta from trunk  (admManager.sh)

---
+++ jmal/src/main/java/fr/jmmc/jmal/model/ModelVariant.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmal.model;
+
+/**
+ * Variant enumeration (standard, elongated and flattened models)
+ * @author bourgesl
+ */
+public enum ModelVariant {
+
+    /** default/standard model variant */
+    Standard,
+    /** elongated model variant */
+    Elongated,
+    /** flattened model variant */
+    Flattened
+}

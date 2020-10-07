57c0a6ae693b8481ae35316234aa6bc000a9ff7c
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2020-10-07 10:22:57 +0000

    Automatically tagged OiDB-deps in version OIDB_V2_0_12 from trunk  (admManager.sh)

---
+++ jmal/src/main/java/fr/jmmc/jmal/model/ImageMode.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmal.model;
+
+/**
+ * Image modes (amplitude, phase)
+ *
+ * @author bourgesl
+ */
+public enum ImageMode {
+
+    /** Amplitude */
+    AMP,
+    /** Phase */
+    PHASE,
+    /** Square amplitude */
+    SQUARE
+}

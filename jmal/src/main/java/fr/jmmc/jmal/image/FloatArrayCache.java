af0c06e7fc3dcbb8d4f7b3b38a7606cb2844166f
bourgesl <bourges.laurent@gmail.com>
2021-06-10 10:02:52 +0000

    Automatically tagged LITpro in version LITPRO_V1_1_0 from trunk  (admManager.sh)

---
+++ jmal/src/main/java/fr/jmmc/jmal/image/FloatArrayCache.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.jmal.image;
+
+import fr.jmmc.jmal.util.GenericWeakCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple float[][] weak cache
+ * @author bourgesl
+ */
+public final class FloatArrayCache {
+
+    /** Class logger */
+    private static final Logger logger = LoggerFactory.getLogger(FloatArrayCache.class.getName());
+
+    /** weak image cache for createImage()/recycleImage() */
+    private final static GenericWeakCache<float[][]> array2dCache = new GenericWeakCache<float[][]>("FloatArray2D") {
+
+        @Override
+        protected boolean checkSizes(float[][] array2D, int length, int length2) {
+            return (array2D.length == length && array2D[0].length == length2);
+        }
+
+        @Override
+        public String getSizes(float[][] array2D) {
+            return String.format("%d x %d", array2D.length, array2D[0].length);
+        }
+    };
+
+    public static float[][] getArray(int length, int length2) {
+        float[][] array2D = array2dCache.getItem(length, length2);
+        if (array2D != null) {
+            return array2D;
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("create a new array: {} x {}", length + 4, length2 + 4);
+        }
+        return new float[length][length2];
+    }
+
+    public static void recycleArray(final float[][] array2D) {
+        array2dCache.putItem(array2D);
+    }
+
+    private FloatArrayCache() {
+        super();
+    }
+
+}

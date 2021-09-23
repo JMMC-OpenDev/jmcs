8d30f0500f7ca8a4edbabd8aed843fea5e2a73ba
bourgesl <bourges.laurent@gmail.com>
2021-09-23 22:21:55 +0200

    added generic ColumnDesc and ColumnDescTableModel (abstract)

---
+++ src/main/java/fr/jmmc/jmcs/model/ColumnDescTableModel.java
@@ -0,0 +1,77 @@
+/** *****************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ***************************************************************************** */
+package fr.jmmc.jmcs.model;
+
+import fr.jmmc.jmcs.gui.component.BasicTableColumnModel;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * This abstract class deals with the ColumnDesc list describing the table columns
+ */
+public abstract class ColumnDescTableModel extends AbstractTableModel implements BasicTableColumnModel {
+
+    /* members */
+    /** list of ColumnDesc. Uniques by getName() */
+    protected final List<ColumnDesc> listColumnDesc;
+    /** temporary buffer */
+    protected final StringBuilder sbTmp = new StringBuilder(256);
+
+    public ColumnDescTableModel() {
+        super();
+        listColumnDesc = new ArrayList<>();
+    }
+
+    /** @return list of ColumnDesc */
+    public final List<ColumnDesc> getColumnDescList() {
+        return listColumnDesc;
+    }
+
+    public final List<String> getColumnNames() {
+        final List<String> columnNames = new ArrayList<>(listColumnDesc.size());
+        listColumnDesc.forEach(columnDesc -> columnNames.add(columnDesc.getName()));
+        return columnNames;
+    }
+
+    public final ColumnDesc getColumnDesc(final int columnIndex) {
+        return this.listColumnDesc.get(columnIndex);
+    }
+
+    @Override
+    public final int getColumnCount() {
+        return listColumnDesc.size();
+    }
+
+    @Override
+    public final String getColumnName(int columnIndex) {
+        return getColumnDesc(columnIndex).getName();
+    }
+
+    @Override
+    public final Class<?> getColumnClass(int columnIndex) {
+        return getColumnDesc(columnIndex).getDataClass();
+    }
+
+    // BasicTableColumnModel impl    
+    @Override
+    public final String getColumnLabel(final int columnIndex) {
+        return getColumnDesc(columnIndex).getLabel();
+    }
+
+    @Override
+    public final String getColumnTooltipText(final int columnIndex) {
+        final ColumnDesc columnDesc = getColumnDesc(columnIndex);
+        sbTmp.setLength(0);
+        if (columnDesc.getLabel() != null) {
+            sbTmp.append(columnDesc.getLabel()).append(' ');
+        }
+        sbTmp.append('[').append(columnDesc.getName()).append("] ");
+        if (columnDesc.getDescription() != null) {
+            sbTmp.append(columnDesc.getDescription());
+        }
+        return sbTmp.toString();
+    }
+
+}
